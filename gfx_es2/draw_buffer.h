3c818e0af7647af95fd1770a5599c08618886d19
Henrik Rydgard <hrydgard@gmail.com>
2012-07-01 11:38:18 +0200

    Cleanup, add some wrappers for compat with some old code I have

--- gfx_es2/draw_buffer.h
+++ gfx_es2/draw_buffer.h
@@ -16,6 +16,7 @@ enum {
   ALIGN_VCENTER = 8,
   ALIGN_VBASELINE = 32,  // text only, possibly not yet working
 
+  ALIGN_CENTER = ALIGN_HCENTER | ALIGN_VCENTER,
   ALIGN_TOPLEFT = ALIGN_TOP | ALIGN_LEFT,
   ALIGN_TOPRIGHT = ALIGN_TOP | ALIGN_RIGHT,
   ALIGN_BOTTOMLEFT = ALIGN_BOTTOM | ALIGN_LEFT,
@@ -51,16 +52,32 @@ public:
   int Count() const { return count_; }
 
   void Flush(const GLSLProgram *program, bool set_blend_state=true);
+  
   void Rect(float x, float y, float w, float h, uint32 color, int align = ALIGN_TOPLEFT);
+  void hLine(float x1, float y, float x2, uint32 color) { Rect(x1, y, x2 - x1, 1, color); }
+  void vLine(float x, float y1, float y2, uint32 color) { Rect(x, y1, 1, y2 - y1, color); }
+  void vLineAlpha50(float x, float y1, float y2, uint32 color) { Rect(x, y1, 1, y2 - y1, (color | 0xFF000000) & 0x7F000000); }
+
+  void RectOutline(float x, float y, float w, float h, uint32 color, int align = ALIGN_TOPLEFT) {
+    hLine(x, y, x + w + 1, color);
+    hLine(x, y + h, x + w + 1, color);
+
+    vLine(x, y, y + h + 1, color);
+    vLine(x + w, y, y + h + 1, color);
+  }
+  
   void RectVGradient(float x, float y, float w, float h, uint32 colorTop, uint32 colorBottom);
+  void RectVDarkFaded(float x, float y, float w, float h, uint32 colorTop) {
+    RectVGradient(x, y, w, h, colorTop, darkenColor(colorTop));
+  }
 
   void MultiVGradient(float x, float y, float w, float h, GradientStop *stops, int numStops);
 
   void RectCenter(float x, float y, float w, float h, uint32 color) {
     Rect(x - w/2, y - h/2, w, h, color);
   }
-  void Rect(float x, float y, float w, float h,
-    float u, float v, float uw, float uh, uint32 color);
+  void Rect(float x, float y, float w, float h, float u, float v, float uw, float uh, uint32 color);
+
   void V(float x, float y, float z, uint32 color, float u, float v);
   void V(float x, float y, uint32 color, float u, float v) {
     V(x, y, 0.0f, color, u, v);
@@ -85,8 +102,8 @@ public:
   void DrawImage2GridH(int atlas_image, float x1, float y1, float x2, Color color = COLOR(0xFFFFFF), float scale = 1.0);
 
   void MeasureText(int font, const char *text, float *w, float *h);
-  void DrawText(int font, const char *text, float x, float y, Color color = 0xFFFFFFFF, int flags = 0);
-  void DrawTextShadow(int font, const char *text, float x, float y, Color color = 0xFFFFFFFF, int flags = 0);
+  void DrawText(int font, const char *text, float x, float y, Color color = 0xFFFFFFFF, int align = 0);
+  void DrawTextShadow(int font, const char *text, float x, float y, Color color = 0xFFFFFFFF, int align = 0);
 
   void RotateSprite(int atlas_entry, float x, float y, float angle, float scale, Color color);
   void SetFontScale(float xs, float ys) {
