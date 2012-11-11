72ad8f5ca850471612f92e7552ab503d09f85557
Henrik Rydgard <hrydgard@gmail.com>
2012-11-11 19:07:07 +0100

    Actually, double-oops.

--- GPU/GLES/TransformPipeline.cpp
+++ GPU/GLES/TransformPipeline.cpp
@@ -286,7 +286,7 @@ void TransformAndDrawPrim(void *verts, void *inds, int prim, int vertexCount, Li
 				// Skinning
 				Vec3 psum(0,0,0);
 				Vec3 nsum(0,0,0);
-				int nweights = (gstate.vertType & GE_VTYPE_WEIGHTCOUNT_MASK) >> GE_VTYPE_WEIGHTCOUNT_SHIFT;
+				int nweights = ((gstate.vertType & GE_VTYPE_WEIGHTCOUNT_MASK) >> GE_VTYPE_WEIGHTCOUNT_SHIFT) + 1;
 				for (int i = 0; i < nweights; i++)
 				{
 					if (decoded[index].weights[i] != 0.0f) {
