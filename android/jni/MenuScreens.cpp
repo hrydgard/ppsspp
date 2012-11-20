45369af482f2bd93ba7a3d3a4ae232de7e1e45bc
Henrik Rydgard <hrydgard@gmail.com>
2012-11-20 22:40:55 +0100

    0.31 in the UI

--- android/jni/MenuScreens.cpp
+++ android/jni/MenuScreens.cpp
@@ -147,7 +147,7 @@ void MenuScreen::render() {
 
 	ui_draw2d.DrawTextShadow(UBUNTU48, "PPSSPP", dp_xres + xoff - w/2, 80, 0xFFFFFFFF, ALIGN_HCENTER | ALIGN_BOTTOM);
 	ui_draw2d.SetFontScale(0.7f, 0.7f);
-	ui_draw2d.DrawTextShadow(UBUNTU24, "V0.3", dp_xres + xoff, 80, 0xFFFFFFFF, ALIGN_RIGHT | ALIGN_BOTTOM);
+	ui_draw2d.DrawTextShadow(UBUNTU24, "V0.31", dp_xres + xoff, 80, 0xFFFFFFFF, ALIGN_RIGHT | ALIGN_BOTTOM);
 	ui_draw2d.SetFontScale(1.0f, 1.0f);
 	VLinear vlinear(dp_xres + xoff, 95, 20);
 
@@ -354,7 +354,7 @@ void CreditsScreen::update(InputState &input_state) {
 
 static const char *credits[] =
 {
-	"PPSSPP v0.3",
+	"PPSSPP v0.31",
 	"",
 	"",
 	"A fast and portable PSP emulator",
