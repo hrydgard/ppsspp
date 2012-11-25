41e5a5f3675765114d25357509ddd564d5691674
Henrik Rydgård <hrydgard@gmail.com>
2012-11-25 12:38:44 +0100

    proper tabs

--- gfx_es2/gl_state.h
+++ gfx_es2/gl_state.h
@@ -23,7 +23,7 @@ private:
 		BoolState() : _value(init) {}
 
 		inline void set(bool value) {
-			if(value && value != _value) {				
+			if(value && value != _value) {
 				_value = value;
 				glEnable(cap);
 			}
@@ -51,39 +51,39 @@ private:
 				glDisable(cap);
 		}
 	};
-	
+
 #define STATE1(func, p1type, p1def) \
 	class SavedState1_##func { \
-        p1type p1; \
+		p1type p1; \
 	public: \
 	SavedState1_##func() : p1(p1def) {}; \
 		void set(p1type newp1) { \
-		    if(newp1 != p1) { \
-			    p1 = newp1; \
-                func(p1); \
+			if(newp1 != p1) { \
+				p1 = newp1; \
+				func(p1); \
 			} \
-	    } \
+		} \
 		void restore() { \
-            func(p1); \
-	    } \
+			func(p1); \
+		} \
 	}
 
 #define STATE2(func, p1type, p2type, p1def, p2def) \
 	class SavedState2_##func { \
-        p1type p1; \
-        p2type p2; \
+		p1type p1; \
+		p2type p2; \
 	public: \
 	SavedState2_##func() : p1(p1def), p2(p2def) {}; \
 		inline void set(p1type newp1, p2type newp2) { \
-		    if(newp1 != p1 || newp2 != p2) { \
-			    p1 = newp1; \
+			if(newp1 != p1 || newp2 != p2) { \
+				p1 = newp1; \
 				p2 = newp2; \
-                func(p1, p2); \
+				func(p1, p2); \
 			} \
-	    } \
+		} \
 		inline void restore() { \
-            func(p1, p2); \
-	    } \
+			func(p1, p2); \
+		} \
 	}
 
 	bool initialized;
