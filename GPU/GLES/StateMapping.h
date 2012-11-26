b76152d88a4e9b9437fd84f36d5bb54ed4cfd5bc
Sacha <xsacha@gmail.com>
2012-11-26 20:38:39 +1000

    Use gl_common from native.

--- GPU/GLES/StateMapping.h
+++ GPU/GLES/StateMapping.h
@@ -1,18 +1,4 @@
-#if defined(USING_GLES2)
-#include <GLES2/gl2.h>
-#include <GLES2/gl2ext.h>
-#elif defined(IOS)
-#include <OpenGLES/ES2/gl.h>
-#include <OpenGLES/ES2/glext.h>
-typedef char GLchar;
-#else
-#include <GL/glew.h>
-#if defined(__APPLE__)
-#include <OpenGL/gl.h>
-#else
-#include <GL/gl.h>
-#endif
-#endif
+#include "../native/gfx/gl_common.h"
 
 extern const GLint aLookup[];
 extern const GLint bLookup[];
