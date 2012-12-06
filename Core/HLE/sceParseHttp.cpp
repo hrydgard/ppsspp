9bcb67f90b55d3462e69902bcbacc6c435541096
TMaul <kja@blueyonder.co.uk>
2012-12-06 15:24:25 +0000

    Add some missing sce* Functions
    Add the missing functions to sceHttp
    Create sceOpenPSID and add what jpcsp is doing
    Create sceParseHttp.cpp
    Create sceParseUri.cpp
    Create sceSsl.cpp and add what jpcsp is doing
    reindent some files.
    
    in an attempt to try and get a few games doing more.

---
+++ Core/HLE/sceParseHttp.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) 2012- PPSSPP Project.
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 2.0 or later versions.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License 2.0 for more details.
+
+// A copy of the GPL 2.0 should have been included with the program.
+// If not, see http://www.gnu.org/licenses/
+
+// Official git repository and contact information can be found at
+// https://github.com/hrydgard/ppsspp and http://www.ppsspp.org/.
+
+#include "HLE.h"
+
+#include "sceParseHttp.h"
+
+const HLEFunction sceParseHttp [] = 
+{
+	{0x8077A433, 0, "sceParseHttpStatusLineFunction"},
+	{0xAD7BFDEF, 0, "sceParseHttpResponseHeaderFunction"},
+};
+
+void Register_sceParseHttp()
+{
+	RegisterModule("sceParseHttp", ARRAY_SIZE(sceParseHttp), sceParseHttp);
+}
