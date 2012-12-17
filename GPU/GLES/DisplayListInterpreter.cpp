8db1cef503d34d8c82fa07e1f8d7c83c9a8e5a13
Henrik Rydgard <hrydgard@gmail.com>
2012-12-17 19:04:19 +0100

    Fix GPU block transfer

--- GPU/GLES/DisplayListInterpreter.cpp
+++ GPU/GLES/DisplayListInterpreter.cpp
@@ -1182,8 +1182,8 @@ void GLES_GPU::DoBlockTransfer()
 	u32 srcBasePtr = (gstate.transfersrc & 0xFFFFFF) | ((gstate.transfersrcw & 0xFF0000) << 8);
 	u32 srcStride = gstate.transfersrcw & 0x3FF;
 
-	u32 dstBasePtr = (gstate.transfersrc & 0xFFFFFF) | ((gstate.transfersrcw & 0xFF0000) << 8);
-	u32 dstStride = gstate.transfersrcw & 0x3FF;
+	u32 dstBasePtr = (gstate.transferdst & 0xFFFFFF) | ((gstate.transferdstw & 0xFF0000) << 8);
+	u32 dstStride = gstate.transferdstw & 0x3FF;
 
 	int srcX = gstate.transfersrcpos & 0x3FF;
 	int srcY = (gstate.transfersrcpos >> 10) & 0x3FF;
