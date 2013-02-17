8ba2769d5ebe8265bbce4c386da59fc12678192a
Sacha <xsacha@gmail.com>
2013-02-18 00:04:44 +1000

    iOS Objective-C code dump. ViewController courtesy of rock88. Made modifications for simplification and code-style. Still missing some files (eg. images). Also, CMake is not set up correctly yet.

--- ios/ios.toolchain.cmake
+++ ios/ios.toolchain.cmake
@@ -141,7 +141,7 @@ set (CMAKE_OSX_SYSROOT ${CMAKE_IOS_SDK_ROOT} CACHE PATH "Sysroot used for iOS su
 # set the architecture for iOS 
 # NOTE: Currently both ARCHS_STANDARD_32_BIT and ARCHS_UNIVERSAL_IPHONE_OS set armv7 only, so set both manually
 if (${IOS_PLATFORM} STREQUAL "OS")
-	set (IOS_ARCH armv6 armv7)
+	set (IOS_ARCH armv7)
 else (${IOS_PLATFORM} STREQUAL "OS")
 	set (IOS_ARCH i386)
 endif (${IOS_PLATFORM} STREQUAL "OS")
