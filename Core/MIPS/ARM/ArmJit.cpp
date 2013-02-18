9d490a8b508004ea04d86f085b95dde00d49c663
Unknown W. Brackets <checkins@unknownbrackets.org>
2013-02-18 08:03:45 -0800

    Proper ARM buildfix.

--- Core/MIPS/ARM/ArmJit.cpp
+++ Core/MIPS/ARM/ArmJit.cpp
@@ -15,6 +15,7 @@
 // Official git repository and contact information can be found at
 // https://github.com/hrydgard/ppsspp and http://www.ppsspp.org/.
 
+#include "Common/ChunkFile.h"
 #include "../../Core.h"
 #include "../../CoreTiming.h"
 #include "../MIPS.h"
