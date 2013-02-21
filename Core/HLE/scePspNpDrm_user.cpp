d464e667492e20c5de85f8a9a090ae2b171590e4
vakko <vakko@vakko.(none)>
2013-02-21 11:03:41 +0200

    implementing decryption

--- Core/HLE/scePspNpDrm_user.cpp
+++ Core/HLE/scePspNpDrm_user.cpp
@@ -1,7 +1,54 @@
 #include "HLE.h"
 
+int sceNpDrmSetLicenseeKey()
+{
+	ERROR_LOG(HLE, "UNIMPL sceNpDrmSetLicenseeKey");
+	return 0;
+}
 
-const HLEFunction sce[] =
+int sceNpDrmClearLicenseeKey()
+{
+	ERROR_LOG(HLE, "UNIMPL sceNpDrmClearLicenseeKey");
+	return 0;
+}
+
+int sceNpDrmRenameCheck()
+{
+	ERROR_LOG(HLE, "UNIMPL sceNpDrmRenameCheck");
+	return 0;
+}
+
+int sceNpDrmEdataSetupKey()
+{
+	ERROR_LOG(HLE, "UNIMPL sceNpDrmEdataSetupKey");
+	return 0;
+}
+
+int sceNpDrmEdataGetDataSize()
+{
+	ERROR_LOG(HLE, "UNIMPL sceNpDrmEdataGetDataSize");
+	return 0;
+}
+
+int sceNpDrmOpen()
+{
+	ERROR_LOG(HLE, "UNIMPL sceNpDrmOpen");
+	return 0;
+}
+
+int sceKernelLoadModuleNpDrm()
+{
+	ERROR_LOG(HLE, "UNIMPL sceKernelLoadModuleNpDrm");
+	return 0;
+}
+
+int sceKernelLoadExecNpDrm()
+{
+	ERROR_LOG(HLE, "UNIMPL sceKernelLoadExecNpDrm");
+	return 0;
+}
+
+const HLEFunction sceNpDrm[] =
 { 
 	{0xA1336091, 0, "sceNpDrmSetLicenseeKey"},
 	{0x9B745542, 0, "sceNpDrmClearLicenseeKey"},
