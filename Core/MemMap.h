5fb4d5d48a57916b6c56a5c8dac212e400ef70a6
Henrik Rydgard <hrydgard@gmail.com>
2013-03-16 09:15:47 +0100

    Set default load address to 0x88004000 according to tpunix

--- Core/MemMap.h
+++ Core/MemMap.h
@@ -66,7 +66,7 @@ inline u32 PSP_GetKernelMemoryEnd()  { return 0x08400000;}
 inline u32 PSP_GetUserMemoryBase() { return 0x08800000;}
 inline u32 PSP_GetUserMemoryEnd()  { return 0x0A000000;}
 
-inline u32 PSP_GetDefaultLoadAddress() { return 0x08800000;}
+inline u32 PSP_GetDefaultLoadAddress() { return 0x08804000;}
 //inline u32 PSP_GetDefaultLoadAddress() { return 0x0898dab0;}
 inline u32 PSP_GetVidMemBase() { return 0x04000000;}
 
