49befb3d41b756720084e26adae9ec4afd8d92db
raven02 <jacky.kktsui@yahoo.com.hk>
2013-03-16 20:35:51 +0800

    Flip in throughmode for HW T&L

--- GPU/GLES/VertexShaderGenerator.cpp
+++ GPU/GLES/VertexShaderGenerator.cpp
@@ -408,7 +408,10 @@ void GenerateVertexShader(int prim, char *buffer) {
 				break;
 			}
 			if (flipV)
-				WRITE(p, "  v_texcoord.y = 1.0 - v_texcoord.y * 2.0;\n");
+				if (gstate.isModeThrough())	
+					WRITE(p, "  v_texcoord.y = 1.0 - v_texcoord.y;\n");
+				else
+					WRITE(p, "  v_texcoord.y = 1.0 - v_texcoord.y * 2.0;\n");
 		}
 
 		// Compute fogdepth
