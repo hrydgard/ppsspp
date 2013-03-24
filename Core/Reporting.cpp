d1af119c6f0285df7ad1144683440d584aba1245
Unknown W. Brackets <checkins@unknownbrackets.org>
2013-03-24 10:18:22 -0700

    Buildfix for Mac/etc.

--- Core/Reporting.cpp
+++ Core/Reporting.cpp
@@ -15,6 +15,8 @@
 // Official git repository and contact information can be found at
 // https://github.com/hrydgard/ppsspp and http://www.ppsspp.org/.
 
+#include "Core/Reporting.h"
+
 #include "Common/StdThread.h"
 #include "Core/Config.h"
 #include "Core/System.h"
