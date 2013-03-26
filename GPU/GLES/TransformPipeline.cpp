5495627038339049a452c1e9301d350fea6a5a56
raven02 <jacky.kktsui@yahoo.com.hk>
2013-03-26 10:51:00 +0800

    Hack render-to-rexture height in SW mode

--- GPU/GLES/TransformPipeline.cpp
+++ GPU/GLES/TransformPipeline.cpp
@@ -650,7 +650,7 @@ void TransformDrawEngine::SoftwareTransformAndDraw(
 			if (throughmode)
 				transformed[index].v = 1.0f - transformed[index].v;
 			else 
-				transformed[index].v = 1.0f - transformed[index].v * 2.0f;
+				transformed[index].v = 1.1f - transformed[index].v * 2.0f;
 		}
 		for (int i = 0; i < 4; i++) {
 			transformed[index].color0[i] = c0[i] * 255.0f;
