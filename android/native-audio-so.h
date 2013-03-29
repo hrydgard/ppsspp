4f5a60d82b38dbb609084782211a272b9e4e53fb
Henrik Rydgard <hrydgard@gmail.com>
2013-03-29 20:04:36 +0100

    Buildfix

--- android/native-audio-so.h
+++ android/native-audio-so.h
@@ -2,7 +2,7 @@
 
 // Header for dynamic loading
 
-typedef void (*AndroidAudioCallback)(short *buffer, int num_samples);
+typedef int (*AndroidAudioCallback)(short *buffer, int num_samples);
 
 typedef bool (*OpenSLWrap_Init_T)(AndroidAudioCallback cb);
 typedef void (*OpenSLWrap_Shutdown_T)();
