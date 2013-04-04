9e5b512edb67fa6d63d420e4f60faf93ca8c09b6
Sacha <xsacha@gmail.com>
2013-04-04 11:58:10 +1000

    Get Qt running and aware of UIState. Update native.

--- Qt/mainwindow.cpp
+++ Qt/mainwindow.cpp
@@ -194,7 +194,7 @@ void MainWindow::Update()
 
 void MainWindow::UpdateMenus()
 {
-	bool enable = false;
+	bool enable = globalUIState == UISTATE_MENU;
 	ui->action_FileLoad->setEnabled(enable);
 	ui->action_FileClose->setEnabled(!enable);
 	ui->action_FileSaveStateFile->setEnabled(!enable);
@@ -210,10 +210,9 @@ void MainWindow::UpdateMenus()
 	ui->action_DebugMemoryViewTexture->setEnabled(!enable);
 	ui->action_DebugDisplayList->setEnabled(!enable);
 
-	enable = !Core_IsStepping() ? false : true;
-	ui->action_EmulationRun->setEnabled(false);
-	ui->action_EmulationPause->setEnabled(true);
-	ui->action_EmulationReset->setEnabled(true);
+	ui->action_EmulationRun->setEnabled(Core_IsStepping() || globalUIState == UISTATE_PAUSEMENU);
+	ui->action_EmulationPause->setEnabled(globalUIState == UISTATE_INGAME);
+	ui->action_EmulationReset->setEnabled(globalUIState == UISTATE_INGAME);
 
 	// checking
 	ui->action_EmulationRunLoad->setChecked(g_Config.bAutoRun);
