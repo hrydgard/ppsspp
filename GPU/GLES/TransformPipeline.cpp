8cc3f01b7b87e2b7e7d37432e7effe9810cad4cb
BeaR <cinaera@web.de>
2013-04-16 19:22:18 +0200

    Use correct state for lmode

--- GPU/GLES/TransformPipeline.cpp
+++ GPU/GLES/TransformPipeline.cpp
@@ -583,7 +583,7 @@ void TransformDrawEngine::SoftwareTransformAndDraw(
 				} else {
 					// Summed color into c0
 					for (int j = 0; j < 4; j++) {
-						c0[j] = c0[j] + litColor1[j];
+						c0[j] = ((c0[j] + litColor1[j]) > 1.0f) ? 1.0f : (c0[j] + litColor1[j]);
 					}
 				}
 			} else {
