ad74735f60f3c2d031f5dc628f1057bb9516338a
mgaver <mingi2007@gmail.com>
2013-04-22 17:50:02 +0900

    Update PSPMsgDialog.h

--- Core/Dialog/PSPMsgDialog.h
+++ Core/Dialog/PSPMsgDialog.h
@@ -18,81 +18,167 @@
 #pragma once
 
 #include "PSPDialog.h"
+#include "../Core/MemMap.h"
 
-#define SCE_UTILITY_MSGDIALOG_OPTION_ERROR				0x00000000
-#define SCE_UTILITY_MSGDIALOG_OPTION_TEXT				0x00000001
-#define SCE_UTILITY_MSGDIALOG_OPTION_NOSOUND			0x00000002
-#define SCE_UTILITY_MSGDIALOG_OPTION_YESNO				0x00000010
-#define SCE_UTILITY_MSGDIALOG_OPTION_OK					0x00000020
-#define SCE_UTILITY_MSGDIALOG_OPTION_NOCANCEL			0x00000080
-#define SCE_UTILITY_MSGDIALOG_OPTION_DEFAULT_NO			0x00000100
 
-#define SCE_UTILITY_MSGDIALOG_SIZE_V1					572
-#define SCE_UTILITY_MSGDIALOG_SIZE_V2					580
-#define SCE_UTILITY_MSGDIALOG_SIZE_V3					708
 
-#define SCE_UTILITY_MSGDIALOG_DEBUG_OPTION_CODED		0x000001B3 // OR of all options coded to display warning
-
-#define SCE_UTILITY_MSGDIALOG_ERROR_BADOPTION			0x80110501
-#define SCE_UTILITY_MSGDIALOG_ERROR_ERRORCODEINVALID	0x80110502
-
-struct pspMessageDialog
+/**
+* Enumeration for input language
+*/
+enum SceUtilityOskInputLanguage
 {
-	pspUtilityDialogCommon common;
+	PSP_UTILITY_OSK_LANGUAGE_DEFAULT =		0x00,
+	PSP_UTILITY_OSK_LANGUAGE_JAPANESE =		0x01,
+	PSP_UTILITY_OSK_LANGUAGE_ENGLISH =		0x02,
+	PSP_UTILITY_OSK_LANGUAGE_FRENCH =		0x03,
+	PSP_UTILITY_OSK_LANGUAGE_SPANISH =		0x04,
+	PSP_UTILITY_OSK_LANGUAGE_GERMAN =		0x05,
+	PSP_UTILITY_OSK_LANGUAGE_ITALIAN =		0x06,
+	PSP_UTILITY_OSK_LANGUAGE_DUTCH =		0x07,
+	PSP_UTILITY_OSK_LANGUAGE_PORTUGESE =	0x08,
+	PSP_UTILITY_OSK_LANGUAGE_RUSSIAN =		0x09,
+	PSP_UTILITY_OSK_LANGUAGE_KOREAN =		0x0a
+};
+
+/**
+* Enumeration for OSK internal state
+*/
+enum SceUtilityOskState
+{
+	PSP_UTILITY_OSK_DIALOG_NONE = 0,	/**< No OSK is currently active */
+	PSP_UTILITY_OSK_DIALOG_INITING,		/**< The OSK is currently being initialized */
+	PSP_UTILITY_OSK_DIALOG_INITED,		/**< The OSK is initialised */
+	PSP_UTILITY_OSK_DIALOG_VISIBLE,		/**< The OSK is visible and ready for use */
+	PSP_UTILITY_OSK_DIALOG_QUIT,		/**< The OSK has been cancelled and should be shut down */
+	PSP_UTILITY_OSK_DIALOG_FINISHED		/**< The OSK has successfully shut down */	
+};
+
+/**
+* Enumeration for OSK field results
+*/
+enum SceUtilityOskResult
+{
+	PSP_UTILITY_OSK_RESULT_UNCHANGED =	0,
+	PSP_UTILITY_OSK_RESULT_CANCELLED,
+	PSP_UTILITY_OSK_RESULT_CHANGED
+};
+
+/**
+* Enumeration for input types (these are limited by initial choice of language)
+*/
+enum SceUtilityOskInputType
+{
+	PSP_UTILITY_OSK_INPUTTYPE_ALL =						0x00000000,
+	PSP_UTILITY_OSK_INPUTTYPE_LATIN_DIGIT =				0x00000001,
+	PSP_UTILITY_OSK_INPUTTYPE_LATIN_SYMBOL =			0x00000002,
+	PSP_UTILITY_OSK_INPUTTYPE_LATIN_LOWERCASE =			0x00000004,
+	PSP_UTILITY_OSK_INPUTTYPE_LATIN_UPPERCASE =			0x00000008,
+	PSP_UTILITY_OSK_INPUTTYPE_JAPANESE_DIGIT =			0x00000100,
+	PSP_UTILITY_OSK_INPUTTYPE_JAPANESE_SYMBOL =			0x00000200,
+	PSP_UTILITY_OSK_INPUTTYPE_JAPANESE_LOWERCASE =		0x00000400,
+	PSP_UTILITY_OSK_INPUTTYPE_JAPANESE_UPPERCASE =		0x00000800,
+	// http://en.wikipedia.org/wiki/Hiragana
+	PSP_UTILITY_OSK_INPUTTYPE_JAPANESE_HIRAGANA =		0x00001000,
+	// http://en.wikipedia.org/wiki/Katakana
+	// Half-width Katakana
+	PSP_UTILITY_OSK_INPUTTYPE_JAPANESE_HALF_KATAKANA =	0x00002000,
+	PSP_UTILITY_OSK_INPUTTYPE_JAPANESE_KATAKANA =		0x00004000,
+	// http://en.wikipedia.org/wiki/Kanji
+	PSP_UTILITY_OSK_INPUTTYPE_JAPANESE_KANJI =			0x00008000,
+	PSP_UTILITY_OSK_INPUTTYPE_RUSSIAN_LOWERCASE =		0x00010000,
+	PSP_UTILITY_OSK_INPUTTYPE_RUSSIAN_UPPERCASE =		0x00020000,
+	PSP_UTILITY_OSK_INPUTTYPE_KOREAN =					0x00040000,
+	PSP_UTILITY_OSK_INPUTTYPE_URL =						0x00080000
+};
+
+/**
+* OSK Field data
+*/
+typedef struct _SceUtilityOskData
+{
+	/** Unknown. Pass 0. */
+	int unk_00;
+	/** Unknown. Pass 0. */
+	int unk_04;
+	/** One of ::SceUtilityOskInputLanguage */
+	int language;
+	/** Unknown. Pass 0. */
+	int unk_12;
+	/** One or more of ::SceUtilityOskInputType (types that are selectable by pressing SELECT) */
+	int inputtype;
+	/** Number of lines */
+	int lines;
+	/** Unknown. Pass 0. */
+	int unk_24;
+	/** Description text */
+	u32 descPtr;
+	/** Initial text */
+	u32 intextPtr;
+	// Length, in unsigned shorts, including the terminator.
+	u32 outtextlength;
+	/** Pointer to the output text */
+	u32 outtextPtr;
+	/** Result. One of ::SceUtilityOskResult */
 	int result;
-	int type;
-	unsigned int errorNum;
-	char string[512];
-	// End of request V1 (Size 572)
-	unsigned int options;
-	unsigned int buttonPressed;
-	// End of request V2 (Size 580)
-	int unknown[32];
-	// End of request V3 (Size 708)
+	// Number of characters to allow, not including terminator (if less than outtextlength - 1.)
+	u32 outtextlimit;
+
+} SceUtilityOskData;
+
+// Parameters to sceUtilityOskInitStart
+struct SceUtilityOskParams
+{
+	pspUtilityDialogCommon base;
+	// Number of fields.
+	int fieldCount;
+	// Pointer to an array of fields (see SceUtilityOskData.)
+	u32 fieldPtr;
+	SceUtilityOskState state;
+	// Maybe just padding?
+	int unk_60;
+
+};
+
+// Internal enum, not from PSP.
+enum OskKeyboardDisplay
+{
+	OSK_KEYBOARD_LATIN_LOWERCASE,
+	OSK_KEYBOARD_LATIN_UPPERCASE,
+	OSK_KEYBOARD_HIRAGANA,
+	OSK_KEYBOARD_KATAKANA,
+/*	OSK_KEYBOARD_KOREAN_LOWERCASE,
+	OSK_KEYBOARD_KOREAN_UPPERCASE,
+*/
+	// TODO: Something to do native?
+	OSK_KEYBOARD_COUNT
 };
 
 
-class PSPMsgDialog: public PSPDialog {
+class PSPOskDialog: public PSPDialog {
 public:
-	PSPMsgDialog();
-	virtual ~PSPMsgDialog();
+	PSPOskDialog();
+	virtual ~PSPOskDialog();
 
-	virtual int Init(unsigned int paramAddr);
+	virtual int Init(u32 oskPtr);
 	virtual int Update();
-	virtual int Shutdown();
 	virtual void DoState(PointerWrap &p);
-	int Abort();
+private:
+	void ConvertUCS2ToUTF8(std::string& _string, const u32 em_address);
+	void ConvertUCS2ToUTF8(std::string& _string, wchar_t* input);
+	void RenderKeyboard();
 
-private :
-	void DisplayBack();
-	void DisplayYesNo();
-	void DisplayEnter();
-	void DisplayOk();
+	std::wstring CombinationString(); // for Japanese, Korean
 
-	enum Flags
-	{
-		DS_MSG 				= 0x1,
-		DS_ERRORMSG 		= 0x2,
-		DS_YESNO 			= 0x4,
-		DS_DEFNO 			= 0x8,
-		DS_OK 				= 0x10,
-		DS_VALIDBUTTON 		= 0x20,
-		DS_CANCELBUTTON 	= 0x40,
-		DS_NOSOUND			= 0x80,
-		DS_ERROR 			= 0x100
-	};
+	u32 FieldMaxLength();
 
-	u32 flag;
+	SceUtilityOskParams *oskParams;
+	SceUtilityOskData oskData;
+	std::string oskDesc;
+	std::string oskIntext;
+	std::string oskOuttext;
 
-	pspMessageDialog messageDialog;
-	int messageDialogAddr;
-
-	char msgText[512];
-	int yesnoChoice;
-
-	int okButtonImg;
-	int cancelButtonImg;
-	int okButtonFlag;
-	int cancelButtonFlag;
+	int selectedChar;
+	std::wstring inputChars;
+	OskKeyboardDisplay currentKeyboard;
+	bool isCombinated;
 };
-
