327bf6f7a2d04f19aab6c1938cf0a44cee10ea1a
oioitff <oioitff@gmail.com>
2013-05-09 00:16:44 +0800

    A foolish bug fix

--- Core/HLE/sceAtrac.cpp
+++ Core/HLE/sceAtrac.cpp
@@ -897,7 +897,7 @@ int _AtracSetData(Atrac *atrac, u32 buffer, u32 bufferSize)
 	{
 		Memory::LASTESTFILECACHE *cache = Memory::lastestAccessFile.findmatchcache(Memory::GetPointer(buffer));
 		if ((atrac->first.size >= atrac->first.filesize) || 
-			(atrac->first.filesize < 0x19000 && checkAudioFileExist(Memory::GetPointer(buffer)))) {
+			(atrac->first.filesize >= 0x19000 && checkAudioFileExist(Memory::GetPointer(buffer)))) {
 			addAtrac3Audio(Memory::GetPointer(buffer), atrac->first.filesize, atracID);
 		} else if (cache) {
 			PGD_DESC pgdinfo = cache->pgd_info;
