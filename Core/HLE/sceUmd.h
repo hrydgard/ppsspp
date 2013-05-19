d69fe6426e6dff051f93fcd342b397572bf83fe2
raven02 <jacky.kktsui@yahoo.com.hk>
2013-05-19 14:08:07 +0800

    More UMD state

--- Core/HLE/sceUmd.h
+++ Core/HLE/sceUmd.h
@@ -18,12 +18,13 @@
 #pragma once
 
 enum pspUmdState { 
+	PSP_UMD_INIT = 0x00,
 	PSP_UMD_NOT_PRESENT = 0x01,
 	PSP_UMD_PRESENT = 0x02,
 	PSP_UMD_CHANGED = 0x04,
-	PSP_UMD_INITING = 0x08, 
-	PSP_UMD_INITED = 0x10,
-	PSP_UMD_READY = 0x20 
+	PSP_UMD_NOT_READY = 0x08,
+	PSP_UMD_READY = 0x10, 
+	PSP_UMD_READABLE = 0x20
 };
 
 void __UmdInit();
