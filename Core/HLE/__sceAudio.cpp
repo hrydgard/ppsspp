0e77b44130f89ec0f11ebea035eb47061ceca05a
Henrik Rydgard <hrydgard@gmail.com>
2013-05-23 13:50:31 +0200

    Buildfix

--- Core/HLE/__sceAudio.cpp
+++ Core/HLE/__sceAudio.cpp
@@ -45,7 +45,7 @@ const int audioHostIntervalUs = (int)(1000000ULL * hostAttemptBlockSize / hwSamp
 // High and low watermarks, basically.  For perfect emulation, the correct values are 0 and 1, respectively.
 // TODO: Tweak
 const int chanQueueMaxSizeFactor = 2;
-const int chanQueueMinSizeFactor = 1
+const int chanQueueMinSizeFactor = 1;
 
 
 FixedSizeQueue<s16, hostAttemptBlockSize * 16> outAudioQueue;
