c115a6f36ca2df3c4c6db92037b126c9f04b735b
Unknown W. Brackets <checkins@unknownbrackets.org>
2013-05-24 22:10:12 -0700

    Don't report alphatest masks for always/never.

--- GPU/GLES/DisplayListInterpreter.cpp
+++ GPU/GLES/DisplayListInterpreter.cpp
@@ -787,7 +787,7 @@ void GLES_GPU::ExecuteOp(u32 op, u32 diff) {
 
 	case GE_CMD_ALPHATEST:
 #ifndef USING_GLES2
-		if (((data >> 16) & 0xFF) != 0xFF && data != 0)
+		if (((data >> 16) & 0xFF) != 0xFF && (data & 7) > 1)
 			WARN_LOG_REPORT_ONCE(alphatestmask, HLE, "Unsupported alphatest mask: %02x", (data >> 16) & 0xFF);
 #endif
 	case GE_CMD_COLORREF:
