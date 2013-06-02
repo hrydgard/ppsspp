f825a076504255c132cfcc7730a2d99e46c68a69
Sacha <xsacha@gmail.com>
2013-06-02 22:26:51 +1000

    Move C++11 functions to std namespace.

--- Common/ThreadPools.h
+++ Common/ThreadPools.h
@@ -6,10 +6,10 @@ class GlobalThreadPool {
 public:
 	// will execute slices of "loop" from "lower" to "upper"
 	// in parallel on the global thread pool
-	static void Loop(const function<void(int,int)>& loop, int lower, int upper);
+	static void Loop(const std::function<void(int,int)>& loop, int lower, int upper);
 
 private:
-	static shared_ptr<ThreadPool> pool;
+	static std::shared_ptr<ThreadPool> pool;
 	static bool initialized;
 	static void Inititialize();
 };
