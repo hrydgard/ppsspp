104a3a316b398a7ad4c4dd8e19fbc22b12897a0b
Sacha <xsacha@gmail.com>
2013-06-10 00:29:08 +1000

    Revert extra regs allocation. Causes some crashes with ffmpeg.

--- Core/MIPS/ARM/ArmRegCacheFPU.cpp
+++ Core/MIPS/ARM/ArmRegCacheFPU.cpp
@@ -54,7 +54,7 @@ static const ARMReg *GetMIPSAllocationOrder(int &count) {
 		S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, S12, S13, S14, S15,
 		S16, S17, S18, S19, S20, S21, S22, S23, S24, S25, S26, S27, S28, S29, S30, S31
 	};
-	if (cpu_info.bNEON) {
+	if (false && cpu_info.bNEON) {
 		count = sizeof(allocationOrderNEON) / sizeof(const int);
 		return allocationOrderNEON;
 	} else {
