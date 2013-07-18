c6953aed65f8d88dd6d208f64b9a8d3d524bb22a
raven02 <jacky.kktsui@yahoo.com.hk>
2013-07-19 06:53:47 +0800

    Use new variables bVsync,iSFXVolume

--- Core/Config.h
+++ Core/Config.h
@@ -71,7 +71,7 @@ public:
 	bool bPartialStretch;
 #endif
 	bool bStretchToDisplay;
-	int iVSyncInterval;
+	bool bVSync;
 	int iFrameSkip;
 
 	int iWindowX;
@@ -100,7 +100,7 @@ public:
 	// Sound
 	bool bEnableSound;
 	bool bEnableAtrac3plus;
-	int iSEVolume;
+	int iSFXVolume;
 	int iBGMVolume;
 
 	// UI
@@ -109,7 +109,6 @@ public:
 	bool bShowAnalogStick;
 	int iShowFPSCounter;
 	bool bShowDebugStats;
-	bool bLargeControls;
 	bool bAccelerometerToAnalogHoriz;
 
 	// Control
