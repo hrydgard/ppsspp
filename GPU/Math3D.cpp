35b5595da0ddaadb671f7a48d1925cccd272e48c
neobrain <neobrainx@gmail.com>
2013-07-18 09:48:02 +0200

    GPU/Math3D: Support converting Vec3Ref and Vec4Ref objects to u32 color values.

--- GPU/Math3D.cpp
+++ GPU/Math3D.cpp
@@ -34,6 +34,20 @@ Vec3<int> Vec3<int>::FromRGB(unsigned int rgb)
 }
 
 template<>
+unsigned int Vec3Ref<float>::ToRGB() const
+{
+	return ((unsigned int)(r*255.f)) +
+			((unsigned int)(g*255.f*256.f)) +
+			((unsigned int)(b*255.f*256.f*256.f));
+}
+
+template<>
+unsigned int Vec3Ref<int>::ToRGB() const
+{
+	return (r&0xFF) | ((g&0xFF)<<8) | ((b&0xFF)<<16);
+}
+
+template<>
 Vec4<float> Vec4<float>::FromRGBA(unsigned int rgba)
 {
 	return Vec4((rgba & 0xFF) * (1.0f/255.0f),
@@ -49,4 +63,19 @@ Vec4<int> Vec4<int>::FromRGBA(unsigned int rgba)
 				(rgba >> 8) & 0xFF,
 				(rgba >> 16) & 0xFF,
 				(rgba >> 24) & 0xFF);
+}
+
+template<>
+unsigned int Vec4Ref<float>::ToRGBA() const
+{
+	return ((unsigned int)(r*255.f)) +
+			((unsigned int)(g*255.f*256.f)) +
+			((unsigned int)(b*255.f*256.f*256.f)) +
+			((unsigned int)(a*255.f*256.f*256.f*256.f));
+}
+
+template<>
+unsigned int Vec4Ref<int>::ToRGBA() const
+{
+	return (r&0xFF) | ((g&0xFF)<<8) | ((b&0xFF)<<16) | ((a&0xFF)<<24);
 }
