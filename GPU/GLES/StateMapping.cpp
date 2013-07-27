b35e89f6467d31830c11cf0a69931e63b5a18fe5
raven02 <jacky.kktsui@yahoo.com.hk>
2013-07-27 18:45:48 +0800

    Fix rear mirror in Ridge Racer 2

--- GPU/GLES/StateMapping.cpp
+++ GPU/GLES/StateMapping.cpp
@@ -362,8 +362,9 @@ void TransformDrawEngine::ApplyDrawState(int prim) {
 		vpWidth *= renderWidthFactor;
 		vpHeight *= renderHeightFactor;
 
+		vpX0 = (vpXb - offsetX - fabsf(vpXa)) * renderWidthFactor;
 		// Flip vpY0 to match the OpenGL coordinate system.
-		vpY0 = renderHeight - (vpYb - offsetY + fabsf(vpYa)) * renderHeightFactor;
+		vpY0 = renderHeight - (vpYb - offsetY + fabsf(vpYa)) * renderHeightFactor;		
 		
 		glstate.viewport.set(vpX0 + renderX, vpY0 + renderY, vpWidth, vpHeight);
 		// Sadly, as glViewport takes integers, we will not be able to support sub pixel offsets this way. But meh.
