8ff2a1bb35415ec8db519734a2b4e0163f280887
raven02 <jacky.kktsui@yahoo.com.hk>
2013-08-17 22:05:50 +0800

    softgpu : fix v_color0 to use MaterialAmbient

--- GPU/Software/TransformUnit.cpp
+++ GPU/Software/TransformUnit.cpp
@@ -143,7 +143,7 @@ static VertexData ReadVertex(VertexReader& vreader)
 		vreader.ReadColor0(col);
 		vertex.color0 = Vec4<int>(col[0]*255, col[1]*255, col[2]*255, col[3]*255);
 	} else {
-		vertex.color0 = Vec4<int>(gstate.materialdiffuse&0xFF, (gstate.materialdiffuse>>8)&0xFF, (gstate.materialdiffuse>>16)&0xFF, gstate.materialalpha&0xFF);
+		vertex.color0 = Vec4<int>(gstate.getMaterialAmbientR(), gstate.getMaterialAmbientG(), gstate.getMaterialAmbientB(), gstate.getMaterialAmbientA());
 	}
 
 	if (vreader.hasColor1()) {
