8c4db99435bb996d89ad18cd51816f47e33e2b2f
Unknown W. Brackets <checkins@unknownbrackets.org>
2013-08-18 20:17:33 -0700

    iOS buildfix.

--- ios/main.mm
+++ ios/main.mm
@@ -1,8 +1,15 @@
-// main.m boilerplate
+// main.mm boilerplate
 
 #import <UIKit/UIKit.h>
+#import <string>
 
 #import "AppDelegate.h"
+
+std::string System_GetName()
+{
+	// TODO: iPad/etc.?
+	return "iOS:";
+}
 
 int main(int argc, char *argv[])
 {
