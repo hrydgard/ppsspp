ffb71e3e9980b6590d146a1bed44410cc39adf53
sum2012 <consignpo@gmail.com>
2013-08-19 22:54:06 +0800

    Add more translate

--- UI/GameSettingsScreen.cpp
+++ UI/GameSettingsScreen.cpp
@@ -226,7 +226,7 @@ void GameSettingsScreen::CreateViews() {
 	tabHolder->AddTab(ms->T("Graphics"), graphicsSettingsScroll);
 
 	graphicsSettings->Add(new ItemHeader(gs->T("Rendering Mode")));
-	static const char *renderingMode[] = { "Non-Buffered Rendering", "Buffered Rendering", "Read Framebuffers To Memory(CPU)", "Read Framebuffers To Memory(GPU)"};
+	static const char *renderingMode[] = { gs->T("Non-Buffered Rendering"), gs->T("Buffered Rendering"), gs->T("Read Framebuffers To Memory(CPU)"), gs->T("Read Framebuffers To Memory(GPU)")};
 	graphicsSettings->Add(new PopupMultiChoice(&g_Config.iRenderingMode, gs->T("Mode"), renderingMode, 0, 4, gs, screenManager()));
 
 	graphicsSettings->Add(new CheckBox(&g_Config.bAntiAliasing, gs->T("Anti-Aliasing")));
