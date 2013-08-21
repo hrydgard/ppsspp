96b2bbdc2e2835850d53d09996ce39f323ac5512
Sacha <xsacha@gmail.com>
2013-08-21 12:30:13 +1000

    Add Russian to LowMem. Modify script to automatically build lowmem atlas at same time.

--- buildatlas.sh
+++ buildatlas.sh
@@ -1,1 +1,2 @@
 ./native/tools/build/atlastool atlasscript.txt ui 8888 && cp ui_atlas.zim assets && mv ui_atlas.zim android/assets && mv ui_atlas.cpp ui_atlas.h UI
+./native/tools/build/atlastool atlasscript_lowmem.txt ui 8888 && mv ui_atlas.zim assets/ui_atlas_lowmem.zim && mv ui_atlas.cpp UI/ui_atlas_lowmem.cpp && rm ui_atlas.h
