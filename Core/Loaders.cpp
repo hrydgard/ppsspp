ec3f336f8a383c04361facec98c9b8b3b5fa1c3d
wuspring <wuspring@users.noreply.github.com>
2013-09-03 13:20:48 +0800

    Update Loaders.cpp for shorten key

--- Core/Loaders.cpp
+++ Core/Loaders.cpp
@@ -226,22 +226,22 @@ bool LoadFile(std::string &filename, std::string *error_string) {
 
 	case FILETYPE_ARCHIVE_RAR:
 #ifdef WIN32
-		*error_string = "File is compressed (RAR).\nPlease decompress first (try WinRAR)";
+		*error_string = "RAR file (requires WINRAR)";
 #else
-		*error_string = "File is compressed (RAR).\nPlease decompress first (try UnRAR)";
+		*error_string = "RAR file (requires UnRAR)";
 #endif
 		break;
 
 	case FILETYPE_ARCHIVE_ZIP:
 #ifdef WIN32
-		*error_string = "File is compressed (ZIP).\nPlease decompress first (try WinRAR)";
+		*error_string = "ZIP file (requires WINRAR)";
 #else
-		*error_string = "File is compressed (ZIP).\nPlease decompress first (try UnRAR)";
+		*error_string = "ZIP file (requires UnRAR)";
 #endif
 		break;
 
 	case FILETYPE_ISO_MODE2:
-		*error_string = "File is a MODE2 image. Are you trying to open a PSX game?";
+		*error_string = "PSX game detected.";
 		break;
 
 	case FILETYPE_NORMAL_DIRECTORY:
