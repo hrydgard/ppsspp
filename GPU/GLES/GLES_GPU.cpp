06feb621df5d724608c67fabd3b49eeb1e4cc603
raven02 <jacky.kktsui@yahoo.com.hk>
2013-09-04 16:38:39 +0800

    Add memory check for srcBasePtr & dstBasePtr

--- GPU/GLES/GLES_GPU.cpp
+++ GPU/GLES/GLES_GPU.cpp
@@ -1350,7 +1350,17 @@ void GLES_GPU::DoBlockTransfer() {
 	int bpp = gstate.getTransferBpp();
 
 	DEBUG_LOG(G3D, "Block transfer: %08x/%x -> %08x/%x, %ix%ix%i (%i,%i)->(%i,%i)", srcBasePtr, srcStride, dstBasePtr, dstStride, width, height, bpp, srcX, srcY, dstX, dstY);
+	
+	if (!Memory::IsValidAddress(srcBasePtr)) {
+		ERROR_LOG_REPORT(G3D, "Bad source transfer address %08x!", srcBasePtr);
+		return;
+	}
 
+	if (!Memory::IsValidAddress(dstBasePtr)) {
+		ERROR_LOG_REPORT(G3D, "Bad destination transfer address %08x!", dstBasePtr);
+		return;
+	}
+	
 	// Do the copy!
 	for (int y = 0; y < height; y++) {
 		const u8 *src = Memory::GetPointer(srcBasePtr + ((y + srcY) * srcStride + srcX) * bpp);
