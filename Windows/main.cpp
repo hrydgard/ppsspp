11a49c05fe74b160803fba811bc2d7273919f16c
The Dax <daxtsu@gmail.com>
2013-09-15 13:12:55 -0400

    Build fix.

--- Windows/main.cpp
+++ Windows/main.cpp
@@ -135,7 +135,7 @@ int WINAPI WinMain(HINSTANCE _hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLin
 
 	// Load config up here, because those changes below would be overwritten
 	// if it's not loaded here first.
-	g_Config.Load(configFilename, controlsConfigFilename);
+	g_Config.Load(configFilename.c_str(), controlsConfigFilename.c_str());
 
 	// The rest is handled in NativeInit().
 	for (int i = 1; i < __argc; ++i)
