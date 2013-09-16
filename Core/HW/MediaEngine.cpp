d13d8ad946532c5f6971d8180037684b6200d0f2
Unknown W. Brackets <checkins@unknownbrackets.org>
2013-09-16 08:35:32 -0700

    Avoid writing to stdout/stderr from ffmpeg.
    This will log some errors in the log even in release.

--- Core/HW/MediaEngine.cpp
+++ Core/HW/MediaEngine.cpp
@@ -192,21 +192,22 @@ int _MpegReadbuffer(void *opaque, uint8_t *buf, int buf_size)
 	return size;
 }
 
-#ifdef _DEBUG
 void ffmpeg_logger(void *, int, const char *format, va_list va_args) {
 	char tmp[1024];
 	vsprintf(tmp, format, va_args);
 	INFO_LOG(ME, "%s", tmp);
 }
-#endif
 
 bool MediaEngine::openContext() {
 #ifdef USE_FFMPEG
 
 #ifdef _DEBUG
 	av_log_set_level(AV_LOG_VERBOSE);
-	av_log_set_callback(&ffmpeg_logger);
+#else
+	av_log_set_level(AV_LOG_ERROR);
 #endif 
+	av_log_set_callback(&ffmpeg_logger);
+
 	if (m_pFormatCtx || !m_pdata)
 		return false;
 	m_mpegheaderReadPos = 0;
