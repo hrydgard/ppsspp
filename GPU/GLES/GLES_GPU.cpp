620d000232350ccbb2967491da6d94c92d5aacfe
raven02 <jacky.kktsui@yahoo.com.hk>
2013-09-23 22:48:27 +0800

    gstate_c.scissorChanged = true when GE_CMD_SCISSOR1/2

--- GPU/GLES/GLES_GPU.cpp
+++ GPU/GLES/GLES_GPU.cpp
@@ -909,6 +909,7 @@ void GLES_GPU::ExecuteOp(u32 op, u32 diff) {
 
 	case GE_CMD_SCISSOR1:
 	case GE_CMD_SCISSOR2:
+		gstate_c.scissorChanged = true;
 		if (diff)
 			gstate_c.framebufChanged = true;
 		break;
