24d587c991738ba809222daa59bf7c9f8efe59a1
The Dax <daxtsu@gmail.com>
2013-09-26 17:49:37 -0400

    Adjust a few calls. Display fullscreen/normal resolution mode info only one time.

--- Windows/WndMainWindow.cpp
+++ Windows/WndMainWindow.cpp
@@ -276,7 +276,7 @@ namespace MainWindow
 		// Reset full screen indicator.
 		g_Config.bFullScreen = false;
 		CorrectCursor();
-		ResizeDisplay(true);
+		ResizeDisplay(true, true);
 		ShowOwnedPopups(hwndMain, TRUE);
 		W32Util::MakeTopMost(hwndMain, g_Config.bTopMost);
 	}
@@ -304,7 +304,7 @@ namespace MainWindow
 		// Set full screen indicator.
 		g_Config.bFullScreen = true;
 		CorrectCursor();
-		ResizeDisplay(true);
+		ResizeDisplay(true, true);
 		ShowOwnedPopups(hwndMain, FALSE);
 		UpdateScreenScale();
 	}
