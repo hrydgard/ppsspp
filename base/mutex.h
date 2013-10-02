dfbc5f3644b0ae85b522c9fe8c5a612cfd48868a
Siddharth <siddu.druid@gmail.com>
2013-10-02 07:49:45 -0700

    atomic_flag needs to have public methods.

--- base/mutex.h
+++ base/mutex.h
@@ -43,10 +43,11 @@ struct atomic_flag_init {
 static const atomic_flag_init NATIVE_ATOMIC_FLAG_INIT;
 
 class atomic_flag {
+public:
 	atomic_flag() {
 	}
 
-	atomic_flag(atomic_flag_init &v) : value(0) {
+	atomic_flag(const atomic_flag_init &v) : value(0) {
 	}
 
 	void clear() {
