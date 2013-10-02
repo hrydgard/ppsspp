da66cb55f4fa91137b186e6ad9faaa98beff5f33
Siddharth <siddu.druid@gmail.com>
2013-10-02 11:11:33 +0530

    Revert "changed the gitignore and android/project files" This was to be done on a separate branch. Reverting change. This reverts commit 43a0d5f60f667a08e25355e124536f68ac4205fd.

--- android/ab.sh
+++ android/ab.sh
@@ -1,3 +1,3 @@
 cp -r ../flash0 assets
 cp -r ../lang assets
-NDK_MODULE_PATH=..:../native/ext ndk-build -j3 TARGET_PLATFORM=android-9 $*
+NDK_MODULE_PATH=..:../native/ext $NDK/ndk-build -j3 TARGET_PLATFORM=android-9 $*
