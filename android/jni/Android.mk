0ec09f9700049afbc9407a8c947b0e448638d3f5
Unknown W. Brackets <checkins@unknownbrackets.org>
2013-10-06 21:26:08 -0700

    Move parts of ChunkFile into a cpp, cleanup.

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -176,6 +176,7 @@ LOCAL_SRC_FILES := \
   $(SRC)/ext/xxhash.c \
   $(SRC)/Common/Crypto/md5.cpp \
   $(SRC)/Common/Crypto/sha1.cpp \
+  $(SRC)/Common/ChunkFile.cpp \
   $(SRC)/Common/KeyMap.cpp \
   $(SRC)/Common/LogManager.cpp \
   $(SRC)/Common/MemArena.cpp \
