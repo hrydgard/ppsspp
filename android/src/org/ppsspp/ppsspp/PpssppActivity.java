8489bd15dbeac2f3e328693dec8c40fe650b8a7d
Henrik Rydgård <hrydgard@gmail.com>
2013-10-09 15:32:37 +0200

    Unlock orientation again after some native fixes. Experiment with Google App Analytics.
    The app analytics only tracks usage, sends no other information about
    what you do with the app. Interested in opinions if anyone thinks this
    is too much or maybe should be disabled by default.

--- android/src/org/ppsspp/ppsspp/PpssppActivity.java
+++ android/src/org/ppsspp/ppsspp/PpssppActivity.java
@@ -1,6 +1,9 @@
 package org.ppsspp.ppsspp;
 
+import android.os.Bundle;
+
 import com.henrikrydgard.libnative.NativeActivity;
+import com.google.analytics.tracking.android.EasyTracker;
 
 public class PpssppActivity extends NativeActivity {
 	static { 
@@ -9,11 +12,26 @@ public class PpssppActivity extends NativeActivity {
 
 	public PpssppActivity() {
 		super();
-	} 
-	
-	public boolean overrideKeys()
-	{   
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		EasyTracker.getInstance(this).activityStart(this);
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		EasyTracker.getInstance(this).activityStop(this);
+	}
+
+	public boolean overrideKeys() {
 		return false;
-	}  
-}                                     
-   
+	}
+}
