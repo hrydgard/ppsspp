fa1858ffafd5fff96f7c03bbe010f2d7d1fa8c2c
Henrik Rydgård <hrydgard@gmail.com>
2013-10-17 12:08:47 +0200

    Fix ab.sh to copy shaders and langregion to assets

--- android/ab.sh
+++ android/ab.sh
@@ -1,3 +1,5 @@
 cp -r ../flash0 assets
 cp -r ../lang assets
+cp -r ../assets/shaders assets
+cp ../assets/langregion.ini assets/langregion.ini
 NDK_MODULE_PATH=..:../native/ext $NDK/ndk-build -j3 TARGET_PLATFORM=android-9 $*
