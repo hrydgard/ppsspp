be40289d6c87c1e3a0ff6fa0264b3e94b82633fc
Henrik Rydgård <hrydgard@gmail.com>
2013-10-17 16:00:43 +0200

    Use ffmpeg for Atrac3+ decoding during video playback as well.

--- Core/HW/atrac3plus.h
+++ Core/HW/atrac3plus.h
@@ -1,5 +1,4 @@
-#ifndef _ATRAC3PLUS_DECODER_
-#define _ATRAC3PLUS_DECODER_
+#pragma once
 
 #include "Core/HW/BufferQueue.h"
 
@@ -19,5 +18,3 @@ namespace Atrac3plus_Decoder {
 	int CloseContext(Context *context);
 	bool Decode(Context context, void* inbuf, int inbytes, int *outbytes, void* outbuf);
 }
-
-#endif // _ATRAC3PLUS_DECODER_
