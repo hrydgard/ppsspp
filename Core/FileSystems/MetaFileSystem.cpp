4b063228f6b206ee6fb0dc4e3223bc782012957e
Henrik Rydgård <hrydgard@gmail.com>
2013-10-20 10:46:23 +0200

    Fix path bug by using the right quotes..

--- Core/FileSystems/MetaFileSystem.cpp
+++ Core/FileSystems/MetaFileSystem.cpp
@@ -30,7 +30,7 @@ static bool ApplyPathStringToComponentsVector(std::vector<std::string> &vector, 
 	while (start < len)
 	{
 		// TODO: This should only be done for ms0:/ etc.
-		size_t i = pathString.find_first_of('/\\', start);
+		size_t i = pathString.find_first_of("/\\", start);
 		if (i == std::string::npos)
 			i = len;
 
