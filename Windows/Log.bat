a801e5c325384960dbe0325453da4c45f6b4ef7c
sum2012 <consignpo@gmail.com>
2013-10-20 11:23:46 +0800

    Fix Windows logging
    Fix https://github.com/hrydgard/ppsspp/issues/4239

--- Windows/Log.bat
+++ Windows/Log.bat
@@ -1,6 +1,4 @@
 @echo off
-IF Not exist .\User md .\User
-IF Not exist .\User\Logs md .\User\Logs
-IF exist .\User\Logs\ppsspp.log Del .\User\Logs\ppsspp.log
-IF exist .\PPSSPPWindows.exe PPSSPPWindows.exe
-IF exist .\PPSSPPWindows64.exe PPSSPPWindows64.exe
+IF exist .\ppsspp.log Del .\ppsspp.log
+IF exist .\PPSSPPWindows.exe PPSSPPWindows.exe  --log=ppsspp.log
+IF exist .\PPSSPPWindows64.exe PPSSPPWindows64.exe --log=ppsspp.log
