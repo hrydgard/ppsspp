bd23f91e0c1917887df7785023e0a18068a245ca
Henrik Rydgård <hrydgard@gmail.com>
2013-11-15 10:34:27 +0100

    Flush the lit pool in the vtx dec

--- GPU/GLES/VertexDecoder.cpp
+++ GPU/GLES/VertexDecoder.cpp
@@ -1055,7 +1055,7 @@ JittedVertexDecoder VertexDecoderJitCache::Compile(const VertexDecoder &dec) {
 	POP(6, R4, R5, R6, R7, R8, _PC);
 
 	FlushIcache();
-
+	FlushLitPool();
 	// DisassembleArm(start, GetCodePtr() - start);
 	// char temp[1024] = {0};
 	// dec.ToString(temp);
