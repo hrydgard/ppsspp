6646605778100f61f8966eb5efebed2d6f0d0bd6
Peter Tissen <dravorek@gmail.com>
2013-11-18 00:11:12 +0100

    reintroduce lowmem texture atlas for the qt build
    added comiled lowmem version with ASCII only
    add lowmem atlas, this is the automatically generated file from the atlastool
    remove nofont generated *.cpp  and only add the first 127 bytes of ASCII to the lowmem generation configuration
    
    For Qt only: using platform independent usleep and load lowmem atlas map

--- buildatlas.sh
+++ buildatlas.sh
@@ -1,2 +1,2 @@
 ./native/tools/build/atlastool atlasscript.txt ui 8888 && cp ui_atlas.zim assets && mv ui_atlas.zim android/assets && mv ui_atlas.cpp ui_atlas.h UI
-./native/tools/build/atlastool atlasscript_nofont.txt ui 8888 && mv ui_atlas.zim assets/ui_atlas_nofont.zim && mv ui_atlas.cpp UI/ui_atlas_nofont.cpp && rm ui_atlas.h
+./native/tools/build/atlastool atlasscript_lowmem.txt ui 8888 && mv ui_atlas.zim assets/ui_atlas_lowmem.zim && mv ui_atlas.cpp UI/ui_atlas_lowmem.cpp && rm ui_atlas.h
