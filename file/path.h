b04f2d4c45264e448fe7b506756d65f67e6170d0
Sacha <xsacha@gmail.com>
2013-11-20 01:23:15 +1000

    Hide account path in friendly path. There is limited space to print out a full path.

--- file/path.h
+++ file/path.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <string>
+#include <string.h>
 #include <vector>
+#include <stdlib.h>
 
 #include "file/file_util.h"
 
@@ -25,6 +27,19 @@ public:
 	}
 	std::string GetFriendlyPath() {
 		std::string str = GetPath();
+#if defined(BLACKBERRY)
+		char* home = getenv("PERIMETER_HOME");
+		if (!strncmp(str.c_str(), home, strlen(home))) {
+			str = str.substr(strlen(home));
+			str.insert(0, 1, '~');
+		}
+#elif defined(__linux)
+		char* home = getenv("HOME");
+		if (!strncmp(str.c_str(), home, strlen(home))) {
+			str = str.substr(strlen(home));
+			str.insert(0, 1, '~');
+		}
+#endif
 		/*
 #ifdef ANDROID
 		if (!memcmp(str.c_str(), g_Config.memCardDirectory.c_str(), g_Config.memCardDirectory.size()))
