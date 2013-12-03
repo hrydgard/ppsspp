1509176dad28de13afce88b18276cc605f0a6d23
Henrik Rydgård <hrydgard@gmail.com>
2013-12-03 16:17:24 +0100

    Remove "topbar" from recents screen, looked cluttered and didn't feel necessary.

--- UI/MainScreen.cpp
+++ UI/MainScreen.cpp
@@ -311,27 +311,29 @@ void GameBrowser::Refresh() {
 	Add(new Spacer(5.0f));
 	I18NCategory *m = GetI18NCategory("MainMenu");
 
-	LinearLayout *topBar = new LinearLayout(ORIENT_HORIZONTAL, new LinearLayoutParams(FILL_PARENT, WRAP_CONTENT));
-	if (allowBrowsing_) {
-		Margins pathMargins(5, 0);
-		topBar->Add(new TextView(path_.GetFriendlyPath().c_str(), ALIGN_VCENTER, true, new LinearLayoutParams(WRAP_CONTENT, FILL_PARENT, pathMargins)));
-		topBar->Add(new Spacer(new LinearLayoutParams(1.0f)));
+	// No topbar on recent screen
+	if (path_.GetPath() != "!RECENT") {
+		LinearLayout *topBar = new LinearLayout(ORIENT_HORIZONTAL, new LinearLayoutParams(FILL_PARENT, WRAP_CONTENT));
+		if (allowBrowsing_) {
+			Margins pathMargins(5, 0);
+			topBar->Add(new TextView(path_.GetFriendlyPath().c_str(), ALIGN_VCENTER, true, new LinearLayoutParams(WRAP_CONTENT, FILL_PARENT, pathMargins)));
+			topBar->Add(new Spacer(new LinearLayoutParams(1.0f)));
 #if defined(_WIN32) || defined(USING_QT_UI)
-		topBar->Add(new Choice(m->T("Browse", "Browse...")))->OnClick.Handle(this, &GameBrowser::HomeClick);
+			topBar->Add(new Choice(m->T("Browse", "Browse...")))->OnClick.Handle(this, &GameBrowser::HomeClick);
 #else
-		topBar->Add(new Choice(m->T("Home")))->OnClick.Handle(this, &GameBrowser::HomeClick);
+			topBar->Add(new Choice(m->T("Home")))->OnClick.Handle(this, &GameBrowser::HomeClick);
 #endif
-	} else {
-		topBar->Add(new Spacer(new LinearLayoutParams(1.0f)));
+		} else {
+			topBar->Add(new Spacer(new LinearLayoutParams(1.0f)));
+		}
+
+		ChoiceStrip *layoutChoice = topBar->Add(new ChoiceStrip(ORIENT_HORIZONTAL));
+		layoutChoice->AddChoice(I_GRID);
+		layoutChoice->AddChoice(I_LINES);
+		layoutChoice->SetSelection(*gridStyle_ ? 0 : 1);
+		layoutChoice->OnChoice.Handle(this, &GameBrowser::LayoutChange);
+		Add(topBar);
 	}
-
-	ChoiceStrip *layoutChoice = topBar->Add(new ChoiceStrip(ORIENT_HORIZONTAL));
-	layoutChoice->AddChoice(I_GRID);
-	layoutChoice->AddChoice(I_LINES);
-	layoutChoice->SetSelection(*gridStyle_ ? 0 : 1);
-	layoutChoice->OnChoice.Handle(this, &GameBrowser::LayoutChange);
-	Add(topBar);
-
 	if (*gridStyle_) {
 		gameList_ = new UI::GridLayout(UI::GridLayoutSettings(150, 85), new LinearLayoutParams(FILL_PARENT, WRAP_CONTENT));
 	} else {
