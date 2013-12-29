64717d1b026e1159e8e470dc71b37ab562add671
Henrik Rydgård <hrydgard@gmail.com>
2013-12-29 12:27:19 +0100

    Shaders: Make sure to always use matching versions.
    Really should unify this somehow.

--- GPU/GLES/VertexShaderGenerator.cpp
+++ GPU/GLES/VertexShaderGenerator.cpp
@@ -176,6 +176,12 @@ void GenerateVertexShader(int prim, u32 vertType, char *buffer, bool useHWTransf
 	if (gl_extensions.VersionGEThan(3, 3, 0)) {
 		glslES30 = true;
 		WRITE(p, "#version 330\n");
+	} else if (gl_extensions.VersionGEThan(3, 0, 0)) {
+		WRITE(p, "#version 130\n");
+		// Remove lowp/mediump in non-mobile non-glsl 3 implementations
+		WRITE(p, "#define lowp\n");
+		WRITE(p, "#define mediump\n");
+		WRITE(p, "#define highp\n");
 	} else {
 		WRITE(p, "#version 110\n");
 		// Remove lowp/mediump in non-mobile non-glsl 3 implementations
