a387f6dd8b684d6b0458e11dd239cfd6bb94b046
Henrik Rydgard <hrydgard@gmail.com>
2014-01-07 00:01:41 +0100

    Also disable some weird axis events we get on Ouya from mapping.

--- UI/ControlMappingScreen.cpp
+++ UI/ControlMappingScreen.cpp
@@ -264,12 +264,20 @@ void KeyMappingNewKeyDialog::key(const KeyInput &key) {
 }
 
 void KeyMappingNewKeyDialog::axis(const AxisInput &axis) {
-	// Ignore the accelerometer for mapping for now.
 	switch (axis.axisId) {
+	// Ignore the accelerometer for mapping for now.
 	case JOYSTICK_AXIS_ACCELEROMETER_X:
 	case JOYSTICK_AXIS_ACCELEROMETER_Y:
 	case JOYSTICK_AXIS_ACCELEROMETER_Z:
 		return;
+
+	// Also ignore some weird axis events we get on Ouya.
+	case JOYSTICK_AXIS_OUYA_UNKNOWN1:
+	case JOYSTICK_AXIS_OUYA_UNKNOWN2:
+	case JOYSTICK_AXIS_OUYA_UNKNOWN3:
+	case JOYSTICK_AXIS_OUYA_UNKNOWN4:
+		return;
+
 	default:
 		;
 	}
