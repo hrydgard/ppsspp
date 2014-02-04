cb2528ab995b6baca2abfc998db178184f69c08e
raven02 <jacky.kktsui@yahoo.com.hk>
2014-02-04 10:59:34 +0800

    Report the mpeg first timestamp

--- Core/HLE/sceMpeg.cpp
+++ Core/HLE/sceMpeg.cpp
@@ -266,8 +266,7 @@ void AnalyzeMpeg(u8 *buffer, MpegContext *ctx) {
 	
 	// Sanity Check ctx->mpegFirstTimestamp
 	if (ctx->mpegFirstTimestamp != 90000) {
-		WARN_LOG(HLE,"Invalid first mpegtimestamp : %i", ctx->mpegFirstTimestamp);
-		return;
+		WARN_LOG_REPORT(ME, "Unexpected mpeg first timestamp: %llx / %lld", ctx->mpegFirstTimestamp, ctx->mpegFirstTimestamp);
 	}
 	
 	if (ctx->mpegMagic != PSMF_MAGIC || ctx->mpegVersion < 0 ||
