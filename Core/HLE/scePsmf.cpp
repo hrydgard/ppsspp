89abfab51ee5d9de8a3ccc72a7bf965bf832ca7a
sum2012 <consignpo@gmail.com>
2014-02-13 07:44:59 +0800

    Add scePsmfPlayerStop and scePsmfPlayerReleasePsmf error code
    Find in #5416

--- Core/HLE/scePsmf.cpp
+++ Core/HLE/scePsmf.cpp
@@ -858,10 +858,17 @@ int scePsmfPlayerCreate(u32 psmfPlayer, u32 psmfPlayerDataAddr)
 
 int scePsmfPlayerStop(u32 psmfPlayer)
 {
-	INFO_LOG(ME, "scePsmfPlayerStop(%08x)", psmfPlayer);
 	PsmfPlayer *psmfplayer = getPsmfPlayer(psmfPlayer);
-	if (psmfplayer)
-		psmfplayer->status = PSMF_PLAYER_STATUS_STANDBY;
+	if (!psmfplayer) {
+		ERROR_LOG(ME, "scePsmfPlayerStop(%08x): invalid psmf player", psmfPlayer);
+		return ERROR_PSMF_NOT_FOUND;
+	}
+	if (psmfplayer->status < PSMF_PLAYER_STATUS_STANDBY) {
+		ERROR_LOG(ME, "scePsmfPlayerStop(%08x): not initialized", psmfPlayer);
+		return ERROR_PSMFPLAYER_NOT_INITIALIZED;
+	}
+	INFO_LOG(ME, "scePsmfPlayerStop(%08x)", psmfPlayer);
+	psmfplayer->status = PSMF_PLAYER_STATUS_STANDBY;
 	return 0;
 }
 
@@ -1085,10 +1092,17 @@ int scePsmfPlayerUpdate(u32 psmfPlayer)
 
 int scePsmfPlayerReleasePsmf(u32 psmfPlayer) 
 {
-	ERROR_LOG(ME, "UNIMPL scePsmfPlayerReleasePsmf(%08x)", psmfPlayer);
 	PsmfPlayer *psmfplayer = getPsmfPlayer(psmfPlayer);
-	if (psmfplayer)
-		psmfplayer->status = PSMF_PLAYER_STATUS_INIT;
+	if (!psmfplayer) {
+		ERROR_LOG(ME, "scePsmfPlayerReleasePsmf(%08x): invalid psmf player", psmfPlayer);
+		return ERROR_PSMF_NOT_FOUND;
+	}
+	if (psmfplayer->status != PSMF_PLAYER_STATUS_STANDBY){
+		ERROR_LOG(ME, "scePsmfPlayerReleasePsmf(%08x): not initalized", psmfPlayer);
+		return ERROR_PSMFPLAYER_NOT_INITIALIZED;
+	}
+	ERROR_LOG(ME, "UNIMPL scePsmfPlayerReleasePsmf(%08x)", psmfPlayer);
+	psmfplayer->status = PSMF_PLAYER_STATUS_INIT;
 	return 0;
 }
 
