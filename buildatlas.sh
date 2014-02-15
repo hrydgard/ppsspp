ef2dfb2db23599298957ad5280447133563d6a18
kaienfr <kaienfr@gmail.com>
2014-02-15 19:46:02 +0100

    Update buildatlas.sh
    add a missing &

--- buildatlas.sh
+++ buildatlas.sh
@@ -1,2 +1,2 @@
-cat chinese_ui.txt lang/zh_CN.ini lang/zh_TW.ini lang/ja_JP.ini > chinese.txt & cat korean-ui.txt lang/ko_KR.ini > korean.txt & ./native/tools/build/atlastool atlasscript.txt ui 8888 && cp ui_atlas.zim assets && mv ui_atlas.zim android/assets && mv ui_atlas.cpp ui_atlas.h UI
+cat chinese_ui.txt lang/zh_CN.ini lang/zh_TW.ini lang/ja_JP.ini > chinese.txt && cat korean-ui.txt lang/ko_KR.ini > korean.txt && ./native/tools/build/atlastool atlasscript.txt ui 8888 && cp ui_atlas.zim assets && mv ui_atlas.zim android/assets && mv ui_atlas.cpp ui_atlas.h UI
 ./native/tools/build/atlastool atlasscript_lowmem.txt ui 8888 && mv ui_atlas.zim assets/ui_atlas_lowmem.zim && mv ui_atlas.cpp UI/ui_atlas_lowmem.cpp && rm ui_atlas.h
