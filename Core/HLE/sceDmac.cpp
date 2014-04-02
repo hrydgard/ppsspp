928445421fb80e362fe6be495b93c186a349897c
sum2012 <consignpo@gmail.com>
2014-04-02 21:53:28 +0800

    More illegal sizecheck in sceDmacMemcpy
    Find in #5782

--- Core/HLE/sceDmac.cpp
+++ Core/HLE/sceDmac.cpp
@@ -75,7 +75,7 @@ u32 sceDmacMemcpy(u32 dst, u32 src, u32 size) {
 		ERROR_LOG(HLE, "sceDmacMemcpy(dest=%08x, src=%08x, size=%i): invalid address", dst, src, size);
 		return SCE_KERNEL_ERROR_INVALID_POINTER;
 	}
-	if (dst + size >= 0x80000000 || src + size >= 0x80000000) {
+	if (dst + size >= 0x80000000 || src + size >= 0x80000000 || size == 0xFFFFFFFF) {
 		ERROR_LOG(HLE, "sceDmacMemcpy(dest=%08x, src=%08x, size=%i): illegal size", dst, src, size);
 		return 0x80000023;
 	}
@@ -100,7 +100,7 @@ u32 sceDmacTryMemcpy(u32 dst, u32 src, u32 size) {
 		ERROR_LOG(HLE, "sceDmacTryMemcpy(dest=%08x, src=%08x, size=%i): invalid address", dst, src, size);
 		return SCE_KERNEL_ERROR_INVALID_POINTER;
 	}
-	if (dst + size >= 0x80000000 || src + size >= 0x80000000) {
+	if (dst + size >= 0x80000000 || src + size >= 0x80000000 || size == 0xFFFFFFFF) {
 		ERROR_LOG(HLE, "sceDmacTryMemcpy(dest=%08x, src=%08x, size=%i): illegal size", dst, src, size);
 		return 0x80000023;
 	}
