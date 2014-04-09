7c2c87fa67b5e6b6cf80288da94a82da85d81695
kaienfr <kaienfr@gmail.com>
2014-04-09 19:49:56 +0200

    fix learnjp09 mp3 issue

--- Core/HLE/sceMp3.cpp
+++ Core/HLE/sceMp3.cpp
@@ -283,7 +283,6 @@ static int readFunc(void *opaque, uint8_t *buf, int buf_size) {
 	if (ctx->bufferAvailable == 0) {
 		ctx->bufferRead = 0;
 		ctx->bufferWrite = 0;
-		return res;
 		// if the mp3 file have not been all decoded, we should not stop but continue
 		// we can control the loop times here. If the mp3 file has been fully decoded, then return zero to stop, return buf_size to loop.
 		int looped = ctx->mp3DecodedBytes / (ctx->mp3StreamEnd - ctx->mp3StreamStart);
