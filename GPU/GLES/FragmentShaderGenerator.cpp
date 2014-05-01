caf5b0ed10988847c3671fdc540edc560fa17a67
sum2012 <consignpo@gmail.com>
2014-05-02 05:12:45 +0800

    GLES:Add a !gstate.isTextureMapEnabled check
    Fix Super Danganronp​a 2 butterfly colour issue
    #5996

--- GPU/GLES/FragmentShaderGenerator.cpp
+++ GPU/GLES/FragmentShaderGenerator.cpp
@@ -61,7 +61,7 @@ static bool IsAlphaTestTriviallyTrue() {
 		return true;
 
 	case GE_COMP_GEQUAL:
-		if (gstate_c.vertexFullAlpha && (gstate_c.textureFullAlpha || !gstate.isTextureAlphaUsed()))
+		if (gstate_c.vertexFullAlpha && (gstate_c.textureFullAlpha || !gstate.isTextureAlphaUsed()) && !gstate.isTextureMapEnabled())
 			return true;  // If alpha is full, it doesn't matter what the ref value is.
 		return gstate.getAlphaTestRef() == 0;
 
