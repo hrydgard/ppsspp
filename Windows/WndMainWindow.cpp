a41737ebe7b44e96d3ee2baea023f639ba420478
pal1000 <liviuprodea@yahoo.com>
2014-06-04 19:15:41 +0300

    Save state, load state and next save slot shortcuts are no longer hardcoded.

--- Windows/WndMainWindow.cpp
+++ Windows/WndMainWindow.cpp
@@ -19,6 +19,7 @@
 // It's improving slowly, though. :)
 
 #include "Common/CommonWindows.h"
+#include "Common/KeyMap.h"
 
 #include <map>
 #include <string>
@@ -574,7 +575,7 @@ namespace MainWindow {
 		TranslateMenuItem(ID_FILE_LOAD_DIR);
 		TranslateMenuItem(ID_FILE_LOAD_MEMSTICK);
 		TranslateMenuItem(ID_FILE_MEMSTICK);
-		TranslateSubMenu("Savestate Slot", MENU_FILE, SUBMENU_FILE_SAVESTATE_SLOT, L"\tF3");
+		TranslateSubMenu("Savestate Slot", MENU_FILE, SUBMENU_FILE_SAVESTATE_SLOT, L"\tF2");
 		TranslateMenuItem(ID_FILE_QUICKLOADSTATE, L"\tF4");
 		TranslateMenuItem(ID_FILE_QUICKSAVESTATE, L"\tF2");
 		TranslateMenuItem(ID_FILE_LOADSTATEFILE);
