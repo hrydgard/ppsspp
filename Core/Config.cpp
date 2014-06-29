55b517f4d10ec844357d07441d85f6eec09ce29c
The Dax <daxtsu@gmail.com>
2014-06-29 13:54:35 -0400

    Win32: Make pausing when minimizing optional in the ini.

--- Core/Config.cpp
+++ Core/Config.cpp
@@ -309,6 +309,7 @@ static ConfigSetting generalSettings[] = {
 	ConfigSetting("WindowWidth", &g_Config.iWindowWidth, 0),   // 0 will be automatically reset later (need to do the AdjustWindowRect dance).
 	ConfigSetting("WindowHeight", &g_Config.iWindowHeight, 0),
 	ConfigSetting("PauseOnLostFocus", &g_Config.bPauseOnLostFocus, false),
+	ConfigSetting("PauseWhenMinimized", &g_Config.bPauseWhenMinimized, true),
 #endif
 	ConfigSetting("DumpDecryptedEboots", &g_Config.bDumpDecryptedEboot, false),
 	ConfigSetting(false),
