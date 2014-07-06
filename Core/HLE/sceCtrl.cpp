b370621a3afb093e7d296c87bb053c8394f94917
Unknown W. Brackets <checkins@unknownbrackets.org>
2014-07-06 01:08:44 -0700

    Fix sceCtrl buffer update timing drift.
    Spotted by xsacha.  Only when not using vblank timing (most commonly used
    by games.)

--- Core/HLE/sceCtrl.cpp
+++ Core/HLE/sceCtrl.cpp
@@ -289,7 +289,7 @@ void __CtrlTimerUpdate(u64 userdata, int cyclesLate)
 	// This only runs in timer mode (ctrlCycle > 0.)
 	_dbg_assert_msg_(SCECTRL, ctrlCycle > 0, "Ctrl: sampling cycle should be > 0");
 
-	CoreTiming::ScheduleEvent(usToCycles(ctrlCycle), ctrlTimer, 0);
+	CoreTiming::ScheduleEvent(usToCycles(ctrlCycle) - cyclesLate, ctrlTimer, 0);
 
 	__CtrlDoSample();
 }
