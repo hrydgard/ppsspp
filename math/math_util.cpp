49e8414fec1e371c30d901916a2c014cd5316d1a
Sacha <xsacha@gmail.com>
2014-07-17 02:29:49 +1000

    Oops. fmrx is only for armv7.

--- math/math_util.cpp
+++ math/math_util.cpp
@@ -5,7 +5,7 @@
 #endif
 
 // QNX can only use RunFast mode and it is already the default.
-#if defined(ARM) && !defined(BLACKBERRY) && !defined(__SYMBIAN32__)
+#if defined(__ARM_ARCH_7A__) && !defined(BLACKBERRY) && !defined(__SYMBIAN32__)
 // Enables 'RunFast' VFP mode.
 void EnableFZ() {
 	int x;
