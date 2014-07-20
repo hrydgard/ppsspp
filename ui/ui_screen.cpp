ed04a0bf28dbe89f894bb33b0700f72c9ec17b72
Unknown W. Brackets <checkins@unknownbrackets.org>
2014-07-19 22:57:09 -0700

    Only trigger a popup's default button if it has one.

--- ui/ui_screen.cpp
+++ ui/ui_screen.cpp
@@ -150,7 +150,7 @@ bool PopupScreen::touch(const TouchInput &touch) {
 
 bool PopupScreen::key(const KeyInput &key) {
 	if (key.flags & KEY_DOWN) {
-		if (key.keyCode == NKCODE_ENTER) {
+		if (key.keyCode == NKCODE_ENTER && defaultButton_) {
 			UI::EventParams e;
 			defaultButton_->OnClick.Trigger(e);
 			return true;
