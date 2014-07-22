41519cef4968edb178471cfbcc3dd9c515a85afc
sum2012 <consignpo@gmail.com>
2014-07-23 06:38:13 +0800

    Change to Debug log

--- Core/HLE/sceKernel.cpp
+++ Core/HLE/sceKernel.cpp
@@ -424,7 +424,7 @@ int sceKernelDcacheWritebackInvalidateAll()
 
 u32 sceKernelIcacheInvalidateAll()
 {
-	ERROR_LOG(CPU, "Unimpl sceKernelIcacheInvalidateAll");
+	DEBUG_LOG(CPU, "Unimpl sceKernelIcacheInvalidateAll");
 #ifdef LOG_CACHE
 	NOTICE_LOG(CPU, "Icache invalidated - should clear JIT someday");
 #endif
