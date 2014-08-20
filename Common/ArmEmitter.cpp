7691e0e0b5bfbb2939f382946e5bbc19cba6495c
Sacha <xsacha@gmail.com>
2014-08-20 17:25:38 +1000

    Enable LitPool for ARMv6. Was originally disabled because vertex JIT wasn't flushing but it now does.

--- Common/ArmEmitter.cpp
+++ Common/ArmEmitter.cpp
@@ -534,9 +534,8 @@ void ARMXEmitter::MOVI2R(ARMReg reg, u32 val, bool optimize)
 				}
 			}
 			// Use literal pool for ARMv6.
-			// Disabled for now as it is crashing since Vertex Decoder JIT
-//			AddNewLit(val);
-//			LDR(reg, R_PC); // To be backpatched later
+			AddNewLit(val);
+			LDR(reg, R_PC); // To be backpatched later
 		}
 #endif
 	}
