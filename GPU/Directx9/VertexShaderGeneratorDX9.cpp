5e829f06e8bd59bf188f370ef0cdec05164f931a
Unknown W. Brackets <checkins@unknownbrackets.org>
2014-08-25 01:39:04 -0700

    d3d: Fix a shader compiler error.

--- GPU/Directx9/VertexShaderGeneratorDX9.cpp
+++ GPU/Directx9/VertexShaderGeneratorDX9.cpp
@@ -538,7 +538,7 @@ void GenerateVertexShaderDX9(int prim, char *buffer, bool useHWTransform) {
 						{
 							static const char *rescaleuv[4] = {"", " * 1.9921875", " * 1.999969482421875", ""}; // 2*127.5f/128.f, 2*32767.5f/32768.f, 1.0f};
 							const char *factor = rescaleuv[(vertType & GE_VTYPE_TC_MASK) >> GE_VTYPE_TC_SHIFT];
-							temp_tc = StringFromFormat("float4(a_texcoord.xy %s, 0.0, 1.0)", factor);
+							temp_tc = StringFromFormat("float4(In.texcoord.xy %s, 0.0, 1.0)", factor);
 						}
 						break;
 					case GE_PROJMAP_NORMALIZED_NORMAL:  // Use normalized transformed normal as source
