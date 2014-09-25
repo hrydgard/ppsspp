d54daa2ea5faea429e6ec2ecc850e5b9b852ea93
beaumanvienna <jc.lache@gmail.com>
2014-09-25 21:59:45 +0200

    amendement for sdl version after Qt changes

--- base/PCMain.cpp
+++ base/PCMain.cpp
@@ -631,9 +631,9 @@ int main(int argc, char *argv[]) {
 #endif
 
 #ifdef _WIN32
-	NativeInit(argc, (const char **)argv, path, "D:\\", "BADCOFFEE");
+	NativeInit(argc, (const char **)argv, path, "D:\\", "BADCOFFEE",false);
 #else
-	NativeInit(argc, (const char **)argv, path, "/tmp", "BADCOFFEE");
+	NativeInit(argc, (const char **)argv, path, "/tmp", "BADCOFFEE",false);
 #endif
 
 	pixel_in_dps = (float)pixel_xres / dp_xres;
