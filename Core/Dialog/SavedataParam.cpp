24ce3d11edb2bef457991796a083112924f38b24
Unknown W. Brackets <checkins@unknownbrackets.org>
2014-11-02 08:12:29 -0800

    Another typo.

--- Core/Dialog/SavedataParam.cpp
+++ Core/Dialog/SavedataParam.cpp
@@ -904,7 +904,7 @@ std::string SavedataParam::GetSpaceText(u64 size)
 	{
 		if (size < 1024)
 		{
-			snprintf(text, sizeof(text), "ll%d %s", size, suffixes[i]);
+			snprintf(text, sizeof(text), "%lld %s", size, suffixes[i]);
 			return std::string(text);
 		}
 		size /= 1024;
