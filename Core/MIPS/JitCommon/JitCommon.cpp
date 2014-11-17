3298c1143f4f5c79845bd9eb46ce6c76e838605b
Henrik Rydgard <hrydgard@gmail.com>
2014-11-17 21:17:53 +0100

    Arm disasm: Coalesce multiple "BKPT 1" like we do on x86 for INT 3

--- Core/MIPS/JitCommon/JitCommon.cpp
+++ Core/MIPS/JitCommon/JitCommon.cpp
@@ -32,6 +32,7 @@ std::vector<std::string> DisassembleArm2(const u8 *data, int size) {
 	std::vector<std::string> lines;
 
 	char temp[256];
+	int bkpt_count = 0;
 	for (int i = 0; i < size; i += 4) {
 		const u32 *codePtr = (const u32 *)(data + i);
 		u32 inst = codePtr[0];
@@ -43,7 +44,7 @@ std::vector<std::string> DisassembleArm2(const u8 *data, int size) {
 			int reg0 = (inst & 0x0000F000) >> 12;
 			int reg1 = (next & 0x0000F000) >> 12;
 			if (reg0 == reg1) {
-				sprintf(temp, "MOV32 %s, %04x%04x", ArmRegName(reg0), hi, low);
+				snprintf(temp, sizeof(temp), "MOV32 %s, %04x%04x", ArmRegName(reg0), hi, low);
 				// sprintf(temp, "%08x MOV32? %s, %04x%04x", (u32)inst, ArmRegName(reg0), hi, low);
 				lines.push_back(temp);
 				i += 4;
@@ -52,7 +53,18 @@ std::vector<std::string> DisassembleArm2(const u8 *data, int size) {
 		}
 		ArmDis((u32)(intptr_t)codePtr, inst, temp, sizeof(temp), false);
 		std::string buf = temp;
-		lines.push_back(buf);
+		if (buf == "BKPT 1") {
+			bkpt_count++;
+		} else {
+			if (bkpt_count) {
+				lines.push_back(StringFromFormat("BKPT 1 (x%i)", bkpt_count));
+				bkpt_count = 0;
+			}
+			lines.push_back(buf);
+		}
+	}
+	if (bkpt_count) {
+		lines.push_back(StringFromFormat("BKPT 1 (x%i)", bkpt_count));
 	}
 	return lines;
 }
