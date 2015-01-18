b48117d01b17c1782880066b9bc7999ebdf0cdcc
Unknown W. Brackets <checkins@unknownbrackets.org>
2015-01-17 19:47:54 -0800

    Fix a potential null deref.
    This is null checked after dereferencing.

--- gfx_es2/gl_state.cpp
+++ gfx_es2/gl_state.cpp
@@ -194,6 +194,7 @@ void CheckGLExtensions() {
 		g_all_gl_extensions = extString;
 	} else {
 		g_all_gl_extensions = "";
+		extString = "";
 	}
 
 #ifdef WIN32
@@ -342,7 +343,7 @@ void CheckGLExtensions() {
 	gl_extensions.FBO_EXT = false;
 	gl_extensions.PBO_ARB = true;
 	gl_extensions.PBO_NV = true;
-	if (extString) {
+	if (strlen(extString) != 0) {
 		gl_extensions.FBO_ARB = strstr(extString, "GL_ARB_framebuffer_object") != 0;
 		gl_extensions.FBO_EXT = strstr(extString, "GL_EXT_framebuffer_object") != 0;
 		gl_extensions.PBO_ARB = strstr(extString, "GL_ARB_pixel_buffer_object") != 0;
