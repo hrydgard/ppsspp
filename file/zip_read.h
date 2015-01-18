45103d19294387e31af45505ada4ef131d67fd20
Unknown W. Brackets <checkins@unknownbrackets.org>
2015-01-18 13:15:18 -0800

    Fix an unlikely buffer overflow.

--- file/zip_read.h
+++ file/zip_read.h
@@ -64,7 +64,8 @@ private:
 class DirectoryAssetReader : public AssetReader {
 public:
 	DirectoryAssetReader(const char *path) {
-		strcpy(path_, path);
+		strncpy(path_, path, ARRAY_SIZE(path_));
+		path_[ARRAY_SIZE(path_) - 1] = '\0';
 	}
 	// use delete[]
 	virtual uint8_t *ReadAsset(const char *path, size_t *size);
