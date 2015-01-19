19b92a3e68ede5674885aa0828797adb3cb3f0c7
Unknown W. Brackets <checkins@unknownbrackets.org>
2015-01-19 09:40:46 -0800

    Typo.

--- Core/MIPS/ARM/ArmCompALU.cpp
+++ Core/MIPS/ARM/ArmCompALU.cpp
@@ -177,7 +177,7 @@ namespace MIPSComp
 				u32 value = gpr.GetImm(rs);
 				int x = 31;
 				int count = 0;
-				while (x >= 0 && (value & (1 << x)))
+				while (x >= 0 && (value & (1 << x))) {
 					count++;
 					x--;
 				}
