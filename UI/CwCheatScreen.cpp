4ef0c14cc86414f12f7657a13517feed26e2fe14
LunaMoo <LunaMoo@users.noreply.github.com>
2015-01-28 09:48:35 +0100

    oops

--- UI/CwCheatScreen.cpp
+++ UI/CwCheatScreen.cpp
@@ -146,8 +146,6 @@ UI::EventReturn CwCheatScreen::OnAddCheat(UI::EventParams &params) {
 
 UI::EventReturn CwCheatScreen::OnEditCheatFile(UI::EventParams &params) {
 	std::string cheatFile;
-	std::fstream fs;
-	fs.close();
 	g_Config.bReloadCheats = true;
 	g_Config.Save();
 	if (MIPSComp::jit) {
