763e684bf7f09aa72a67b7e68c8e1c982c7959a5
daniel229 <gd.mk.cg@gmail.com>
2015-02-15 17:26:54 +0800

    agains

--- Core/HLE/sceSfmt19937.cpp
+++ Core/HLE/sceSfmt19937.cpp
@@ -15,7 +15,7 @@
 // Official git repository and contact information can be found at
 // https://github.com/hrydgard/ppsspp and http://www.ppsspp.org/.
 
-#include "ext/sfmt19937/sfmt.h"
+#include "ext/sfmt19937/SFMT.h"
 
 #include "sceSfmt19937.h"
 #include "Common/Log.h"
