3ada9887403df9299af5402c5bf079475d34986e
Peter Tissen <dravorek@gmail.com>
2015-03-06 22:10:10 +0100

    capture by value, since the dispatch seems asynchronous

--- GPU/Software/Rasterizer.cpp
+++ GPU/Software/Rasterizer.cpp
@@ -1310,7 +1310,7 @@ void DrawTriangle(const VertexData& v0, const VertexData& v1, const VertexData& 
 	if (gstate.isModeClear()) {
 		if (range >= 24 && (maxX - minX) >= 24 * 16)
 		{
-			auto bound = [&](int a, int b) -> void {DrawTriangleSlice<true>(v0, v1, v2, minX, minY, maxX, maxY, a, b); };
+			auto bound = [=](int a, int b) -> void {DrawTriangleSlice<true>(v0, v1, v2, minX, minY, maxX, maxY, a, b); };
 			GlobalThreadPool::Loop(bound, 0, range);
 		}
 		else
@@ -1320,7 +1320,7 @@ void DrawTriangle(const VertexData& v0, const VertexData& v1, const VertexData& 
 	} else {
 		if (range >= 24 && (maxX - minX) >= 24 * 16)
 		{
-			auto bound = [&](int a, int b) -> void {DrawTriangleSlice<false>(v0, v1, v2, minX, minY, maxX, maxY, a, b); };
+			auto bound = [=](int a, int b) -> void {DrawTriangleSlice<false>(v0, v1, v2, minX, minY, maxX, maxY, a, b); };
 			GlobalThreadPool::Loop(bound, 0, range);
 		}
 		else
