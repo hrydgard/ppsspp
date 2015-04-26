6e50a0b2743da8d7486ad5e400e9962962f3976e
Unknown W. Brackets <checkins@unknownbrackets.org>
2015-04-26 00:36:08 -0700

    Allow depal palette entries to repeat.
    This is what happens if the base is 0x10 in 8888 mode (means | 0x100.)

--- GPU/GLES/DepalettizeShader.cpp
+++ GPU/GLES/DepalettizeShader.cpp
@@ -152,7 +152,7 @@ GLuint DepalShaderCache::GetClutTexture(const u32 clutID, u32 *rawClut) {
 
 	glTexImage2D(GL_TEXTURE_2D, 0, components, texturePixels, 1, 0, components2, dstFmt, (void *)rawClut);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
