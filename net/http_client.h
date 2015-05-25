babb5036e62e85e65f4ce15344363429c9e6d932
Jan Beich <jbeich@FreeBSD.org>
2015-05-25 13:40:26 +0000

    net: don't add #ifdefs for POSIX headers
    http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/netinet_in.h.html
    http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/arpa_inet.h.html

--- net/http_client.h
+++ net/http_client.h
@@ -11,11 +11,8 @@
 #define NOMINMAX
 #include <winsock2.h>
 #else
-#if defined(__FreeBSD__) || defined(__SYMBIAN32__)
 #include <netinet/in.h>
-#else
 #include <arpa/inet.h>
-#endif
 #include <sys/socket.h>
 #include <netdb.h>
 #endif
--- net/http_server.cpp
+++ net/http_server.cpp
@@ -11,6 +11,7 @@
 #include <sys/socket.h>       /*  socket definitions        */
 #include <sys/types.h>        /*  socket types              */
 #include <sys/wait.h>         /*  for waitpid()             */
+#include <netinet/in.h>       /*  struct sockaddr_in        */
 #include <arpa/inet.h>        /*  inet (3) funtions         */
 #include <unistd.h>           /*  misc. UNIX functions      */
 
