3d59fe5f6fa6a3e870c6a0edd62faafdd92c6454
Unknown W. Brackets <checkins@unknownbrackets.org>
2015-06-28 17:56:04 -0700

    Move system info to tools instead of hidden in dev.

--- UI/GameSettingsScreen.h
+++ UI/GameSettingsScreen.h
@@ -100,6 +100,7 @@ private:
 	UI::EventReturn OnAudioBackend(UI::EventParams &e);
 
 	UI::EventReturn OnSavedataManager(UI::EventParams &e);
+	UI::EventReturn OnSysInfo(UI::EventParams &e);
 
 	// Temporaries to convert bools to int settings
 	bool cap60FPS_;
@@ -125,7 +126,6 @@ protected:
 private:
 	UI::EventReturn OnBack(UI::EventParams &e);
 	UI::EventReturn OnRunCPUTests(UI::EventParams &e);
-	UI::EventReturn OnSysInfo(UI::EventParams &e);
 	UI::EventReturn OnLoggingChanged(UI::EventParams &e);
 	UI::EventReturn OnLoadLanguageIni(UI::EventParams &e);
 	UI::EventReturn OnSaveLanguageIni(UI::EventParams &e);
