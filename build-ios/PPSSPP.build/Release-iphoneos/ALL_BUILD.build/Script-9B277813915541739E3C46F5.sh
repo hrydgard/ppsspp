9a421d2166065c08a5f12105d176ba74e6ee433d
Dan Gillespie <dgillespie@broadspring.com>
2015-07-27 12:53:12 -0700

    updated minor preprocesser definition and cleaned build

---
+++ build-ios/PPSSPP.build/Release-iphoneos/ALL_BUILD.build/Script-9B277813915541739E3C46F5.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/dan/Desktop/repos/ppsspp/build-ios -f /Users/dan/Desktop/repos/ppsspp/build-ios/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
---
+++ build-ios/PPSSPP.build/Release-iphoneos/ZERO_CHECK.build/Script-8C8CD36DB7FE4F8AA1F26CC7.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/dan/Desktop/repos/ppsspp/build-ios -f /Users/dan/Desktop/repos/ppsspp/build-ios/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
