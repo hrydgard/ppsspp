5dbc8abd94b3727858776985cc7aa9b95b7ab3e0
Henrik Rydgard <hrydgard@gmail.com>
2015-08-26 17:28:34 +0200

    experiment: Add a different offset that unknown found during testing, plus remove rounding of through mode depth (which should already be rounded)

--- GPU/GLES/VertexShaderGenerator.cpp
+++ GPU/GLES/VertexShaderGenerator.cpp
@@ -391,7 +391,7 @@ void GenerateVertexShader(int prim, u32 vertType, char *buffer, bool useHWTransf
 
 	// See comment above this function (GenerateVertexShader).
 	WRITE(p, "\nvec4 depthRoundZ(vec4 v) {\n");
-	WRITE(p, "  return vec4(v.x, v.y, (1.0/65535.0) * floor(v.z * 65535.0), v.w);\n");
+	WRITE(p, "  return v; //vec4(v.x, v.y, (1.0/65535.0) * floor(v.z * 65535.0), v.w);\n");
 	WRITE(p, "}\n\n");
 
 	if (!gstate.isModeThrough()) {
@@ -399,7 +399,7 @@ void GenerateVertexShader(int prim, u32 vertType, char *buffer, bool useHWTransf
 		WRITE(p, "\nvec4 depthRoundZVP(vec4 v) {\n");
 		WRITE(p, "  float z = v.z / v.w;\n");
 		WRITE(p, "  z = z * u_depthRange.x + u_depthRange.y;\n");
-		WRITE(p, "  z = floor(z - 0.5);\n");
+		WRITE(p, "  z = floor(z + 0.375);\n");
 		WRITE(p, "  z = (z - u_depthRange.y) / u_depthRange.x;\n");
 		WRITE(p, "  return vec4(v.x, v.y, z * v.w, v.w);\n");
 		WRITE(p, "}\n\n");
