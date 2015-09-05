86a02fed6149b0ec99badab4006fbac3f57fb7c9
Henrik Rydgard <hrydgard@gmail.com>
2015-09-05 19:42:58 +0200

    Minor cleanup

--- Common/ConsoleListener.cpp
+++ Common/ConsoleListener.cpp
@@ -237,7 +237,7 @@ void ConsoleListener::BufferWidthHeight(int BufferWidth, int BufferHeight, int S
 		COORD Co = {BufferWidth, BufferHeight};
 		SB = SetConsoleScreenBufferSize(hConsole, Co);
 		// Change the screen buffer window size
-		SMALL_RECT coo = {0,0,ScreenWidth, ScreenHeight}; // top, left, right, bottom
+		SMALL_RECT coo = {(SHORT)0, (SHORT)0, (SHORT)ScreenWidth, (SHORT)ScreenHeight}; // top, left, right, bottom
 		SW = SetConsoleWindowInfo(hConsole, TRUE, &coo);
 	}
 	else
@@ -246,7 +246,7 @@ void ConsoleListener::BufferWidthHeight(int BufferWidth, int BufferHeight, int S
 		SMALL_RECT coo = {0,0, ScreenWidth, ScreenHeight}; // top, left, right, bottom
 		SW = SetConsoleWindowInfo(hConsole, TRUE, &coo);
 		// Change screen buffer size
-		COORD Co = {BufferWidth, BufferHeight};
+		COORD Co = {(SHORT)BufferWidth, (SHORT)BufferHeight};
 		SB = SetConsoleScreenBufferSize(hConsole, Co);
 	}
 #endif
