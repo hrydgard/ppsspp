5ff5cf1ac7a33cac3fbd34ee8313652773c19516
Henrik Rydgard <hrydgard@gmail.com>
2015-09-27 11:50:23 +0200

    Add compat.ini as a dependency to our various build systems

--- android/ab.sh
+++ android/ab.sh
@@ -2,5 +2,6 @@ cp -r ../flash0 assets
 cp -r ../lang assets
 cp -r ../assets/shaders assets
 cp ../assets/langregion.ini assets/langregion.ini
+cp ../assets/compat.ini assets/compat.ini
 cp ../assets/*.png assets
 NDK_MODULE_PATH=../ext:../ext/native/ext $NDK/ndk-build -j3 $*
