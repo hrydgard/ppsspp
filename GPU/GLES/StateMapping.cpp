hrydgard/ppsspp

commit 9e18fb7e908a61f3a9b3637ff0fd92ee10ac298b

Round Z in the vertex shader to match a 16-bit Z buffer.
However this inexplicably does still not solve the Phantasy Star issue!

GPU/GLES/StateMapping.cpp
@@ -871,11 +871,11 @@ void TransformDrawEngine::ApplyDrawState(int prim) {
 
 		glstate.viewport.set(left, bottom, right - left, top - bottom);
 
-		float zScale = gstate.getViewportZScale() * (1.0f / 65535.0f);
-		float zCenter = gstate.getViewportZCenter() * (1.0f / 65535.0f);
+		float zScale = gstate.getViewportZScale();
+		float zCenter = gstate.getViewportZCenter();
 		float depthRangeMin = zCenter - zScale;
 		float depthRangeMax = zCenter + zScale;
-		glstate.depthRange.set(depthRangeMin, depthRangeMax);
+		glstate.depthRange.set(depthRangeMin * (1.0f / 65535.0f), depthRangeMax * (1.0f / 65535.0f));
 
 #ifndef MOBILE_DEVICE
 		float minz = gstate.getDepthRangeMin() * (1.0f / 65535.0f);

GPU/Common/SoftwareTransformCommon.cpp
@@ -211,6 +211,7 @@ void SoftwareTransform(
 			// The w of uv is also never used (hardcoded to 1.0.)
 		}
 	} else {
+		// Okay, need to actually perform the full transform.
 		for (int index = 0; index < maxIndex; index++) {
 			reader.Goto(index);
 
@@ -401,6 +402,9 @@ void SoftwareTransform(
 			}
 			transformed[index].color0_32 = c0.ToRGBA();
 			transformed[index].color1_32 = c1.ToRGBA();
+
+			// The multiplication by the projection matrix is still performed in the vertex shader.
+			// So is vertex depth rounding, to simulate the 16-bit depth buffer.
 		}
 	}
 

GPU/GLES/GLES_GPU.cpp
@@ -77,8 +77,8 @@ static const CommandTableEntry commandTable[] = {
 	{GE_CMD_FOG2, FLAG_FLUSHBEFOREONCHANGE | FLAG_EXECUTEONCHANGE, DIRTY_FOGCOEF, &GLES_GPU::Execute_FogCoef},
 
 	// Should these maybe flush?
-	{GE_CMD_MINZ, FLAG_FLUSHBEFOREONCHANGE},
-	{GE_CMD_MAXZ, FLAG_FLUSHBEFOREONCHANGE},
+	{GE_CMD_MINZ, FLAG_FLUSHBEFOREONCHANGE, DIRTY_DEPTHRANGE},
+	{GE_CMD_MAXZ, FLAG_FLUSHBEFOREONCHANGE, DIRTY_DEPTHRANGE},
 
 	// Changes that dirty texture scaling.
 	{GE_CMD_TEXMAPMODE, FLAG_FLUSHBEFOREONCHANGE | FLAG_EXECUTEONCHANGE, DIRTY_UVSCALEOFFSET, &GLES_GPU::Execute_TexMapMode},
@@ -206,8 +206,8 @@ static const CommandTableEntry commandTable[] = {
 	{GE_CMD_VIEWPORTY1, FLAG_FLUSHBEFOREONCHANGE | FLAG_EXECUTEONCHANGE, 0, &GLES_GPU::Execute_ViewportType},
 	{GE_CMD_VIEWPORTX2, FLAG_FLUSHBEFOREONCHANGE | FLAG_EXECUTEONCHANGE, 0, &GLES_GPU::Execute_ViewportType},
 	{GE_CMD_VIEWPORTY2, FLAG_FLUSHBEFOREONCHANGE | FLAG_EXECUTEONCHANGE, 0, &GLES_GPU::Execute_ViewportType},
-	{GE_CMD_VIEWPORTZ1, FLAG_FLUSHBEFOREONCHANGE | FLAG_EXECUTEONCHANGE, 0, &GLES_GPU::Execute_ViewportType},
-	{GE_CMD_VIEWPORTZ2, FLAG_FLUSHBEFOREONCHANGE | FLAG_EXECUTEONCHANGE, 0, &GLES_GPU::Execute_ViewportType},
+	{GE_CMD_VIEWPORTZ1, FLAG_FLUSHBEFOREONCHANGE | FLAG_EXECUTEONCHANGE, DIRTY_DEPTHRANGE, &GLES_GPU::Execute_ViewportType},
+	{GE_CMD_VIEWPORTZ2, FLAG_FLUSHBEFOREONCHANGE | FLAG_EXECUTEONCHANGE, DIRTY_DEPTHRANGE, &GLES_GPU::Execute_ViewportType},
 
 	// Region
 	{GE_CMD_REGION1, FLAG_FLUSHBEFOREONCHANGE | FLAG_EXECUTEONCHANGE, 0, &GLES_GPU::Execute_Region},

GPU/GLES/ShaderManager.cpp
@@ -167,6 +167,7 @@ LinkedShader::LinkedShader(Shader *vs, Shader *fs, u32 vertType, bool useHWTrans
 		numBones = TranslateNumBones(vertTypeGetNumBoneWeights(vertType));
 	else
 		numBones = 0;
+	u_depthRange = glGetUniformLocation(program, "u_depthRange");
 
 #ifdef USE_BONE_ARRAY
 	u_bone = glGetUniformLocation(program, "u_bone");
@@ -233,6 +234,8 @@ LinkedShader::LinkedShader(Shader *vs, Shader *fs, u32 vertType, bool useHWTrans
 	if (u_texmtx != -1) availableUniforms |= DIRTY_TEXMATRIX;
 	if (u_stencilReplaceValue != -1) availableUniforms |= DIRTY_STENCILREPLACEVALUE;
 	if (u_blendFixA != -1 || u_blendFixB != -1 || u_fbotexSize != -1) availableUniforms |= DIRTY_SHADERBLEND;
+	if (u_depthRange != -1)
+		availableUniforms |= DIRTY_DEPTHRANGE;
 
 	// Looping up to numBones lets us avoid checking u_bone[i]
 #ifdef USE_BONE_ARRAY
@@ -341,6 +344,10 @@ static void SetFloat24Uniform3(int uniform, const u32 data[3]) {
 		data[0] << 8, data[1] << 8, data[2] << 8
 	};
 	glUniform3fv(uniform, 1, (const GLfloat *)&col[0]);
+}
+
+static void SetFloatUniform4(int uniform, float data[4]) {
+	glUniform4fv(uniform, 1, data);
 }
 
 static void SetMatrix4x3(int uniform, const float *m4x3) {
@@ -547,7 +554,7 @@ void LinkedShader::UpdateUniforms(u32 vertType) {
 		glUniform4fv(u_uvscaleoffset, 1, uvscaleoff);
 	}
 
-	if (dirty & DIRTY_TEXCLAMP) {
+	if ((dirty & DIRTY_TEXCLAMP) && u_texclamp != -1) {
 		const float invW = 1.0f / (float)gstate_c.curTextureWidth;
 		const float invH = 1.0f / (float)gstate_c.curTextureHeight;
 		const int w = gstate.getTextureWidth(0);
@@ -581,6 +588,14 @@ void LinkedShader::UpdateUniforms(u32 vertType) {
 	}
 	if (dirty & DIRTY_TEXMATRIX) {
 		SetMatrix4x3(u_texmtx, gstate.tgenMatrix);
+	}
+	if ((dirty & DIRTY_DEPTHRANGE) && u_depthRange != -1) {
+		float viewZScale = gstate.getViewportZScale();
+		float viewZCenter = gstate.getViewportZCenter();
+		float minZ = gstate.getDepthRangeMin();
+		float maxZ = gstate.getDepthRangeMax();
+		float data[4] = { viewZScale, viewZCenter, minZ, maxZ };
+		SetFloatUniform4(u_depthRange, data);
 	}
 
 	if (dirty & DIRTY_STENCILREPLACEVALUE) {

GPU/GLES/ShaderManager.h
@@ -88,6 +88,8 @@ public:
 	int u_view;
 	int u_texmtx;
 	int u_world;
+	int u_depthRange;   // x,y = viewport xscale/xcenter. z,w=clipping minz/maxz (?)
+
 #ifdef USE_BONE_ARRAY
 	int u_bone;  // array, size is numBones
 #else
@@ -132,10 +134,10 @@ public:
 enum {
 	DIRTY_PROJMATRIX = (1 << 0),
 	DIRTY_PROJTHROUGHMATRIX = (1 << 1),
-	DIRTY_FOGCOLOR	 = (1 << 2),
-	DIRTY_FOGCOEF    = (1 << 3),
-	DIRTY_TEXENV		 = (1 << 4),
-	DIRTY_ALPHACOLORREF	 = (1 << 5),
+	DIRTY_FOGCOLOR = (1 << 2),
+	DIRTY_FOGCOEF = (1 << 3),
+	DIRTY_TEXENV = (1 << 4),
+	DIRTY_ALPHACOLORREF = (1 << 5),
 
 	// 1 << 6 is free! Wait, not anymore...
 	DIRTY_STENCILREPLACEVALUE = (1 << 6),
@@ -155,7 +157,10 @@ enum {
 	DIRTY_SHADERBLEND = (1 << 17),  // Used only for in-shader blending.
 
 	DIRTY_UVSCALEOFFSET = (1 << 18),  // this will be dirtied ALL THE TIME... maybe we'll need to do "last value with this shader compares"
+
+	// Texclamp is fairly rare so let's share it's bit with DIRTY_DEPTHRANGE.
 	DIRTY_TEXCLAMP = (1 << 19),
+	DIRTY_DEPTHRANGE = (1 << 19),
 
 	DIRTY_WORLDMATRIX = (1 << 21),
 	DIRTY_VIEWMATRIX = (1 << 22),  // Maybe we'll fold this into projmatrix eventually

GPU/GLES/VertexShaderGenerator.cpp
@@ -152,6 +152,33 @@ enum DoLightComputation {
 	LIGHT_FULL,
 };
 
+
+// Depth range and viewport
+//
+// After the multiplication with the projection matrix, we have a 4D vector in clip space.
+// In OpenGL, Z is from -1 to 1, while in D3D, Z is from 0 to 1.
+// PSP appears to use the OpenGL convention. As Z is from -1 to 1, and the viewport is represented
+// by a center and a scale, to find the final Z value, all we need to do is to multiply by ZScale and
+// add ZCenter - these are properly scaled to directly give a Z value in [0, 65535].
+//
+// z = vec.z * ViewportZScale + ViewportZCenter;
+//
+// That will give us the final value between 0 and 65535, which we can simply floor to simulate
+// the limited precision of the PSP's depth buffer. Then we convert it back:
+// z = floor(z);
+//
+// vec.z = (z - ViewportZCenter) / ViewportZScale;
+//
+// Now, the regular machinery will take over and do the calculation again.
+//
+// All this above is for full transform mode.
+// In through mode, the Z coordinate just goes straight through and there is no perspective division.
+// We simulate this of course with pretty much an identity matrix. Rounding Z becomes very easy.
+//
+// TODO: Skip all this if we can actually get a 16-bit depth buffer along with stencil, which
+// is a bit of a rare configuration, although quite common on mobile.
+
+
 void GenerateVertexShader(int prim, u32 vertType, char *buffer, bool useHWTransform) {
 	char *p = buffer;
 
@@ -213,6 +240,7 @@ void GenerateVertexShader(int prim, u32 vertType, char *buffer, bool useHWTransf
 		varying = "out";
 		boneWeightDecl = boneWeightInDecl;
 	}
+
 
 	bool lmode = gstate.isUsingSecondaryColor() && gstate.isLightingEnabled() && !gstate.isModeThrough();
 	bool doTexture = gstate.isTextureMapEnabled() && !gstate.isModeClear();
@@ -336,6 +364,10 @@ void GenerateVertexShader(int prim, u32 vertType, char *buffer, bool useHWTransf
 		WRITE(p, "uniform highp vec2 u_fogcoef;\n");
 	}
 
+	if (!gstate.isModeThrough()) {
+		WRITE(p, "uniform highp vec4 u_depthRange;\n");
+	}
+
 	WRITE(p, "%s %s lowp vec4 v_color0;\n", shading, varying);
 	if (lmode) {
 		WRITE(p, "%s %s lowp vec3 v_color1;\n", shading, varying);
@@ -357,11 +389,23 @@ void GenerateVertexShader(int prim, u32 vertType, char *buffer, bool useHWTransf
 		}
 	}
 
-	// Even with this we don't match, because the viewport is different I think.
-	// I think we need to apply viewportZ, round/floor, and then undo viewportZ.
+	// See comment above this function (GenerateVertexShader).
 	WRITE(p, "\nvec4 depthRoundZ(vec4 v) {\n");
 	WRITE(p, "  return vec4(v.x, v.y, (1.0/65536.0) * floor(v.z * 65536.0), v.w);\n");
 	WRITE(p, "}\n\n");
+
+	if (!gstate.isModeThrough()) {
+		// Apply the transform to get the Z buffer value, floor to integer, undo the transform.
+		WRITE(p, "\nvec4 depthRoundZVP(vec4 v) {\n");
+		WRITE(p, "  float z = v.z / v.w;\n");
+		WRITE(p, "  z *= u_depthRange.x;\n");
+		WRITE(p, "  z += u_depthRange.y;\n");
+		WRITE(p, "  z = ceil(z);\n");
+		WRITE(p, "  z -= u_depthRange.y;\n");
+		WRITE(p, "  z /= u_depthRange.x;\n");
+		WRITE(p, "  return vec4(v.x, v.y, z * v.w, v.w);\n");
+		WRITE(p, "}\n\n");
+	}
 
 	WRITE(p, "void main() {\n");
 
@@ -389,7 +433,8 @@ void GenerateVertexShader(int prim, u32 vertType, char *buffer, bool useHWTransf
 		if (gstate.isModeThrough())	{
 			WRITE(p, "  gl_Position = depthRoundZ(u_proj_through * vec4(position.xyz, 1.0));\n");
 		} else {
-			WRITE(p, "  gl_Position = depthRoundZ(u_proj * vec4(position.xyz, 1.0));\n");
+			// The viewport is used in this case, so need to compensate for that.
+			WRITE(p, "  gl_Position = depthRoundZVP(u_proj * vec4(position.xyz, 1.0));\n");
 		}
 	} else {
 		// Step 1: World Transform / Skinning
@@ -482,7 +527,7 @@ void GenerateVertexShader(int prim, u32 vertType, char *buffer, bool useHWTransf
 		WRITE(p, "  vec4 viewPos = u_view * vec4(worldpos, 1.0);\n");
 
 		// Final view and projection transforms.
-		WRITE(p, "  gl_Position = depthRoundZ(u_proj * viewPos);\n");
+		WRITE(p, "  gl_Position = depthRoundZVP(u_proj * viewPos);\n");
 
 		// TODO: Declare variables for dots for shade mapping if needed.
 

commit bc0a694ec2ee7e31476983249da01bedbb2a1527

Unbreak depth range test reporting.
Was broken in 9e18fb7e908a61f3a9b3637ff0fd92ee10ac298b.

GPU/GLES/StateMapping.cpp
@@ -902,14 +902,14 @@ void TransformDrawEngine::ApplyDrawState(int prim) {
 		glstate.depthRange.set(depthRangeMin * (1.0f / 65535.0f), depthRangeMax * (1.0f / 65535.0f));
 
 #ifndef MOBILE_DEVICE
-		float minz = gstate.getDepthRangeMin() * (1.0f / 65535.0f);
-		float maxz = gstate.getDepthRangeMax() * (1.0f / 65535.0f);
+		float minz = gstate.getDepthRangeMin();
+		float maxz = gstate.getDepthRangeMax();
 		if ((minz > depthRangeMin && minz > depthRangeMax) || (maxz < depthRangeMin && maxz < depthRangeMax)) {
-			WARN_LOG_REPORT_ONCE(minmaxz, G3D, "Unsupported depth range test - depth range: %f-%f, test: %f-%f", depthRangeMin, depthRangeMax, minz, maxz);
+			WARN_LOG_REPORT_ONCE(minmaxz, G3D, "Unsupported depth range in test - depth range: %f-%f, test: %f-%f", depthRangeMin, depthRangeMax, minz, maxz);
 		} else if ((gstate.clipEnable & 1) == 0) {
 			// TODO: Need to test whether clipEnable should even affect depth or not.
 			if ((minz < depthRangeMin && minz < depthRangeMax) || (maxz > depthRangeMin && maxz > depthRangeMax)) {
-				WARN_LOG_REPORT_ONCE(znoclip, G3D, "Unsupported depth range test without clipping - depth range: %f-%f, test: %f-%f", depthRangeMin, depthRangeMax, minz, maxz);
+				WARN_LOG_REPORT_ONCE(znoclip, G3D, "Unsupported depth range in test without clipping - depth range: %f-%f, test: %f-%f", depthRangeMin, depthRangeMax, minz, maxz);
 			}
 		}
 #endif