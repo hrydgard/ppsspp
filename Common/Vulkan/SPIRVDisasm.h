0c0378c9d6c995523e98e07c6d2cad673b0ac23a
Henrik Rydgard <hrydgard@gmail.com>
2015-12-31 00:30:52 +0100

    Enable vulkan validation

--- Common/Vulkan/SPIRVDisasm.h
+++ Common/Vulkan/SPIRVDisasm.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 
-// Mostly just a toy to understand SPIR-V better.
+// Mostly just a toy to understand SPIR-V better. Not an accurate SPIR-V disassembler,
+// and the result will not reassemble.
 
 bool DisassembleSPIRV(std::vector<uint32_t> spirv, std::string *output);
