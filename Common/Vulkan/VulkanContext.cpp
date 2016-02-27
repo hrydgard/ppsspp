b06cf4e68846b45cd83f4be5c72873090ce9be15
Henrik Rydgard <hrydgard@gmail.com>
2016-02-28 00:34:50 +0100

    Re-fix depth buffer initialization

--- Common/Vulkan/VulkanContext.cpp
+++ Common/Vulkan/VulkanContext.cpp
@@ -695,8 +695,12 @@ void VulkanContext::InitDepthStencilBuffer(VkCommandBuffer cmd) {
   VkResult U_ASSERT_ONLY res;
   bool U_ASSERT_ONLY pass;
   VkImageCreateInfo image_info = {};
-	const VkFormat depth_format = VK_FORMAT_D16_UNORM;
-  // const VkFormat depth_format = VK_FORMAT_D24_UNORM_S8_UINT;
+	// const VkFormat depth_format = VK_FORMAT_D16_UNORM;
+	// int aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT;
+
+	const VkFormat depth_format = VK_FORMAT_D24_UNORM_S8_UINT;
+	int aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT;
+
   VkFormatProperties props;
 	vkGetPhysicalDeviceFormatProperties(physical_devices_[0], depth_format, &props);
 	if (props.optimalTilingFeatures & VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT) {
@@ -772,7 +776,7 @@ void VulkanContext::InitDepthStencilBuffer(VkCommandBuffer cmd) {
   view_info.components.g = VK_COMPONENT_SWIZZLE_G;
   view_info.components.b = VK_COMPONENT_SWIZZLE_B;
   view_info.components.a = VK_COMPONENT_SWIZZLE_A;
-  view_info.subresourceRange.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT;
+  view_info.subresourceRange.aspectMask = aspectMask;
   view_info.subresourceRange.baseMipLevel = 0;
   view_info.subresourceRange.levelCount = 1;
   view_info.subresourceRange.baseArrayLayer = 0;
