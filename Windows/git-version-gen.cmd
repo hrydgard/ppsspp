de932b5efcfa24dbb31049ecba9e68345a2e8662
Henrik Rydgard <hrydgard@gmail.com>
2016-02-28 11:03:12 +0100

    Make git-version-gen work with a freshly installed 64-bit git msys.
    For some reason %ProgramFiles% returns the (x86) one anyway...

--- Windows/git-version-gen.cmd
+++ Windows/git-version-gen.cmd
@@ -37,7 +37,12 @@ if errorlevel 1 (
 	set GIT="%ProgramFiles(x86)%\Git\bin\git.exe"
 	call !GIT! describe > NUL 2> NUL
 	if errorlevel 1 (
+		echo !GIT!
 		set GIT="%ProgramFiles%\Git\bin\git.exe"
+		call !GIT! describe > NUL 2> NUL
+		if errorlevel 1 (
+			set GIT="C:\Program Files\Git\bin\git.exe"
+		)
 	)
 )
 
