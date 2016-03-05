d32bffb1409bd040262f55f556b1b441a7827239
Henrik Rydgard <hrydgard@gmail.com>
2016-03-05 01:00:59 +0100

    Vulkan: We're doing dual src blending wrong (GLSL part, not sure how to do it right) so disable it.

--- Common/Vulkan/VulkanContext.cpp
+++ Common/Vulkan/VulkanContext.cpp
@@ -613,7 +613,7 @@ VkResult VulkanContext::CreateDevice(int physical_device) {
 
 	// Enable a few safe ones if they are available.
 	if (featuresAvailable_.dualSrcBlend) {
-		featuresEnabled_.dualSrcBlend = true;
+		// featuresEnabled_.dualSrcBlend = true;
 	}
 	if (featuresAvailable_.largePoints) {
 		featuresEnabled_.largePoints = true;
