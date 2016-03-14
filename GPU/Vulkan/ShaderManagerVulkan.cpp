d25eafada2ebb25be8546c0e2e0d87a14a1691d6
Henrik Rydgard <hrydgard@gmail.com>
2016-03-14 20:00:38 +0100

    Matrix depth fix. Still have depth issues though.

--- GPU/Vulkan/ShaderManagerVulkan.cpp
+++ GPU/Vulkan/ShaderManagerVulkan.cpp
@@ -162,7 +162,7 @@ static void ConvertProjMatrixToVulkan(Matrix4x4 &in, bool invertedX, bool invert
 	if (invertedY)
 		yoff = -yoff;
 
-	const Vec3 trans(xoff, yoff, gstate_c.vpZOffset + 0.5f);
+	const Vec3 trans(xoff, yoff, gstate_c.vpZOffset * 0.5f + 0.5f);
 	const Vec3 scale(gstate_c.vpWidthScale, gstate_c.vpHeightScale, gstate_c.vpDepthScale * 0.5f);
 	in.translateAndScale(trans, scale);
 }
