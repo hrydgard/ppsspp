02db45982a3ba4843b340cb729a58cabc6315d82
Unknown W. Brackets <checkins@unknownbrackets.org>
2016-05-27 20:11:25 -0700

    Pass Build.BOARD from Java, avoid lookup.
    Fixes arm64 build with r10, but is ugly coupling.

--- android/src/org/ppsspp/ppsspp/NativeActivity.java
+++ android/src/org/ppsspp/ppsspp/NativeActivity.java
@@ -266,7 +266,7 @@ public class NativeActivity extends Activity implements SurfaceHolder.Callback {
 		String languageRegion = Locale.getDefault().getLanguage() + "_" + Locale.getDefault().getCountry();
 
 		NativeApp.audioConfig(optimalFramesPerBuffer, optimalSampleRate);
-		NativeApp.init(model, deviceType, languageRegion, apkFilePath, dataDir, externalStorageDir, libraryDir, cacheDir, shortcutParam, Build.VERSION.SDK_INT, javaGL);
+		NativeApp.init(model, deviceType, languageRegion, apkFilePath, dataDir, externalStorageDir, libraryDir, cacheDir, shortcutParam, Build.VERSION.SDK_INT, Build.BOARD, javaGL);
 
 		sendInitialGrants();
 
