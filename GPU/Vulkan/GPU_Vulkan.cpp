40e2ab1d12ed89c42e412f17adfbd7048a4bc483
Unknown W. Brackets <checkins@unknownbrackets.org>
2016-10-09 11:27:16 -0700

    Vulkan: Actually call the new lost/restore funcs.

--- GPU/Vulkan/GPU_Vulkan.cpp
+++ GPU/Vulkan/GPU_Vulkan.cpp
@@ -1945,11 +1945,25 @@ void GPU_Vulkan::FastLoadBoneMatrix(u32 target) {
 }
 
 void GPU_Vulkan::DeviceLost() {
-	// TODO
+	framebufferManager_->DeviceLost();
+	drawEngine_.DeviceLost();
+	pipelineManager_->DeviceLost();
+	textureCache_.DeviceLost();
+	depalShaderCache_.Clear();
+	shaderManager_->ClearShaders();
 }
 
 void GPU_Vulkan::DeviceRestore() {
-	// TODO
+	vulkan_ = (VulkanContext *)PSP_CoreParameter().graphicsContext->GetAPIContext();
+	CheckGPUFeatures();
+	BuildReportingInfo();
+	UpdateCmdInfo();
+
+	framebufferManager_->DeviceRestore(vulkan_);
+	drawEngine_.DeviceRestore(vulkan_);
+	pipelineManager_->DeviceRestore(vulkan_);
+	textureCache_.DeviceRestore(vulkan_);
+	shaderManager_->DeviceRestore(vulkan_);
 }
 
 void GPU_Vulkan::GetStats(char *buffer, size_t bufsize) {
