c87d4a66c14bba41463596ad14ed97571226189a
Henrik Rydgard <hrydgard@gmail.com>
2016-10-14 22:33:45 +0200

    Move lang into assets.

--- android/ab.cmd
+++ android/ab.cmd
@@ -1,5 +1,5 @@
 xcopy ..\flash0 assets\flash0 /s /y <d.txt
-xcopy ..\lang assets\lang /s /y <d.txt
+xcopy ..\assets\lang assets\lang /s /y <d.txt
 xcopy ..\assets\shaders assets\shaders /s /y <d.txt
 copy ..\assets\langregion.ini assets\langregion.ini
 copy ..\assets\compat.ini assets\compat.ini
--- android/ab.sh
+++ android/ab.sh
@@ -1,5 +1,5 @@
 cp -r ../flash0 assets
-cp -r ../lang assets
+cp -r ../assets/lang assets
 cp -r ../assets/shaders assets
 cp ../assets/langregion.ini assets/langregion.ini
 cp ../assets/compat.ini assets/compat.ini
