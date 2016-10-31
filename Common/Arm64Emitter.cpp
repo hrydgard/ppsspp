6e7e62f3d5951833e711faa48b0aef985998e563
Henrik Rydgard <hrydgard@gmail.com>
2016-10-31 22:34:15 +0100

    Fix compiler check in Arm64Emitter.cpp. Should help #9104

--- Common/Arm64Emitter.cpp
+++ Common/Arm64Emitter.cpp
@@ -12,6 +12,7 @@
 #include <string.h>
 
 #include "base/basictypes.h"
+#include "ppsspp_config.h"
 
 #include "Arm64Emitter.h"
 #include "MathUtil.h"
@@ -320,7 +321,7 @@ void ARM64XEmitter::FlushIcacheSection(u8* start, u8* end)
 #if defined(IOS)
 	// Header file says this is equivalent to: sys_icache_invalidate(start, end - start);
 	sys_cache_control(kCacheFunctionPrepareForExecution, start, end - start);
-#elif !defined(_M_IX86) && !defined(_M_X64)
+#elif	PPSSPP_ARCH(ARM64)
 	// Code from Dolphin, contributed by the Mono project.
 
 	// Don't rely on GCC's __clear_cache implementation, as it caches
