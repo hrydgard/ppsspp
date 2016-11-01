afe8e2bfb40cda9c2dc3009cf3e7f6d48ffa17d0
Jools Wills <buzz@exotica.org.uk>
2016-11-01 02:45:30 +0000

    Fix building on rpi - #9104
    Check for PPSSPP_ARCH(ARM_NEON) for neon code
    Fix up rpi armv6/armv6 toolchain to work around issue with CMAKE_*_FLAGS not being set.

--- cmake/Toolchains/raspberry.armv6.cmake
+++ cmake/Toolchains/raspberry.armv6.cmake
@@ -8,20 +8,16 @@ include_directories(SYSTEM
   /opt/vc/include/interface/vmcx_host/linux
 )
 
-link_directories(
-  /opt/vc/lib
-)
-
 add_definitions(
   -DPPSSPP_PLATFORM_RPI=1
 )
 
 set(ARCH_FLAGS "-mfpu=vfp -march=armv6j -mfloat-abi=hard")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${ARCH_FLAGS}")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${ARCH_FLAGS}")
-set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} ${ARCH_FLAGS}")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${ARCH_FLAGS}" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${ARCH_FLAGS}" CACHE STRING "" FORCE)
+set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} ${ARCH_FLAGS}" CACHE STRING "" FORCE)
 
-set(OPENGL_LIBRARIES GLESv2 bcm_host)
+set(OPENGL_LIBRARIES /opt/vc/lib/libGLESv2.so /opt/vc/lib/libbcm_host.so)
 set(USING_GLES2 ON)
 set(USING_FBDEV ON)
 
