d5d9d87dc6d6b2a80c319a87183f68de9afffd32
Henrik Rydgård <hrydgard@gmail.com>
2017-01-28 22:11:48 +0100

    Update the shader cache version to avoid obsolete shaders sticking around. Should fix #9249.

--- GPU/GLES/ShaderManagerGLES.cpp
+++ GPU/GLES/ShaderManagerGLES.cpp
@@ -991,7 +991,7 @@ std::string ShaderManagerGLES::DebugGetShaderString(std::string id, DebugShaderT
 // as sometimes these features might have an effect on the ID bits.
 
 #define CACHE_HEADER_MAGIC 0x83277592
-#define CACHE_VERSION 1
+#define CACHE_VERSION 2
 struct CacheHeader {
 	uint32_t magic;
 	uint32_t version;
