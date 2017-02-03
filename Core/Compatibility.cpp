8d37b97b20390bbf951e48362d860ec1728a4679
LunaMoo <moo@moo.moo>
2017-02-03 15:30:22 +0100

    Place the hack into compat.ini

--- Core/Compatibility.cpp
+++ Core/Compatibility.cpp
@@ -50,6 +50,7 @@ void Compatibility::CheckSettings(IniFile &iniFile, const std::string &gameID) {
 	CheckSetting(iniFile, gameID, "DepthRangeHack", &flags_.DepthRangeHack);
 	CheckSetting(iniFile, gameID, "ClearToRAM", &flags_.ClearToRAM);
 	CheckSetting(iniFile, gameID, "Force04154000Download", &flags_.Force04154000Download);
+	CheckSetting(iniFile, gameID, "DrawSyncEatCycles", &flags_.DrawSyncEatCycles);
 }
 
 void Compatibility::CheckSetting(IniFile &iniFile, const std::string &gameID, const char *option, bool *flag) {
