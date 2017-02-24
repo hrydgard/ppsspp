ce7a93e24cf6abed691f17543b4705188e861b28
Fireball Wayne <wleister@gmail.com>
2017-02-23 22:08:13 -0500

    remoteiso gui settings

--- UI/GameSettingsScreen.h
+++ UI/GameSettingsScreen.h
@@ -78,6 +78,7 @@ private:
 	UI::EventReturn OnChangeNickname(UI::EventParams &e);
 	UI::EventReturn OnChangeproAdhocServerAddress(UI::EventParams &e);
 	UI::EventReturn OnChangeMacAddress(UI::EventParams &e);
+	UI::EventReturn OnChangeRemoteISOSubdir(UI::EventParams &e);
 	UI::EventReturn OnClearRecents(UI::EventParams &e);
 	UI::EventReturn OnFullscreenChange(UI::EventParams &e);
 	UI::EventReturn OnDisplayLayoutEditor(UI::EventParams &e);
