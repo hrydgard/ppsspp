5ce7e5e10967774b295ab0e1cacfc614d4bfb00b
Henrik Rydgård <hrydgard@gmail.com>
2017-03-09 14:22:33 +0100

    UWP ARM: Shrink the JIT hack

--- Core/MIPS/ARM/ArmJit.cpp
+++ Core/MIPS/ARM/ArmJit.cpp
@@ -198,13 +198,14 @@ void ArmJit::CompileDelaySlot(int flags)
 void ArmJit::Compile(u32 em_address) {
 	PROFILE_THIS_SCOPE("jitc");
 
-	// If I keep any of these, it works on UWP for ARM!
 #if PPSSPP_PLATFORM(UWP)
-	INFO_LOG(JIT, "Compiling at %08x", em_address);
-#endif
-	// Sleep(1);
+	// INFO_LOG(JIT, "Compiling at %08x", em_address);
 	// Unfortunately Microsoft forgot to expose FlushInstructionCache to UWP applications... even though they expose
-	// the ability to generate code :( This will work great on x86 but on ARM we're out of luck.
+	// the ability to generate code :( This works great on x86 but on ARM we're out of luck.
+	// This seems to be enough to flush the instruction cache:
+	// If this OutputDebugStringUTF8 is not called, we crash.
+	OutputDebugStringUTF8("JITHACK");
+#endif
 
 	if (GetSpaceLeft() < 0x10000 || blocks.IsFull()) {
 		ClearCache();
