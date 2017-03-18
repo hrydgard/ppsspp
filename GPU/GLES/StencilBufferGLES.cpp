78191e645dbff67ed898640622375fac56c1ca24
Unknown W. Brackets <checkins@unknownbrackets.org>
2017-03-18 13:47:49 -0700

    GL: Avoid unnecessary stencil shader binding.
    The comment became untrue, so no need to rebind for each bit.

--- GPU/GLES/StencilBufferGLES.cpp
+++ GPU/GLES/StencilBufferGLES.cpp
@@ -203,8 +203,6 @@ bool FramebufferManagerGLES::NotifyStencilUpload(u32 addr, int size, bool skipZe
 			// It's already zero, let's skip it.
 			continue;
 		}
-		// DrawActiveTexture unbinds it, so rebind here before setting uniforms.
-		glsl_bind(stencilUploadProgram_);
 		if (dstBuffer->format == GE_FORMAT_4444) {
 			glstate.stencilMask.set((i << 4) | i);
 			glUniform1f(u_stencilValue, i * (16.0f / 255.0f));
