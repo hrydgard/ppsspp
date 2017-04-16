89ab11637bce07e380f99b32043a88c63a905b48
zminhquanz <vuminhquan2011@gmail.com>
2017-04-16 12:16:27 +0700

    Add name on credits developer
    My name is in credit

--- UI/MiscScreens.cpp
+++ UI/MiscScreens.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2013- PPSSPP Project.
+﻿// Copyright (c) 2013- PPSSPP Project.
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -644,6 +644,7 @@ void CreditsScreen::render() {
 		"Coldbird",
 		"Kyhel",
 		"xebra",
+		"zminhquanz",
 		"",
 		cr->T("specialthanks", "Special thanks to:"),
 		"Maxim for his amazing Atrac3+ decoder work",
