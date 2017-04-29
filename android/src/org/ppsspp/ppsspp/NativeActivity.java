c3a6d5a39249ef5a38cc387500fbdd39a3bd4d9b
xiushu <461474853@qq.com>
2017-04-30 02:09:52 +0800

    Remove blank line

--- android/src/org/ppsspp/ppsspp/NativeActivity.java
+++ android/src/org/ppsspp/ppsspp/NativeActivity.java
@@ -917,15 +917,12 @@ public class NativeActivity extends Activity implements SurfaceHolder.Callback {
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-
 		if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
 			Uri selectedImage = data.getData();
 			String[] filePathColumn = { MediaStore.Images.Media.DATA };
-
 			Cursor cursor = getContentResolver().query(selectedImage,
 					filePathColumn, null, null, null);
 			cursor.moveToFirst();
-
 			int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
 			String picturePath = cursor.getString(columnIndex);
 			cursor.close();
