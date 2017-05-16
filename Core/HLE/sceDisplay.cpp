70b000bba7e10367838343f5ad2a8a251c48f5e3
Henrik Rydgård <hrydgard@gmail.com>
2017-05-16 12:27:40 +0200

    Remove slowness warning if software renderer is enabled, it'll show on almost everything.

--- Core/HLE/sceDisplay.cpp
+++ Core/HLE/sceDisplay.cpp
@@ -670,7 +670,7 @@ void hleEnterVblank(u64 userdata, int cyclesLate) {
 		// Let the user know if we're running slow, so they know to adjust settings.
 		// Sometimes users just think the sound emulation is broken.
 		static bool hasNotifiedSlow = false;
-		if (!g_Config.bHideSlowWarnings && !hasNotifiedSlow && PSP_CoreParameter().fpsLimit == FPS_LIMIT_NORMAL && IsRunningSlow()) {
+		if (!g_Config.bHideSlowWarnings && !hasNotifiedSlow && PSP_CoreParameter().fpsLimit == FPS_LIMIT_NORMAL && !g_Config.bSoftwareRendering && IsRunningSlow()) {
 #ifndef _DEBUG
 			I18NCategory *err = GetI18NCategory("Error");
 			host->NotifyUserMessage(err->T("Running slow: try frameskip, sound is choppy when slow"), 6.0f, 0xFF30D0D0);
