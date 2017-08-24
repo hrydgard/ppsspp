91a0331f992167c0a36d2aef754a674a8bab7adc
Henrik Rydgård <hrydgard@gmail.com>
2017-08-24 14:57:51 +0200

    Bump the GL shader cache version

--- GPU/GLES/ShaderManagerGLES.cpp
+++ GPU/GLES/ShaderManagerGLES.cpp
@@ -983,7 +983,7 @@ std::string ShaderManagerGLES::DebugGetShaderString(std::string id, DebugShaderT
 // as sometimes these features might have an effect on the ID bits.
 
 #define CACHE_HEADER_MAGIC 0x83277592
-#define CACHE_VERSION 3
+#define CACHE_VERSION 4
 struct CacheHeader {
 	uint32_t magic;
 	uint32_t version;
