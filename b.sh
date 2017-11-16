4b9f3db26002a7a357cb6695906899a4e5f87384
orbea <ovariegata@yahoo.com>
2017-11-15 17:55:24 -0800

    travis: Enable libretro builds for linux.

--- b.sh
+++ b.sh
@@ -33,6 +33,9 @@ do
 		--headless) echo "Headless mode enabled"
 			CMAKE_ARGS="-DHEADLESS=ON ${CMAKE_ARGS}"
 			;;
+		--libretro) echo "Libretro mode enabled"
+			CMAKE_ARGS="-DLIBRETRO=ON ${CMAKE_ARGS}"
+			;;
 		--unittest) echo "Build unittest"
 			CMAKE_ARGS="-DUNITTEST=ON ${CMAKE_ARGS}"
 			;;
