a49885c422c220410a6b72ef660c66e3e1335d30
orbea <ovariegata@yahoo.com>
2018-01-03 17:42:10 -0800

    libretro: Rework PSP_Init.

--- libretro/libretro.cpp
+++ libretro/libretro.cpp
@@ -1186,15 +1186,20 @@ void retro_run(void)
 
 	      bool bootPending_ = !PSP_Init(coreParam, &error_string);
 
-	      if(!bootPending_)
+	      if(PSP_IsIniting())
 	      {
-		      _initialized = !PSP_IsInited();
-		      if (_initialized)
-		      {
-			      if (log_cb)
-				      log_cb(RETRO_LOG_ERROR, "PSP_Init() failed: %s.\n", error_string.c_str());
-			      environ_cb(RETRO_ENVIRONMENT_SHUTDOWN, nullptr);
-		      }
+		     bootPending_ = !PSP_InitUpdate(&error_string);
+
+		     if(!bootPending_)
+		     {
+			_initialized = !PSP_IsInited();
+			if (!_initialized)
+			{
+				if (log_cb)
+					log_cb(RETRO_LOG_ERROR, "PSP_Init() failed: %s.\n", error_string.c_str());
+				environ_cb(RETRO_ENVIRONMENT_SHUTDOWN, nullptr);
+			}
+		     }
 	      }
 
 	      host->BootDone();
