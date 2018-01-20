01223cfaa3b2d09753206d4859cd1abacb1260af
Unknown W. Brackets <checkins@unknownbrackets.org>
2018-01-20 08:28:26 -0800

    D3D11: Remove potentially misleading rebind func.
    This doesn't override the GPUCommon one, it just shares the same name,
    which might lead to confusion later.  Better not to use the parent.

--- GPU/D3D11/FramebufferManagerD3D11.h
+++ GPU/D3D11/FramebufferManagerD3D11.h
@@ -58,7 +58,6 @@ public:
 	void BindFramebufferAsColorTexture(int stage, VirtualFramebuffer *framebuffer, int flags);
 
 	virtual bool NotifyStencilUpload(u32 addr, int size, bool skipZero = false) override;
-	void RebindFramebuffer();
 
 	// TODO: Remove
 	ID3D11Buffer *GetDynamicQuadBuffer() {
