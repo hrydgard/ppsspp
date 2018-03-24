b0a22fb5c7de48bac598b053de6ccc814d2f81a2
Henrik Rydgård <hrydgard@gmail.com>
2018-03-25 00:46:11 +0100

    Redirect the user to download the D3D9 runtime if D3D9 is not available and they don't want to switch to OpenGL.
    Maybe should also update the messagebox text somehow...

--- Windows/EmuThread.cpp
+++ Windows/EmuThread.cpp
@@ -184,10 +184,15 @@ void MainThreadFunc() {
 			g_Config.Save();
 
 			W32Util::ExitAndRestart();
+		} else {
+			if (g_Config.iGPUBackend == (int)GPUBackend::DIRECT3D9) {
+				// Allow the user to download the DX9 runtime.
+				LaunchBrowser("https://www.microsoft.com/en-us/download/details.aspx?id=34429");
+			}
 		}
 
 		// No safe way out without graphics.
-		exit(1);
+		ExitProcess(1);
 	}
 
 	GraphicsContext *graphicsContext = g_graphicsContext;
