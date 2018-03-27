6505f7762c915917e3c0f2be4de6115ba72038c7
Henrik Rydgård <hrydgard@gmail.com>
2018-03-27 16:28:33 +0200

    GCC buildfix (can't use things without definitions in templates).
    Could also have included base/logging.h but meh.

--- Common/Hashmaps.h
+++ Common/Hashmaps.h
@@ -315,7 +315,7 @@ private:
 				Insert(old[i].hash, old[i].value);
 			}
 		}
-		ILOG("Grew hashmap capacity from %d to %d", oldCapacity, capacity_);
+		INFO_LOG(G3D, "Grew hashmap capacity from %d to %d", oldCapacity, capacity_);
 		_assert_msg_(SYSTEM, oldCount == count_, "PrehashMap: count should not change in Grow()");
 	}
 	struct Pair {
