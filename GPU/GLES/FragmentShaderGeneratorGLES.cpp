4ba9fe3e0e35e1b4a993b478aac3a3170e925e71
Henrik Rydgård <hrydgard@gmail.com>
2018-05-13 20:51:20 +0200

    GLES depal (currently disabled): Use high int precision.
    Should fix issue mentioned in https://github.com/hrydgard/ppsspp/commit/fb7a63bd11ba785209babfe9631956110d46c9bf

--- GPU/GLES/FragmentShaderGeneratorGLES.cpp
+++ GPU/GLES/FragmentShaderGeneratorGLES.cpp
@@ -174,6 +174,10 @@ bool GenerateFragmentShader(const FShaderID &id, char *buffer, uint64_t *uniform
 
 	bool isModeClear = id.Bit(FS_BIT_CLEARMODE);
 
+	if (shaderDepal) {
+		WRITE(p, "precision highp int;\n");
+	}
+
 	const char *shading = "";
 	if (glslES30)
 		shading = doFlatShading ? "flat" : "";
