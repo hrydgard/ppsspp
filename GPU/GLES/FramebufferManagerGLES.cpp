87085d18f0850d8ffc2e738bcc2d5b9e6368c004
Unknown W. Brackets <checkins@unknownbrackets.org>
2018-11-22 09:26:12 -0800

    GLES: Fix render-to-self detection.
    Was broken by the recent fb_address changes, so match Vulkan and D3D11
    for consistency.

--- GPU/GLES/FramebufferManagerGLES.cpp
+++ GPU/GLES/FramebufferManagerGLES.cpp
@@ -513,7 +513,7 @@ void FramebufferManagerGLES::BindFramebufferAsColorTexture(int stage, VirtualFra
 	if (GPUStepping::IsStepping() || g_Config.bDisableSlowFramebufEffects) {
 		skipCopy = true;
 	}
-	if (!skipCopy && currentRenderVfb_ && framebuffer->fb_address == gstate.getFrameBufRawAddress()) {
+	if (!skipCopy && framebuffer == currentRenderVfb_) {
 		// TODO: Maybe merge with bvfbs_?  Not sure if those could be packing, and they're created at a different size.
 		Draw::Framebuffer *renderCopy = GetTempFBO(TempFBO::COPY, framebuffer->renderWidth, framebuffer->renderHeight, (Draw::FBColorDepth)framebuffer->colorDepth);
 		if (renderCopy) {
