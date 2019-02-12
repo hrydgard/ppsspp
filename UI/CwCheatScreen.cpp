cbf735e2e2e13038cc75f6545d64fd1820bac0ab
Henrik Rydgård <hrydgard@gmail.com>
2019-02-12 20:18:52 +0100

    CwCheat warning fixes

--- UI/CwCheatScreen.cpp
+++ UI/CwCheatScreen.cpp
@@ -229,7 +229,7 @@ UI::EventReturn CwCheatScreen::OnImportCheat(UI::EventParams &params) {
 					continue;
 				}
 			loop:;
-			} while (fs.good() && (line[0] == '_' && line[1] != 'S') || line[0] == '/' || line[0] == '#');
+			} while (fs.good() && ((line[0] == '_' && line[1] != 'S') || line[0] == '/' || line[0] == '#'));
 			finished = true;
 		}
 		if (finished == true)
@@ -243,7 +243,7 @@ UI::EventReturn CwCheatScreen::OnImportCheat(UI::EventParams &params) {
 	File::OpenCPPFile(fs, activeCheatFile, std::ios::out | std::ios::app);
 
 	auto it = title.begin();
-	if ((title2[0] == '_' && title2[1] != 'S') || title2[0] == '/' || title2[0] == '#' && it != title.end() && (++it) != title.end()) {
+	if (((title2[0] == '_' && title2[1] != 'S') || title2[0] == '/' || title2[0] == '#') && it != title.end() && (++it) != title.end()) {
 		fs << title[0] << "\n" << title[1];
 	}
 
