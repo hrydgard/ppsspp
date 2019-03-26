aaefde41e6f0673db434a8e74b77a54d0407df80
LunaMoo <luna2moo@gmail.com>
2019-03-26 06:12:02 +0100

    Add texture dump/replace hotkeys.

--- Common/KeyMap.cpp
+++ Common/KeyMap.cpp
@@ -672,6 +672,8 @@ const KeyMap_IntStrPair psp_button_names[] = {
 
 	{VIRTKEY_AXIS_SWAP, "AxisSwap"},
 	{VIRTKEY_DEVMENU, "DevMenu"},
+	{VIRTKEY_TEXTURE_DUMP, "Texture Dumping"},
+	{VIRTKEY_TEXTURE_REPLACE, "Texture Replacement"},
 
 	{CTRL_HOME, "Home"},
 	{CTRL_HOLD, "Hold"},
