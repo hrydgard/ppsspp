8d3da2ce8881e16d93712ac8cc933e8f2a314c0a
Unknown W. Brackets <checkins@unknownbrackets.org>
2019-10-06 09:34:23 -0700

    UI: Cleanup browse button check.

--- UI/MainScreen.cpp
+++ UI/MainScreen.cpp
@@ -526,11 +526,11 @@ void GameBrowser::Refresh() {
 		if (allowBrowsing_) {
 			topBar->Add(new Spacer(2.0f));
 			topBar->Add(new TextView(path_.GetFriendlyPath().c_str(), ALIGN_VCENTER | FLAG_WRAP_TEXT, true, new LinearLayoutParams(FILL_PARENT, 64.0f, 1.0f)));
-#if defined(USING_WIN_UI) || defined(USING_QT_UI)
-			topBar->Add(new Choice(mm->T("Browse", "Browse..."), new LayoutParams(WRAP_CONTENT, 64.0f)))->OnClick.Handle(this, &GameBrowser::HomeClick);
-#else
-			topBar->Add(new Choice(mm->T("Home"), new LayoutParams(WRAP_CONTENT, 64.0f)))->OnClick.Handle(this, &GameBrowser::HomeClick);
-#endif
+			if (System_GetPropertyBool(SYSPROP_HAS_FILE_BROWSER)) {
+				topBar->Add(new Choice(mm->T("Browse", "Browse..."), new LayoutParams(WRAP_CONTENT, 64.0f)))->OnClick.Handle(this, &GameBrowser::HomeClick);
+			} else {
+				topBar->Add(new Choice(mm->T("Home"), new LayoutParams(WRAP_CONTENT, 64.0f)))->OnClick.Handle(this, &GameBrowser::HomeClick);
+			}
 		} else {
 			topBar->Add(new Spacer(new LinearLayoutParams(FILL_PARENT, 64.0f, 1.0f)));
 		}
