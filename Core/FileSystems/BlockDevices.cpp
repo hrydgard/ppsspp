1ddc8ac3dfd4a02dd87c4accfbf532a2cf1bd5e5
Henrik Rydgård <hrydgard@gmail.com>
2020-03-28 14:12:42 +0100

    Back to ERROR

--- Core/FileSystems/BlockDevices.cpp
+++ Core/FileSystems/BlockDevices.cpp
@@ -197,7 +197,7 @@ CISOFileBlockDevice::CISOFileBlockDevice(FileLoader *fileLoader)
 	u64 lastIndexPos = index[indexSize - 1] & 0x7FFFFFFF;
 	u64 expectedFileSize = lastIndexPos << indexShift;
 	if (expectedFileSize > fileSize) {
-		WARN_LOG(LOADER, "Expected CSO to at least be %lld bytes, but file is %lld bytes. File: '%s'",
+		ERROR_LOG(LOADER, "Expected CSO to at least be %lld bytes, but file is %lld bytes. File: '%s'",
 			expectedFileSize, fileSize, fileLoader->Path().c_str());
 		NotifyReadError();
 	}
