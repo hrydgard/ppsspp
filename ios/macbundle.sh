bc948b9b943df847557e740e98ea74044b993702
Florin9doi <Florin9doi@users.noreply.github.com>
2020-04-30 20:06:03 +0300

    iOS/macOS : set bundle version

---
+++ ios/macbundle.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PPSSPP="${1}"
+PPSSPPiOS="${PPSSPP}/PPSSPP"
+
+if [ ! -f "${PPSSPPiOS}" ]; then
+  echo "No such file: ${PPSSPPiOS}!"
+  exit 0
+fi
+
+GIT_VERSION_LINE=$(grep "PPSSPP_GIT_VERSION = " "$(dirname "${0}")/../git-version.cpp")
+SHORT_VERSION_MATCH='.*"v([0-9\.]+(-[0-9]+)?).*";'
+LONG_VERSION_MATCH='.*"v(.*)";'
+if [[ "${GIT_VERSION_LINE}" =~ ^${SHORT_VERSION_MATCH}$ ]]; then
+	plutil -replace CFBundleShortVersionString -string $(echo ${GIT_VERSION_LINE} | perl -pe "s/${SHORT_VERSION_MATCH}/\$1/g") ${PPSSPP}/Info.plist
+	plutil -replace CFBundleVersion            -string $(echo ${GIT_VERSION_LINE} | perl -pe "s/${LONG_VERSION_MATCH}/\$1/g")  ${PPSSPP}/Info.plist
+else
+	plutil -replace CFBundleShortVersionString -string "" ${PPSSPP}/Info.plist
+	plutil -replace CFBundleVersion            -string "" ${PPSSPP}/Info.plist
+fi
