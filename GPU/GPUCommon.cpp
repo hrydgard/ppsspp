52a3c2248d2d263df5e000467f1dbff2c45a6ebf
Henrik Rydgård <hrydgard@gmail.com>
2020-05-20 10:25:54 +0200

    Move the check from #12908 into the firmware version check.
    The original change took care of #11660, let's hope it stay fixed. This fixes
    Toca (#12936).

--- GPU/GPUCommon.cpp
+++ GPU/GPUCommon.cpp
@@ -678,17 +678,18 @@ u32 GPUCommon::EnqueueList(u32 listpc, u32 stall, int subIntrBase, PSPPointer<Ps
 		ERROR_LOG_REPORT(G3D, "sceGeListEnqueue: invalid address %08x", listpc);
 		return SCE_KERNEL_ERROR_INVALID_POINTER;
 	}
-	
-	if (args.IsValid() && args->numStacks >= 256) {
-		ERROR_LOG_REPORT(G3D, "sceGeListEnqueue: invalid size %d", args->numStacks);
-		return SCE_KERNEL_ERROR_INVALID_SIZE;
-	}
-	
+
 	int id = -1;
 	u64 currentTicks = CoreTiming::GetTicks();
 	u32_le stackAddr = args.IsValid() ? args->stackAddr : 0;
 	// Check compatibility
 	if (sceKernelGetCompiledSdkVersion() > 0x01FFFFFF) {
+		// See #12908.
+		if (args.IsValid() && args->numStacks >= 256) {
+			ERROR_LOG(G3D, "sceGeListEnqueue: invalid size %08x", args->numStacks);
+			return SCE_KERNEL_ERROR_INVALID_SIZE;
+		}
+
 		//numStacks = 0;
 		//stack = NULL;
 		for (int i = 0; i < DisplayListMaxCount; ++i) {
