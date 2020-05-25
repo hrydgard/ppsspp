0edc3fa8696a124f71953fbfab047ac940122ef4
Unknown W. Brackets <checkins@unknownbrackets.org>
2020-05-25 15:10:55 -0700

    UWP: Fix safe insets.

--- UWP/PPSSPP_UWPMain.cpp
+++ UWP/PPSSPP_UWPMain.cpp
@@ -377,6 +377,11 @@ float System_GetPropertyFloat(SystemProperty prop) {
 	switch (prop) {
 	case SYSPROP_DISPLAY_REFRESH_RATE:
 		return 60.f;
+	case SYSPROP_DISPLAY_SAFE_INSET_LEFT:
+	case SYSPROP_DISPLAY_SAFE_INSET_RIGHT:
+	case SYSPROP_DISPLAY_SAFE_INSET_TOP:
+	case SYSPROP_DISPLAY_SAFE_INSET_BOTTOM:
+		return 0.0f;
 	default:
 		return -1;
 	}
