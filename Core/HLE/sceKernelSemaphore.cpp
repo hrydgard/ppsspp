a897be92c74e1b36de9d4b8ae6d24d08de487763
sum <consignpo@gmail.com>
2020-08-01 20:15:12 +0800

    Imp sceUtilsBufferCopyWithRange and sceUtilsBufferCopyByPollingWithRange (#13206)
    * Imp sceUtilsBufferCopyWithRange
    
    Used in Shin Sangoku Musou 6 Special chinese patach #13205
    
    * Fix sceUtilsBufferCopyByPollingWithRange
    
    * Add logging and reporting
    
    * Fix logging and remove report by @hrydgard

--- Core/HLE/sceKernelSemaphore.cpp
+++ Core/HLE/sceKernelSemaphore.cpp
@@ -26,6 +26,7 @@
 #include "Core/HLE/sceKernelThread.h"
 #include "Core/HLE/sceKernelSemaphore.h"
 #include "Core/HLE/KernelWaitHelpers.h"
+#include "Core/HLE/FunctionWrappers.h"
 
 #define PSP_SEMA_ATTR_FIFO 0
 #define PSP_SEMA_ATTR_PRIORITY 0x100
@@ -457,3 +458,28 @@ int sceKernelPollSema(SceUID id, int wantedCount)
 	}
 }
 
+static u32 hleUtilsBufferCopyWithRange(u32 outAddr, int outSize, u32 inAddr, int inSize, int cmd)
+{
+		int temp = sceUtilsBufferCopyWithRange((u8*)outAddr, outSize, (u8*)inAddr, inSize, cmd);
+		if (temp != 0) {
+			ERROR_LOG(SCEKERNEL, "hleUtilsBufferCopyWithRange: Failed with %d", temp);
+		}		
+		return 0;	
+}
+
+static int sceUtilsBufferCopyByPollingWithRange(u32 outAddr, int outSize, u32 inAddr, int inSize, int cmd)
+{
+	return sceUtilsBufferCopyWithRange((u8*)outAddr, outSize, (u8*)inAddr, inSize, cmd);	
+}
+
+const HLEFunction semaphore[] = {
+	{0x4C537C72, &WrapU_UIUII<hleUtilsBufferCopyWithRange>,                        "sceUtilsBufferCopyWithRange",                   'x', "xixii" },
+	{0x77E97079, &WrapI_UIUII<sceUtilsBufferCopyByPollingWithRange>,               "sceUtilsBufferCopyByPollingWithRange",          'i', "xixii"  },
+	
+};
+
+
+
+void Register_semaphore() {
+	RegisterModule("semaphore", ARRAY_SIZE(semaphore), semaphore);
+}
