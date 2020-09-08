824beb7232838837339fd72ee5b91f0dd8dbfdf6
ANR2ME <anr2000@gmail.com>
2020-09-08 08:30:40 +0700

    Prevent sceNetGetMallocStat from spamming Info Logs

--- Core/HLE/sceNet.cpp
+++ Core/HLE/sceNet.cpp
@@ -712,7 +712,7 @@ static void sceNetEtherStrton(u32 bufferPtr, u32 macPtr) {
 
 // Write static data since we don't actually manage any memory for sceNet* yet.
 static int sceNetGetMallocStat(u32 statPtr) {
-	WARN_LOG(SCENET, "UNTESTED sceNetGetMallocStat(%x)", statPtr);
+	DEBUG_LOG(SCENET, "UNTESTED sceNetGetMallocStat(%x)", statPtr);
 	if(Memory::IsValidAddress(statPtr))
 		Memory::WriteStruct(statPtr, &netMallocStat);
 	else
