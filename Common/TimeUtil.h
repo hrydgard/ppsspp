5411a40f2a8ce23dbf0cd6a8d64512b724c48ed9
Henrik Rydgård <hrydgard@gmail.com>
2020-09-25 09:33:42 +0200

    TimeUtil.cpp cleanup

--- Common/TimeUtil.h
+++ Common/TimeUtil.h
@@ -1,24 +1,7 @@
 #pragma once
 
-// http://linux.die.net/man/3/clock_gettime
-
 // Seconds.
 double time_now_d();
 
 // Sleep. Does not necessarily have millisecond granularity, especially on Windows.
 void sleep_ms(int ms);
-
-// Can be sprinkled around the code to make sure that thing don't take
-// unexpectedly long. What that means is up to you.
-class LoggingDeadline {
-public:
-	LoggingDeadline(const char *name, int deadline_in_ms);
-	~LoggingDeadline();
-	bool End();
-
-private:
-	const char *name_;
-	bool endCalled_;
-	double totalTime_;
-	double endTime_;
-};
