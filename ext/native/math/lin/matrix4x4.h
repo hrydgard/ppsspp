ec7458b6a939b8ec2c73f6a2126d5f57c3fa0ffc
Henrik Rydgård <hrydgard@gmail.com>
2020-09-29 10:13:27 +0200

    Delete more useless math code

--- ext/native/math/lin/matrix4x4.h
+++ ext/native/math/lin/matrix4x4.h
@@ -24,19 +24,6 @@ public:
 	const Vec3 front() const {return Vec3(zx, zy, zz);}
 	const Vec3 move()	const {return Vec3(wx, wy, wz);}
 
-	void setRight(const Vec3 &v) {
-		xx = v.x; xy = v.y; xz = v.z;
-	}
-	void setUp(const Vec3 &v) {
-		yx = v.x; yy = v.y; yz = v.z;
-	}
-	void setFront(const Vec3 &v) {
-		zx = v.x; zy = v.y; zz = v.z;
-	}
-	void setMove(const Vec3 &v) {
-		wx = v.x; wy = v.y; wz = v.z;
-	}
-
 	const float &operator[](int i) const {
 		return *(((const float *)this) + i);
 	}
@@ -53,17 +40,14 @@ public:
 	void empty() {
 		memset(this, 0, 16 * sizeof(float));
 	}
-	void setScaling(const float f) {
-		empty();
-		xx=yy=zz=f; ww=1.0f;
-	}
 	static Matrix4x4 identity() {
 		Matrix4x4 id;
 		id.setIdentity();
 		return id;
 	}
 	void setIdentity() {
-		setScaling(1.0f);
+		empty();
+		xx = yy = zz = ww = 1.0f;
 	}
 	void setTranslation(const Vec3 &trans) {
 		setIdentity();
@@ -72,37 +56,8 @@ public:
 		wz = trans.z;
 	}
 
-	Matrix4x4 inverse() const;
-	Matrix4x4 simpleInverse() const;
 	Matrix4x4 transpose() const;
 
-	void setRotationX(const float a) {
-		empty();
-		float c = cosf(a);
-		float s = sinf(a);
-		xx = 1.0f;
-		yy = c; yz = s;
-		zy = -s; zz = c;
-		ww = 1.0f;
-	}
-	void setRotationY(const float a)	 {
-		empty();
-		float c = cosf(a);
-		float s = sinf(a);
-		xx = c; xz = -s;
-		yy = 1.0f;
-		zx = s; zz = c;
-		ww = 1.0f;
-	}
-	void setRotationZ(const float a)	 {
-		empty();
-		float c = cosf(a);
-		float s = sinf(a);
-		xx = c; xy = s;
-		yx = -s; yy = c;
-		zz = 1.0f; 
-		ww = 1.0f;
-	}
 	// Exact angles to avoid any artifacts.
 	void setRotationZ90() {
 		empty();
@@ -132,15 +87,10 @@ public:
 		ww = 1.0f;
 	}
 
-	void setRotation(float x,float y, float z);
-	void setProjection(float near_plane, float far_plane, float fov_horiz, float aspect = 0.75f);
-	void setProjectionD3D(float near_plane, float far_plane, float fov_horiz, float aspect = 0.75f);
 	void setOrtho(float left, float right, float bottom, float top, float near, float far);
 	void setOrthoD3D(float left, float right, float bottom, float top, float near, float far);
 	void setOrthoVulkan(float left, float right, float top, float bottom, float near, float far);
 
-	void setViewLookAt(const Vec3 &from, const Vec3 &at, const Vec3 &worldup);
-	void setViewLookAtD3D(const Vec3 &from, const Vec3 &at, const Vec3 &worldup);
 	void setViewFrame(const Vec3 &pos, const Vec3 &right, const Vec3 &forward, const Vec3 &up);
 	void toText(char *buffer, int len) const;
 	void print() const;
