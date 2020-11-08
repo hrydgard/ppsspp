0c3ead1437e3b8da9ba267324d59a5731c376e45
Henrik Rydgård <hrydgard@gmail.com>
2020-11-09 00:30:24 +0100

    Temporarily disable GL frame-init error checks, see #13639

--- Common/GPU/OpenGL/GLQueueRunner.cpp
+++ Common/GPU/OpenGL/GLQueueRunner.cpp
@@ -385,7 +385,7 @@ void GLQueueRunner::RunInitSteps(const std::vector<GLRInitStep> &steps, bool ski
 	CHECK_GL_ERROR_IF_DEBUG();
 
 	// TODO: Use GL_KHR_no_error or a debug callback, where supported?
-	if (allocatedTextures) {
+	if (false && allocatedTextures) {
 		// Users may use replacements or scaling, with high render resolutions, and run out of VRAM.
 		// This detects that, rather than looking like PPSSPP is broken.
 		// Calling glGetError() isn't great, but at the end of init, only after creating textures, shouldn't be too bad...
