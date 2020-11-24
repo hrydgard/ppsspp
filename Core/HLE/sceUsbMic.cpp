6afeed877c9b9573477cb78e028d534112ae1bcd
shenweip <1037567878@qq.com>
2020-11-24 21:36:17 +0800

    Build fixes

--- Core/HLE/sceUsbMic.cpp
+++ Core/HLE/sceUsbMic.cpp
@@ -15,6 +15,7 @@
 // Official git repository and contact information can be found at
 // https://github.com/hrydgard/ppsspp and http://www.ppsspp.org/.
 
+#include <algorithm>
 #include <mutex>
 
 #include "Common/Serialize/Serializer.h"
