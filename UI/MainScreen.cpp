c546c603247bac4646fdc128756c577161911bc5
Henrik Rydgård <hrydgard@gmail.com>
2020-12-13 10:29:12 +0100

    Call g_Config.Save() first thing in MainScreen::OnExit. Workaround for #13286

--- UI/MainScreen.cpp
+++ UI/MainScreen.cpp
@@ -1369,13 +1369,11 @@ UI::EventReturn MainScreen::OnForums(UI::EventParams &e) {
 }
 
 UI::EventReturn MainScreen::OnExit(UI::EventParams &e) {
-	System_SendMessage("event", "exitprogram");
+	// Let's make sure the config was saved, since it may not have been.
+	g_Config.Save("MainScreen::OnExit");
 
 	// Request the framework to exit cleanly.
 	System_SendMessage("finish", "");
-
-	// However, let's make sure the config was saved, since it may not have been.
-	g_Config.Save("MainScreen::OnExit");
 
 #ifdef __ANDROID__
 #ifdef ANDROID_NDK_PROFILER
