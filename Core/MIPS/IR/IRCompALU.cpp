d3c06266c569775ea51290773bae4c931132a247
Unknown W. Brackets <checkins@unknownbrackets.org>
2021-01-09 11:50:32 -0800

    jit: Fix conditional disable flags.

--- Core/MIPS/IR/IRCompALU.cpp
+++ Core/MIPS/IR/IRCompALU.cpp
@@ -37,7 +37,7 @@
 // All functions should have CONDITIONAL_DISABLE, so we can narrow things down to a file quickly.
 // Currently known non working ones should have DISABLE.
 
-// #define CONDITIONAL_DISABLE { Comp_Generic(op); return; }
+// #define CONDITIONAL_DISABLE(flag) { Comp_Generic(op); return; }
 #define CONDITIONAL_DISABLE(flag) if (opts.disableFlags & (uint32_t)JitDisable::flag) { Comp_Generic(op); return; }
 #define DISABLE { Comp_Generic(op); return; }
 #define INVALIDOP { Comp_Generic(op); return; }
