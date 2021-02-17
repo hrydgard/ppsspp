f448e8e923eb6c6ac672369cc943901b4a0174cd
Unknown W. Brackets <checkins@unknownbrackets.org>
2021-02-17 00:32:58 -0800

    UI: Divert background only on change.
    To avoid triggering a transition and event.

--- Common/UI/View.cpp
+++ Common/UI/View.cpp
@@ -201,7 +201,7 @@ void Clickable::DrawBG(UIContext &dc, const Style &style) {
 	if (style.background.type == DRAW_SOLID_COLOR) {
 		if (time_now_d() - bgColorLast_ >= 0.25f) {
 			bgColor_->Reset(style.background.color);
-		} else {
+		} else if (bgColor_->CurrentValue() != style.background.color) {
 			bgColor_->Divert(style.background.color, down_ ? 0.05f : 0.1f);
 		}
 		bgColorLast_ = time_now_d();
