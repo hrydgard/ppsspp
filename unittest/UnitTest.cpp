9a318673a7a3bb83755a650a7a21bb446e0bb7c8
Sean McGovern <gseanmcg@gmail.com>
2021-02-18 00:27:45 -0500

    UnitTest: call InitVFPUSinCos() to set vfpu_* function pointers

--- unittest/UnitTest.cpp
+++ unittest/UnitTest.cpp
@@ -298,6 +298,7 @@ bool TestParsers() {
 
 bool TestVFPUSinCos() {
 	float sine, cosine;
+	InitVFPUSinCos(false);
 	EXPECT_FALSE(vfpu_sincos == nullptr);
 	vfpu_sincos(0.0f, sine, cosine);
 	EXPECT_EQ_FLOAT(sine, 0.0f);
