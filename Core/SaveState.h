8c655750fee274cc93289c453f6cd68b99358836
Unknown W. Brackets <checkins@unknownbrackets.org>
2021-02-21 08:18:13 -0800

    SaveState: Delay restart handling until end frame.
    Otherwise, we end up having unmatched pushbuffer map/unmap.

--- Core/SaveState.h
+++ Core/SaveState.h
@@ -93,4 +93,7 @@ namespace SaveState
 
 	// Check if there's any save stating needing to be done.  Normally called once per frame.
 	void Process();
+
+	// Cleanup by triggering a restart if needed.
+	void Cleanup();
 };
