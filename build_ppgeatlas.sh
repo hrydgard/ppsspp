c5db0018cfe51bbb03ea2074c942d2017d01f3b8
LunaMoo <luna2moo@gmail.com>
2021-06-03 08:48:35 +0200

    Update zim files with new atlastool fixed.

--- build_ppgeatlas.sh
+++ build_ppgeatlas.sh
@@ -1,1 +1,2 @@
 ./ext/native/tools/build/atlastool ppge_atlasscript.txt ppge && rm ppge_atlas_luna.cpp ppge_atlas_luna.h && cp ppge_atlas_luna.zim assets && cp ppge_atlas_luna.meta assets && mv ppge_atlas_luna.zim ppge_atlas_luna.meta android/assets
+$SHELL
--- buildatlas.sh
+++ buildatlas.sh
@@ -1,10 +1,3 @@
-./ext/native/tools/build/atlastool atlasscript.txt ui 8888 && cp ui_atlas.zim ui_atlas.meta assets && cp ui_atlas.zim ui_atlas.meta android/assets && rm ui_atlas.cpp ui_atlas.h
-
-mv assets/ui_atlas.zim assets/ui_atlas_luna.zim
-mv assets/ui_atlas.meta assets/ui_atlas_luna.meta
-mv android/assets/ui_atlas.zim android/assets/ui_atlas_luna.zim
-mv android/assets/ui_atlas.meta android/assets/ui_atlas_luna.meta
-
-rm ui_atlas.zim ui_atlas.meta
-pause
+./ext/native/tools/build/atlastool atlasscript.txt ui 8888 && cp ui_atlas_luna.zim ui_atlas_luna.meta assets && cp ui_atlas_luna.zim ui_atlas_luna.meta android/assets && rm ui_atlas_luna.cpp ui_atlas_luna.h
+rm ui_atlas_luna.zim ui_atlas_luna.meta
 $SHELL
