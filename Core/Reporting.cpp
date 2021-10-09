d9b6e847deb84c1b54e8ef1a1666041e8e86c463
Unknown W. Brackets <checkins@unknownbrackets.org>
2021-10-09 10:55:42 -0700

    Reporting: Require I/O on thread to be on.
    This will also cause a purple crash screen if it's off.

--- Core/Reporting.cpp
+++ Core/Reporting.cpp
@@ -527,6 +527,8 @@ namespace Reporting
 			return false;
 		if (g_Config.uJitDisableFlags != 0)
 			return false;
+		if (!g_Config.bSeparateIOThread)
+			return false;
 		// Don't allow builds without version info from git.  They're useless for reporting.
 		if (strcmp(PPSSPP_GIT_VERSION, "unknown") == 0)
 			return false;
