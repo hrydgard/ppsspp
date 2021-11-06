09c4539d686b6431bd5a3435f4665a1964ecebb1
Henrik Rydgård <hrydgard@gmail.com>
2021-11-06 18:10:41 +0100

    Allow repeat draw detection when vertex cache is on. Even faster.

--- GPU/Vulkan/DrawEngineVulkan.cpp
+++ GPU/Vulkan/DrawEngineVulkan.cpp
@@ -580,7 +580,10 @@ void DrawEngineVulkan::DoFlush() {
 			useCache = false;
 		}
 
-		if (useCache) {
+		if (useCache && dcid_ == prevDcid_ && g_Config.bOptimizeRepeatDraws) {
+			reuseVertexData = true;
+			gpuStats.numRepeatDraws++;
+		} else if (useCache) {
 			PROFILE_THIS_SCOPE("vcache");
 			u32 id = dcid_ ^ gstate.getUVGenMode();  // This can have an effect on which UV decoder we need to use! And hence what the decoded data will look like. See #9263
 			VertexArrayInfoVulkan *vai = vai_.Get(id);
