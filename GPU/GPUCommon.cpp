be8c74cabea63280b0127b2c161048651406115d
Unknown W. Brackets <checkins@unknownbrackets.org>
2022-01-31 19:32:46 -0800

    softgpu: Avoid flush on END.
    We only, but always, flush when exiting list interp in FinishDeferred.
    It's not necessary at END, and hurts for simple signals that don't stop
    list processing.

--- GPU/GPUCommon.cpp
+++ GPU/GPUCommon.cpp
@@ -1348,7 +1348,8 @@ void GPUCommon::Execute_Ret(u32 op, u32 diff) {
 }
 
 void GPUCommon::Execute_End(u32 op, u32 diff) {
-	Flush();
+	if (flushOnParams_)
+		Flush();
 
 	const u32 prev = Memory::ReadUnchecked_U32(currentList->pc - 4);
 	UpdatePC(currentList->pc, currentList->pc);
@@ -2408,7 +2409,8 @@ void GPUCommon::FastLoadBoneMatrix(u32 target) {
 	}
 
 	if (!g_Config.bSoftwareSkinning) {
-		Flush();
+		if (flushOnParams_)
+			Flush();
 		gstate_c.Dirty(uniformsToDirty);
 	} else {
 		gstate_c.deferredVertTypeDirty |= uniformsToDirty;
