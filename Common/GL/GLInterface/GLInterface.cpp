d443c1bb30bf25bfc7f2ca64233562db9b372c45
Unknown W. Brackets <checkins@unknownbrackets.org>
2022-04-02 16:37:04 -0700

    Switch: Fix GL build typo.

--- Common/GL/GLInterface/GLInterface.cpp
+++ Common/GL/GLInterface/GLInterface.cpp
@@ -26,7 +26,7 @@
 cInterfaceBase* HostGL_CreateGLInterface(){
 	#ifdef __ANDROID__
 		return new cInterfaceEGLAndroid;
-	#elif if PPSSPP_PLATFORM(SWITCH)
+	#elif PPSSPP_PLATFORM(SWITCH)
 		return new cInterfaceEGLSwitch;
 	#elif defined(__APPLE__)
 		return new cInterfaceAGL;
