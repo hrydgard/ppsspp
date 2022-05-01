c3f48ecd6ff8df1ae75db843b55d5c7d6c0acbc4
Henrik Rydgård <hrydgard@gmail.com>
2022-05-01 11:56:18 +0200

    Remove log output

--- Common/GPU/Vulkan/VulkanBarrier.cpp
+++ Common/GPU/Vulkan/VulkanBarrier.cpp
@@ -4,9 +4,6 @@
 
 void VulkanBarrier::Flush(VkCommandBuffer cmd) {
 	if (!imageBarriers_.empty()) {
-		if (imageBarriers_.size() >= 3) {
-			NOTICE_LOG(G3D, "barriers: %d", (int)imageBarriers_.size());
-		}
 		vkCmdPipelineBarrier(cmd, srcStageMask_, dstStageMask_, 0, 0, nullptr, 0, nullptr, (uint32_t)imageBarriers_.size(), imageBarriers_.data());
 	}
 	imageBarriers_.clear();
