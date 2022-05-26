43c54cf5d9a4c46c0eea64c8804d25ef52897c39
iota97 <giovi.cocco@gmail.com>
2022-05-26 07:07:59 +0200

    Fix themes as well

--- UI/Theme.cpp
+++ UI/Theme.cpp
@@ -96,6 +96,9 @@ static void LoadThemeInfo(const std::vector<Path> &directories) {
 		for (size_t f = 0; f < fileInfo.size(); f++) {
 			IniFile ini;
 			bool success = false;
+			if (fileInfo[f].isDirectory)
+				continue;
+
 			Path name = fileInfo[f].fullName;
 			Path path = directories[d];
 			// Hack around Android VFS path bug. really need to redesign this.
