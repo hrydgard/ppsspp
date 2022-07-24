89845eae7a2d7b9a3373c7ee86b94d776a8c792d
Henrik Rydgård <hrydgard@gmail.com>
2022-07-24 17:12:43 +0200

    Add GPU stat for number of depth copies per frame
    Will be useful for evaluating #15700

--- GPU/GPUCommon.cpp
+++ GPU/GPUCommon.cpp
@@ -3029,7 +3029,7 @@ size_t GPUCommon::FormatGPUStatsCommon(char *buffer, size_t size) {
 		"Vertices: %d cached: %d uncached: %d\n"
 		"FBOs active: %d (evaluations: %d)\n"
 		"Textures: %d, dec: %d, invalidated: %d, hashed: %d kB\n"
-		"Readbacks: %d, uploads: %d\n"
+		"Readbacks: %d, uploads: %d, depth copies: %d\n"
 		"GPU cycles executed: %d (%f per vertex)\n",
 		gpuStats.msProcessingDisplayLists * 1000.0f,
 		gpuStats.numDrawCalls,
@@ -3049,6 +3049,7 @@ size_t GPUCommon::FormatGPUStatsCommon(char *buffer, size_t size) {
 		gpuStats.numTextureDataBytesHashed / 1024,
 		gpuStats.numReadbacks,
 		gpuStats.numUploads,
+		gpuStats.numDepthCopies,
 		gpuStats.vertexGPUCycles + gpuStats.otherGPUCycles,
 		vertexAverageCycles
 	);
