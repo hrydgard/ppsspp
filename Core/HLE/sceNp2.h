31ea7a6a9517635c79876b208f45badbd9c14419
ANR2ME <anr2000@gmail.com>
2022-08-04 16:34:05 +0700

    Updated copyright's year.

--- Core/HLE/sceNp2.h
+++ Core/HLE/sceNp2.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2012- PPSSPP Project.
+// Copyright (c) 2022- PPSSPP Project.
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
