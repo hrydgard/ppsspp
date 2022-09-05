d3501d0a7c0d680d13bfdd8114484d52a98e127a
Unknown W. Brackets <checkins@unknownbrackets.org>
2022-09-04 20:56:33 -0700

    Build: Update xxhash to v0.8.1.

--- ext/xxhash.c
+++ ext/xxhash.c
@@ -33,9 +33,9 @@
  */
 
 
- /*
-  * xxhash.c instantiates functions defined in xxhash.h
-  */
+/*
+ * xxhash.c instantiates functions defined in xxhash.h
+ */
 
 #define XXH_STATIC_LINKING_ONLY   /* access advanced declarations */
 #define XXH_IMPLEMENTATION   /* access definitions */
