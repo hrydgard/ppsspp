9ec41436d16c8d11d1bc4e9dd15482dffe7faa74
Henrik Rydgård <hrydgard@gmail.com>
2022-10-01 19:06:02 +0200

    ES2 crash fix: Don't draw depth if lacking fragment shader depth write.

--- GPU/Common/FramebufferManagerCommon.cpp
+++ GPU/Common/FramebufferManagerCommon.cpp
@@ -575,7 +575,7 @@ void FramebufferManagerCommon::SetDepthFrameBuffer(bool isClearingDepth) {
 
 		// Need to upload the first line of depth buffers, for Burnout Dominator lens flares. See issue #11100 and comments to #16081.
 		// Might make this more generic and upload the whole depth buffer if we find it's needed for something.
-		if (newlyUsingDepth) {
+		if (newlyUsingDepth && draw_->GetDeviceCaps().fragmentShaderDepthWriteSupported) {
 			// Sanity check the depth buffer pointer.
 			if (Memory::IsValidRange(currentRenderVfb_->z_address, currentRenderVfb_->width * 2)) {
 				const u16 *src = (const u16 *)Memory::GetPointerUnchecked(currentRenderVfb_->z_address);
