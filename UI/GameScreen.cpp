d7a6ce47a921f9ccf0edd9f5fc30dc6cf2adcf2c
sum2012 <consignpo@gmail.com>
2022-10-03 21:00:16 +0800

    Change int2hexstr method

--- UI/GameScreen.cpp
+++ UI/GameScreen.cpp
@@ -44,6 +44,7 @@
 #include "UI/MainScreen.h"
 #include "UI/BackgroundAudio.h"
 #include "Core/Reporting.h"
+#include <sstream>
 
 GameScreen::GameScreen(const Path &gamePath) : UIDialogScreenWithGameBackground(gamePath) {
 	g_BackgroundAudio.SetGame(gamePath);
@@ -55,12 +56,10 @@ GameScreen::~GameScreen() {
 	}
 }
 
-template <typename I> std::string int2hexstr(I w, size_t hex_len = sizeof(I) << 1) {
-	static const char* digits = "0123456789ABCDEF";
-	std::string rc(hex_len, '0');
-	for (size_t i = 0, j = (hex_len - 1) * 4; i < hex_len; ++i, j -= 4)
-		rc[i] = digits[(w >> j) & 0x0f];
-	return rc;
+std::string int2hexstr(const int a) {
+	std::stringstream stream;
+	stream << std::hex << a;
+	return stream.str();
 }
 
 void GameScreen::update() {
