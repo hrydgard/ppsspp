22980c8b9f51d944a0f36cab4f3c40c71d45ae02
Lubos <tridosm@gmail.com>
2022-11-05 10:16:24 +0100

    Code formatting fixed

--- Common/VR/VRRenderer.cpp
+++ Common/VR/VRRenderer.cpp
@@ -152,8 +152,7 @@ void VR_Recenter(engine_t* engine) {
 	// Create a default stage space to use if SPACE_TYPE_STAGE is not
 	// supported, or calls to xrGetReferenceSpaceBoundsRect fail.
 	spaceCreateInfo.referenceSpaceType = XR_REFERENCE_SPACE_TYPE_LOCAL;
-	if (VR_GetPlatformFLag(VR_PLATFORM_TRACKING_FLOOR))
-	{
+	if (VR_GetPlatformFLag(VR_PLATFORM_TRACKING_FLOOR)) {
 		spaceCreateInfo.poseInReferenceSpace.position.y = -1.6750f;
 	}
 	OXR(xrCreateReferenceSpace(engine->appState.Session, &spaceCreateInfo, &engine->appState.FakeStageSpace));
@@ -165,8 +164,7 @@ void VR_Recenter(engine_t* engine) {
 		spaceCreateInfo.poseInReferenceSpace.position.y = 0.0;
 		OXR(xrCreateReferenceSpace(engine->appState.Session, &spaceCreateInfo, &engine->appState.StageSpace));
 		ALOGV("Created stage space");
-		if (VR_GetPlatformFLag(VR_PLATFORM_TRACKING_FLOOR))
-		{
+		if (VR_GetPlatformFLag(VR_PLATFORM_TRACKING_FLOOR)) {
 			engine->appState.CurrentSpace = engine->appState.StageSpace;
 		}
 	}
