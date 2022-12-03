e50eae1bfbb46dd0922f62e57bbb38a9f47725ee
Unknown W. Brackets <checkins@unknownbrackets.org>
2022-12-03 07:17:12 -0800

    GPU: Cleanup some bounds checks, assignments.

--- GPU/Directx9/StateMappingDX9.cpp
+++ GPU/Directx9/StateMappingDX9.cpp
@@ -99,7 +99,7 @@ void DrawEngineDX9::ApplyDrawState(int prim) {
 	if (!gstate.isModeClear()) {
 		textureCache_->ApplyTexture();
 
-		if (fboTexBindState_ = FBO_TEX_COPY_BIND_TEX) {
+		if (fboTexBindState_ == FBO_TEX_COPY_BIND_TEX) {
 			// Note that this is positions, not UVs, that we need the copy from.
 			framebufferManager_->BindFramebufferAsColorTexture(1, framebufferManager_->GetCurrentRenderVFB(), BINDFBCOLOR_MAY_COPY, 0);
 			// If we are rendering at a higher resolution, linear is probably best for the dest color.
