3dbe69f585cce75d3a1a7994265e1ad588258d29
Henrik Rydgård <hrydgard@gmail.com>
2023-01-04 10:17:59 +0100

    Cache version

--- GPU/GLES/ShaderManagerGLES.cpp
+++ GPU/GLES/ShaderManagerGLES.cpp
@@ -938,7 +938,7 @@ std::string ShaderManagerGLES::DebugGetShaderString(std::string id, DebugShaderT
 // as sometimes these features might have an effect on the ID bits.
 
 #define CACHE_HEADER_MAGIC 0x83277592
-#define CACHE_VERSION 17
+#define CACHE_VERSION 18
 struct CacheHeader {
 	uint32_t magic;
 	uint32_t version;
--- GPU/Vulkan/ShaderManagerVulkan.cpp
+++ GPU/Vulkan/ShaderManagerVulkan.cpp
@@ -499,7 +499,7 @@ VulkanGeometryShader *ShaderManagerVulkan::GetGeometryShaderFromModule(VkShaderM
 // instantaneous.
 
 #define CACHE_HEADER_MAGIC 0xff51f420 
-#define CACHE_VERSION 32
+#define CACHE_VERSION 33
 struct VulkanCacheHeader {
 	uint32_t magic;
 	uint32_t version;
