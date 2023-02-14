6b0cec2ae0cd9e5133213c88811007dd7c71e74e
Serena <osama.id78@gmail.com>
2023-02-14 19:11:44 +0300

    i forgot this

---
+++ SDL/CocoaBarItems.h
@@ -0,0 +1,18 @@
+//
+//  CocoaBarItems.h
+//  PPSSPP
+//
+//  Created by Serena on 06/02/2023.
+//
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void initBarItemsForApp();
+
+#ifdef __cplusplus
+}
+#endif
