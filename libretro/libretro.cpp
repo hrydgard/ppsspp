fd47db7bcd0a628fa409391a684923b1da2c02e5
Henrik Rydgård <hrydgard@gmail.com>
2023-03-24 16:26:06 +0100

    libretro: try a different approach for the temporary solution

--- libretro/libretro.cpp
+++ libretro/libretro.cpp
@@ -32,6 +32,7 @@
 #include "Core/HLE/sceUtility.h"
 #include "Core/HLE/__sceAudio.h"
 #include "Core/HW/MemoryStick.h"
+#include "Core/HW/StereoResampler.h"
 #include "Core/Host.h"
 #include "Core/MemMap.h"
 #include "Core/System.h"
@@ -1879,6 +1880,38 @@ void NativeRender(GraphicsContext *graphicsContext) {}
 void NativeResized() {}
 
 void System_Toast(const char *str) {}
+
+
+// Temporary, to keep the old behavior before changing it.
+
+StereoResampler g_resampler;
+
+// numFrames is number of stereo frames.
+// This is called from *outside* the emulator thread.
+int __AudioMix(int16_t *outstereo, int numFrames, int sampleRate) {
+   return g_resampler.Mix(outstereo, numFrames, false, sampleRate);
+}
+
+void System_AudioGetDebugStats(char *buf, size_t bufSize) {
+   if (buf) {
+      g_resampler.GetAudioDebugStats(buf, bufSize);
+   } else {
+      g_resampler.ResetStatCounters();
+   }
+}
+
+void System_AudioClear() {
+   g_resampler.Clear();
+}
+
+void System_AudioPushSamples(const int32_t *audio, int numSamples) {
+   if (audio) {
+      g_resampler.PushSamples(audio, numSamples);
+   } else {
+      g_resampler.Clear();
+   }
+}
+
 
 #if PPSSPP_PLATFORM(ANDROID) || PPSSPP_PLATFORM(IOS)
 std::vector<std::string> System_GetCameraDeviceList() { return std::vector<std::string>(); }
