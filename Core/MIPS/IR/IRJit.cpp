e4f9c72fe9b01cc984dbfa7538ebad5babad2ddb
Unknown W. Brackets <checkins@unknownbrackets.org>
2023-07-16 16:20:58 -0700

    riscv: Avoid unaligned mem combine in IR.

--- Core/MIPS/IR/IRJit.cpp
+++ Core/MIPS/IR/IRJit.cpp
@@ -15,6 +15,7 @@
 // Official git repository and contact information can be found at
 // https://github.com/hrydgard/ppsspp and http://www.ppsspp.org/.
 
+#include "ppsspp_config.h"
 #include <set>
 
 #include "ext/xxhash.h"
@@ -49,7 +50,10 @@ IRJit::IRJit(MIPSState *mipsState) : frontend_(mipsState->HasDefaultPrefix()), m
 
 	IROptions opts{};
 	opts.disableFlags = g_Config.uJitDisableFlags;
+	// Assume that RISC-V always has very slow unaligned memory accesses.
+#if !PPSSPP_ARCH(RISCV64)
 	opts.unalignedLoadStore = (opts.disableFlags & (uint32_t)JitDisable::LSU_UNALIGNED) == 0;
+#endif
 	frontend_.SetOptions(opts);
 }
 
