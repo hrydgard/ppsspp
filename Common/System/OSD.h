d268dd639a46727ca3c086224be3377fdaea1e9d
Henrik Rydgård <hrydgard@gmail.com>
2023-08-01 01:01:57 +0200

    Move the ScreenEdgePosition enum to a more appropriate location

--- Common/System/OSD.h
+++ Common/System/OSD.h
@@ -27,18 +27,6 @@ enum class OSDType {
 	VALUE_COUNT,
 };
 
-enum class ScreenEdgePosition {
-	BOTTOM_LEFT = 0,
-	BOTTOM_CENTER = 1,
-	BOTTOM_RIGHT = 2,
-	TOP_LEFT = 3,
-	TOP_CENTER = 4,
-	TOP_RIGHT = 5,
-	CENTER_LEFT = 6,
-	CENTER_RIGHT = 7,
-	VALUE_COUNT,
-};
-
 // Data holder for on-screen messages.
 class OnScreenDisplay {
 public:
