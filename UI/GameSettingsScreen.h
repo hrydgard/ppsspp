ab58827ed66599daf93d22994d30f83ff25d27fc
Henrik Rydgård <hrydgard@gmail.com>
2023-08-30 23:30:23 +0200

    Callback -> lambda (cleanup)

--- UI/GameSettingsScreen.h
+++ UI/GameSettingsScreen.h
@@ -96,7 +96,6 @@ private:
 	UI::EventReturn OnFullscreenChange(UI::EventParams &e);
 	UI::EventReturn OnFullscreenMultiChange(UI::EventParams &e);
 	UI::EventReturn OnResolutionChange(UI::EventParams &e);
-	UI::EventReturn OnHwScaleChange(UI::EventParams &e);
 	UI::EventReturn OnRestoreDefaultSettings(UI::EventParams &e);
 	UI::EventReturn OnRenderingBackend(UI::EventParams &e);
 	UI::EventReturn OnRenderingDevice(UI::EventParams &e);
