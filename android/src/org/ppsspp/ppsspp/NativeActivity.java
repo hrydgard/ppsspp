7558544183ae63cc560024e20c0a638b38d55dc6
Henrik Rydgård <hrydgard@gmail.com>
2023-11-26 19:08:09 +0100

    Android: Add *.chd and *.CHD to allowed path patterns for shortcut/intents

--- android/src/org/ppsspp/ppsspp/NativeActivity.java
+++ android/src/org/ppsspp/ppsspp/NativeActivity.java
@@ -1370,9 +1370,6 @@ public abstract class NativeActivity extends Activity {
 					intent.setType("*/*");
 				}
 				intent.addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
-				// Possible alternative approach:
-				// String[] mimeTypes = {"application/octet-stream", "/x-iso9660-image"};
-				// intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
 				startActivityForResult(intent, packedResultCode);
 				// intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri);
 			} catch (Exception e) {
