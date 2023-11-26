eaac8656f0c81c2c492f93cc36c563f6e0b34487
sum2012 <consignpo@gmail.com>
2023-11-26 10:33:43 +0800

    Try to fix sceGeDrawSync timing
    Not EatCycles when SDK < 6.60
    fix #15224

--- Core/HLE/sceGe.cpp
+++ Core/HLE/sceGe.cpp
@@ -391,7 +391,7 @@ static u32 sceGeDrawSync(u32 mode) {
 	//wait/check entire drawing state
 	if (PSP_CoreParameter().compat.flags().DrawSyncEatCycles)
 		hleEatCycles(500000); //HACK(?) : Potential fix for Crash Tag Team Racing and a few Gundam games
-	else
+	else if (sceKernelGetCompiledSdkVersion() < 0x06060010)
 		hleEatCycles(1240);
 	DEBUG_LOG(SCEGE, "sceGeDrawSync(mode=%d)  (0=wait for completion, 1=peek)", mode);
 	return gpu->DrawSync(mode);
