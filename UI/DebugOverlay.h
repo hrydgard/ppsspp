7feae30c417bed99f8f38f721f3bcc8e864f4908
Henrik Rydgård <hrydgard@gmail.com>
2024-01-19 16:52:42 +0100

    Move the crash and FPS overlays to DebugOverlay.cpp/h

--- UI/DebugOverlay.h
+++ UI/DebugOverlay.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include "Common/File/Path.h"
 #include "Common/UI/Context.h"
 #include "Core/ConfigValues.h"
 #include "Core/ControlMapper.h"
 
 void DrawControlMapperOverlay(UIContext *ctx, const Bounds &bounds, const ControlMapper &controlMapper);
 void DrawDebugOverlay(UIContext *ctx, const Bounds &bounds, DebugOverlay overlay);
+void DrawCrashDump(UIContext *ctx, const Path &gamePath);
+void DrawFPS(UIContext *ctx, const Bounds &bounds);
