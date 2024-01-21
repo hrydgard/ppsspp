558bcff3687a27df7547fbef885a71028ed8ac82
Henrik Rydgård <hrydgard@gmail.com>
2024-01-21 17:52:35 +0100

    Reorder some settings in System so the sub-categories make sense

--- UI/GameSettingsScreen.h
+++ UI/GameSettingsScreen.h
@@ -44,7 +44,7 @@ protected:
 	void dialogFinished(const Screen *dialog, DialogResult result) override;
 
 	void CreateTabs() override;
-	bool ShowSearchControls() const { return true; }
+	bool ShowSearchControls() const override { return true; }
 
 private:
 	void PreCreateViews() override;
