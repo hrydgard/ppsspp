3011a5222a9cfe44ccf3aa3e136ba7bba0e2453a
Henrik Rydgård <hrydgard@gmail.com>
2024-05-03 00:20:12 +0200

    Pass in team ID, start of gold prep

--- b-appstore.sh
+++ b-appstore.sh
@@ -5,7 +5,12 @@
 mkdir build-ios
 pushd build-ios
 
-cmake .. -DIOS_APP_STORE=ON -DCMAKE_TOOLCHAIN_FILE=cmake/Toolchains/ios.cmake -DDEVELOPMENT_TEAM_ID=$(DEVTEAM) -DIOS_PLATFORM=OS -GXcode
+BUILD_TYPE=Release
+
+echo $DEVTEAM
+echo $BUILD_TYPE
+
+cmake .. -DIOS_APP_STORE=ON -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_TOOLCHAIN_FILE=cmake/Toolchains/ios.cmake -DDEVELOPMENT_TEAM_ID=${DEVTEAM} -DIOS_PLATFORM=OS -GXcode
 # TODO: Get a MoltenVK somewhere.
 #cp ../MoltenVK/iOS/Frameworks/libMoltenVK.dylib PPSSPP.app/Frameworks
 popd
