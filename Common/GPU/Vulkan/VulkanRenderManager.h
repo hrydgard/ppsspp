dcb8b4f81ba71a64a6d6400c00b4fcf695c77f9b
Herman Semenov <GermanAizek@yandex.ru>
2024-05-13 21:02:55 -0500

    [GPU/Core/Debugger] Reduced struct sizes using align for x64 platforms, fields carefully moved
    Then revert changes that aren't worth it

--- Common/GPU/Vulkan/VulkanRenderManager.h
+++ Common/GPU/Vulkan/VulkanRenderManager.h
@@ -156,10 +156,10 @@ struct CompileQueueEntry {
 	enum class Type {
 		GRAPHICS,
 	};
-	Type type;
+	VKRGraphicsPipeline* graphics = nullptr;
 	VkRenderPass compatibleRenderPass;
+	Type type;
 	RenderPassType renderPassType;
-	VKRGraphicsPipeline *graphics = nullptr;
 	VkSampleCountFlagBits sampleCount;
 };
 
