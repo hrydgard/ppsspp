46fb1272e9c54f312f902c133d83e1f03577bd3a
sandboxgamedev123 <158007017+sandboxgamedev123@users.noreply.github.com>
2024-06-01 07:41:23 -0400

    Add files via upload

---
+++ scripts/makeappimage_64-bit.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+if [ ! -f appimagetool-x86_64.AppImage ]; then
+    wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
+    chmod +x appimagetool-x86_64.AppImage
+fi
+
+if [ ! -f linuxdeploy-x86_64.AppImage ]; then
+    wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+    chmod +x linuxdeploy-x86_64.AppImage
+fi
+
+DESTDIR=AppDir make install
+./linuxdeploy-x86_64.AppImage --appimage-extract-and-run --appdir=AppDir \
+	--exclude-library="libX*" \
+	--exclude-library="libglib*" \
+	--exclude-library="libgobject*" \
+	--exclude-library="libgdk_pixbuf*" \
+	--exclude-library="libwayland*" \
+	--exclude-library="libgmodule*" \
+	--exclude-library="libgio*" \
+	--exclude-library="libxcb*" \
+	--exclude-library="libxkbcommon*" \
+	--exclude-library="libdb*"
+
+rm AppDir/ppsspp.png
+pushd AppDir
+ln -s usr/share/icons/hicolor/256x256/apps/ppsspp.png
+chmod +x AppRun
+popd
+./appimagetool-x86_64.AppImage --appimage-extract-and-run AppDir
