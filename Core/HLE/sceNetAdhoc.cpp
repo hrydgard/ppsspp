2222254f3e8461f3d84f91ae2d48f0ef02593727
AdamN <7974720+anr2me@users.noreply.github.com>
2024-06-29 00:44:51 +0700

    Update NetAdhocPdp_Delete

--- Core/HLE/sceNetAdhoc.cpp
+++ Core/HLE/sceNetAdhoc.cpp
@@ -2212,7 +2212,7 @@ int NetAdhocPdp_Delete(int id, int unknown) {
 				sl.l_onoff = 1;		// non-zero value enables linger option in kernel
 				sl.l_linger = 0;	// timeout interval in seconds
 				setsockopt(sock->data.pdp.id, SOL_SOCKET, SO_LINGER, (const char*)&sl, sizeof(sl));
-				shutdown(sock->data.pdp.id, SD_BOTH);
+				shutdown(sock->data.pdp.id, SD_RECEIVE);
 				closesocket(sock->data.pdp.id);
 
 				// Remove Port Forward from Router
