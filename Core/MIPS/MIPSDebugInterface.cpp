eab8d977a52c7054d368da745c1e16065c834e6f
Henrik Rydgård <hrydgard@gmail.com>
2024-09-30 20:20:12 +0200

    Android: Another gradle plugin version bump. Typo fix.

--- Core/MIPS/MIPSDebugInterface.cpp
+++ Core/MIPS/MIPSDebugInterface.cpp
@@ -291,7 +291,7 @@ std::string MIPSDebugInterface::GetRegName(int cat, int index) {
 	static const char * const fpRegName[32] = {
 		"f0", "f1", "f2", "f3", "f4", "f5", "f6", "f7",
 		"f8", "f9", "f10", "f11", "f12", "f13", "f14", "f15",
-		"f16", "f16", "f18", "f19", "f20", "f21", "f22", "f23",
+		"f16", "f17", "f18", "f19", "f20", "f21", "f22", "f23",
 		"f24", "f25", "f26", "f27", "f28", "f29", "f30", "f31",
 	};
 
