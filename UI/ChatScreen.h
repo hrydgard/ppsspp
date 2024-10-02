a9ea372c90dc8b48c4b68484bd0319d12a8af353
Henrik Rydgård <hrydgard@gmail.com>
2024-10-02 16:15:26 +0200

    Android/iOS buildfix

--- UI/ChatScreen.h
+++ UI/ChatScreen.h
@@ -34,9 +34,7 @@ private:
 	UI::EventReturn OnQuickChat4(UI::EventParams &e);
 	UI::EventReturn OnQuickChat5(UI::EventParams &e);
 
-#if PPSSPP_PLATFORM(WINDOWS) || defined(USING_QT_UI) || defined(SDL)
 	UI::TextEdit *chatEdit_ = nullptr;
-#endif
 	UI::ScrollView *scroll_ = nullptr;
 	UI::LinearLayout *chatVert_ = nullptr;
 	UI::ViewGroup *box_ = nullptr;
