4016539c85ab3332d1832253ca08f60f999d4b40
Henrik Rydgård <hrydgard@gmail.com>
2024-10-29 13:19:58 +0100

    Pre-release shader cache version bump for safety

--- GPU/Vulkan/ShaderManagerVulkan.cpp
+++ GPU/Vulkan/ShaderManagerVulkan.cpp
@@ -502,7 +502,7 @@ enum class VulkanCacheDetectFlags {
 };
 
 #define CACHE_HEADER_MAGIC 0xff51f420 
-#define CACHE_VERSION 51
+#define CACHE_VERSION 52
 
 struct VulkanCacheHeader {
 	uint32_t magic;
