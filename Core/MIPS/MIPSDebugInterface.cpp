80474fd0f0c07235c408530845839b2dcb14c82f
Henrik Rydgård <hrydgard@gmail.com>
2024-11-07 20:11:23 +0100

    Some cosmetic fixes to the ImGui debugger

--- Core/MIPS/MIPSDebugInterface.cpp
+++ Core/MIPS/MIPSDebugInterface.cpp
@@ -244,8 +244,8 @@ void MIPSDebugInterface::toggleBreakpoint(unsigned int address)
 
 
 int MIPSDebugInterface::getColor(unsigned int address, bool darkMode) const {
-	int colors[6] = { 0xe0FFFF, 0xFFe0e0, 0xe8e8FF, 0xFFe0FF, 0xe0FFe0, 0xFFFFe0 };
-	int colorsDark[6] = { ~0xe0FFFF, ~0xFFe0e0, ~0xe8e8FF, ~0xFFe0FF, ~0xe0FFe0, ~0xFFFFe0 };
+	uint32_t colors[6] = { 0xFFe0FFFF, 0xFFFFe0e0, 0xFFe8e8FF, 0xFFFFe0FF, 0xFFe0FFe0, 0xFFFFFFe0 };
+	uint32_t colorsDark[6] = { 0xFF301010, 0xFF103030, 0xFF403010, 0xFF103000, 0xFF301030, 0xFF101030 };
 
 	int n = g_symbolMap->GetFunctionNum(address);
 	if (n == -1) {
