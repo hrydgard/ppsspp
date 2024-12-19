d2a96dc7b5ce7b06a9b7527652aa15599777d5d6
Henrik Rydgård <hrydgard@gmail.com>
2024-12-19 16:04:44 +0100

    Replace some SSE4-specific operations with SSE2 sequences.

--- Common/Math/SIMDHeaders.h
+++ Common/Math/SIMDHeaders.h
@@ -64,3 +64,54 @@ static inline uint32x4_t vcgezq_f32(float32x4_t v) {
 }
 
 #endif
+
+#if PPSSPP_ARCH(SSE2)
+
+// These are SSE2 versions of SSE4.1 instructions, for compatibility and ease of
+// writing code.
+// May later figure out how to use the appropriate ones depending on compile flags.
+
+inline __m128i _mm_mullo_epi32_SSE2(const __m128i v0, const __m128i v1) {
+       __m128i a13 = _mm_shuffle_epi32(v0, 0xF5);             // (-,a3,-,a1)
+       __m128i b13 = _mm_shuffle_epi32(v1, 0xF5);             // (-,b3,-,b1)
+       __m128i prod02 = _mm_mul_epu32(v0, v1);                // (-,a2*b2,-,a0*b0)
+       __m128i prod13 = _mm_mul_epu32(a13, b13);              // (-,a3*b3,-,a1*b1)
+       __m128i prod01 = _mm_unpacklo_epi32(prod02, prod13);   // (-,-,a1*b1,a0*b0)
+       __m128i prod23 = _mm_unpackhi_epi32(prod02, prod13);   // (-,-,a3*b3,a2*b2)
+       return _mm_unpacklo_epi64(prod01, prod23);
+}
+
+inline __m128i _mm_max_epu16_SSE2(const __m128i v0, const __m128i v1) {
+       return _mm_xor_si128(
+               _mm_max_epi16(
+                       _mm_xor_si128(v0, _mm_set1_epi16((int16_t)0x8000)),
+                       _mm_xor_si128(v1, _mm_set1_epi16((int16_t)0x8000))),
+               _mm_set1_epi16((int16_t)0x8000));
+}
+
+inline __m128i _mm_min_epu16_SSE2(const __m128i v0, const __m128i v1) {
+       return _mm_xor_si128(
+               _mm_min_epi16(
+                       _mm_xor_si128(v0, _mm_set1_epi16((int16_t)0x8000)),
+                       _mm_xor_si128(v1, _mm_set1_epi16((int16_t)0x8000))),
+               _mm_set1_epi16((int16_t)0x8000));
+}
+
+// SSE2 replacement for half of a _mm_packus_epi32 but without the saturation.
+inline __m128i _mm_packu_epi32_SSE2(const __m128i v0) {
+	__m128i temp = _mm_shufflelo_epi16(v0, _MM_SHUFFLE(3, 3, 2, 0));
+	__m128 temp2 = _mm_castsi128_ps(_mm_shufflehi_epi16(temp, _MM_SHUFFLE(3, 3, 2, 0)));
+	return _mm_castps_si128(_mm_shuffle_ps(temp2, temp2, _MM_SHUFFLE(3, 3, 2, 0)));
+}
+
+// SSE2 replacement for the entire _mm_packus_epi32 but without the saturation.
+// Not ideal! pshufb would make this faster but that's SSSE3.
+inline __m128i _mm_packu2_epi32_SSE2(const __m128i v0, const __m128i v1) {
+	__m128i a0 = _mm_shufflelo_epi16(v0, _MM_SHUFFLE(3, 3, 2, 0));
+	__m128 packed0 = _mm_castsi128_ps(_mm_shufflehi_epi16(a0, _MM_SHUFFLE(3, 3, 2, 0)));
+	__m128i a1 = _mm_shufflelo_epi16(v1, _MM_SHUFFLE(3, 3, 2, 0));
+	__m128 packed1 = _mm_castsi128_ps(_mm_shufflehi_epi16(a1, _MM_SHUFFLE(3, 3, 2, 0)));
+	return _mm_castps_si128(_mm_shuffle_ps(packed0, packed1, _MM_SHUFFLE(2, 0, 2, 0)));
+}
+
+#endif
