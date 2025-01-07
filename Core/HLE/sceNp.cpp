6922483460e146b894cdc7f8385ad5a079145ff2
Henrik Rydgård <hrydgard@gmail.com>
2025-01-07 21:53:13 +0100

    Change Np country code to 'us'

--- Core/HLE/sceNp.cpp
+++ Core/HLE/sceNp.cpp
@@ -41,9 +41,9 @@ PSPTimeval npSigninTimestamp{};
 int npParentalControl = PARENTAL_CONTROL_ENABLED;
 int npUserAge = 24; // faking user Age to 24 yo
 int npChatRestriction = 0; // default/initial value on Patapon 3 is 1 (restricted boolean?)
-SceNpMyLanguages npMyLangList = { 1033, 2057, 1036 };
+SceNpMyLanguages npMyLangList = { 1033, 2057, 1036 };  // TODO: What are these?
 // Fields are 4-sized, so the data needs to be too.
-char npCountryCode[4] = "fr"; // dummy data taken from https://www.psdevwiki.com/ps3/X-I-5-Ticket. France?
+char npCountryCode[4] = "us"; // dummy data taken from https://www.psdevwiki.com/ps3/X-I-5-Ticket. Mainly affects what EULA is downloaded.
 char npRegionCode[4] = "c9"; // not sure what "c9" meant, since it was close to country code data, might be region-related data?
 std::string npOnlineId = "DummyOnlineId"; // SceNpOnlineId struct?
 std::string npServiceId = ""; // UNO game uses EP2006-NPEH00020_00
