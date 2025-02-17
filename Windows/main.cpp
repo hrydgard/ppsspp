fe19283c6e84bdc95fc90aa9b69e3f6695bd9262
Henrik Rydgård <hrydgard@gmail.com>
2025-02-17 15:44:22 -0600

    Fix main menu exit button on Windows

--- Windows/main.cpp
+++ Windows/main.cpp
@@ -537,8 +537,8 @@ bool System_MakeRequest(SystemRequestType type, int requestId, const std::string
 	switch (type) {
 	case SystemRequestType::EXIT_APP:
 		if (!NativeIsRestarting()) {
-			// Clean exit is handled in WM_DESTROY.
-			DestroyWindow(MainWindow::GetHWND());
+			// Not on the window thread here, so postmessage wm_close is pretty reasonable.
+			PostMessage(MainWindow::GetHWND(), WM_CLOSE, 0, 0);
 		}
 		return true;
 	case SystemRequestType::RESTART_APP:
@@ -550,7 +550,8 @@ bool System_MakeRequest(SystemRequestType type, int requestId, const std::string
 			PostMessage(MainWindow::GetHWND(), MainWindow::WM_USER_RESTART_EMUTHREAD, 0, 0);
 		} else {
 			g_Config.bRestartRequired = true;
-			DestroyWindow(MainWindow::GetHWND());
+			// Not on the window thread here, so postmessage wm_close is pretty reasonable.
+			PostMessage(MainWindow::GetHWND(), WM_CLOSE, 0, 0);
 		}
 		return true;
 	}
