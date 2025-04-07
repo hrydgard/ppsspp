4d215b1f5477a27e4caed3d88be47a7b3a81a290
Henrik Rydgård <hrydgard@gmail.com>
2025-04-07 16:33:43 +0200

    Cleaner exit from when stepping through a frame dump

--- GPU/Debugger/Playback.cpp
+++ GPU/Debugger/Playback.cpp
@@ -91,7 +91,7 @@ static std::vector<u8> lastExecPushbuf;
 static std::thread replayThread;
 
 static std::mutex opStartLock;
-static std::condition_variable opStartWait;
+static std::condition_variable g_condOpStartWait;
 
 static std::mutex opFinishLock;
 static std::condition_variable opFinishWait;
@@ -107,7 +107,7 @@ u32 ExecuteOnMain(Operation opToExec) {
 		g_opToExec = opToExec;
 		g_retVal = 0;
 		g_opDone = false;
-		opStartWait.notify_one();
+		g_condOpStartWait.notify_one();
 	}
 
 	// now wait for completion. At that point, noone cares about g_opToExec anymore, and we can safely
@@ -900,7 +900,16 @@ static u32 LoadReplay(const std::string &filename) {
 }
 
 void Replay_Unload() {
-	// TODO: Hm, we have a problem here. We might be paused inside a replay and exiting - in this case, the thread is still running.
+	// We might be paused inside a replay - in this case, the thread is still running and we need to tell it to stop.
+	if (replayThread.joinable()) {
+		{
+			std::unique_lock<std::mutex> waitLock(opFinishLock);
+			// Just override the next op to bail.
+			g_opToExec = Operation{ OpType::Done };
+			opFinishWait.notify_one();
+		}
+		replayThread.join();
+	}
 
 	_dbg_assert_(!replayThread.joinable());
 
@@ -981,7 +990,7 @@ ReplayResult RunMountedReplay(const std::string &filename) {
 	// OK, now wait for and perform the desired action.
 	{
 		std::unique_lock<std::mutex> lock(opStartLock);
-		opStartWait.wait(lock, []() { return g_opToExec.type != OpType::None; });
+		g_condOpStartWait.wait(lock, []() { return g_opToExec.type != OpType::None; });
 	}
 
 	switch (g_opToExec.type) {
