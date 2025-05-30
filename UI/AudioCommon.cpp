3119eab6ef42b115ac3a966eebbd1793b40b340c
Henrik Rydgård <hrydgard@gmail.com>
2025-05-30 21:54:16 +0200

    Integrate Dolphin's granule based audio resampler.
    Removed parts of it that were not relevant.
    
    Working, it seems. Not sure about the buffer size thing.
    
    See #20146 and https://github.com/dolphin-emu/dolphin/pull/13352

--- UI/AudioCommon.cpp
+++ UI/AudioCommon.cpp
@@ -1,24 +1,52 @@
+// Copyright (c) 2012- PPSSPP Project.
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 2.0 or later versions.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License 2.0 for more details.
+
+// A copy of the GPL 2.0 should have been included with the program.
+// If not, see http://www.gnu.org/licenses/
+
+// Official git repository and contact information can be found at
+// https://github.com/hrydgard/ppsspp and http://www.ppsspp.org/.
+
 #include "Common/System/System.h"
+#include "Core/Config.h"
 #include "Core/HW/StereoResampler.h"  // TODO: doesn't belong in Core/HW...
+#include "Core/HW/GranularMixer.h"
 #include "UI/AudioCommon.h"
 #include "UI/BackgroundAudio.h"
 
 StereoResampler g_resampler;
+GranularMixer g_granular;
 
 // numFrames is number of stereo frames.
 // This is called from *outside* the emulator thread.
 int NativeMix(int16_t *outStereo, int numFrames, int sampleRateHz) {
-	int validFrames = g_resampler.Mix(outStereo, numFrames, false, sampleRateHz);
+	// Mix UI sound effects on top.
+	int validFrames = 0;
+	if (g_Config.iAudioSyncMode == (int)AudioSyncMode::GRANULAR) {
+		validFrames = g_granular.Mix(outStereo, numFrames, sampleRateHz);
+	} else {
+		validFrames = g_resampler.Mix(outStereo, numFrames, false, sampleRateHz);
+	}
 
-	// Mix sound effects on top.
 	g_BackgroundAudio.SFX().Mix(outStereo, validFrames, sampleRateHz);
-
 	return validFrames;
 }
 
 void System_AudioGetDebugStats(char *buf, size_t bufSize) {
 	if (buf) {
-		g_resampler.GetAudioDebugStats(buf, bufSize);
+		if (g_Config.iAudioSyncMode == (int)AudioSyncMode::GRANULAR) {
+			snprintf(buf, bufSize, "(No stats available for granular yet)");
+		} else {
+			g_resampler.GetAudioDebugStats(buf, bufSize);
+		}
 	} else {
 		g_resampler.ResetStatCounters();
 	}
@@ -30,7 +58,11 @@ void System_AudioClear() {
 
 void System_AudioPushSamples(const int32_t *audio, int numSamples, float volume) {
 	if (audio) {
-		g_resampler.PushSamples(audio, numSamples, volume);
+		if (g_Config.iAudioSyncMode == (int)AudioSyncMode::GRANULAR) {
+			g_granular.PushSamples(audio, numSamples, volume);
+		} else {
+			g_resampler.PushSamples(audio, numSamples, volume);
+		}
 	} else {
 		g_resampler.Clear();
 	}
