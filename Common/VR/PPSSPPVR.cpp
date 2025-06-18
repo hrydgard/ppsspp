51d60b2a0502b62cff1269f43ac28a312239ac86
Luboš V. <tridosm@gmail.com>
2025-06-18 16:03:39 +0200

    OpenXR - Support passthough on Pico headsets

--- Common/VR/PPSSPPVR.cpp
+++ Common/VR/PPSSPPVR.cpp
@@ -153,6 +153,7 @@ void InitVROnAndroid(void* vm, void* activity, const char* system, int version, 
 	if (strcmp(vendor, "PICO") == 0) {
 		VR_SetPlatformFLag(VR_PLATFORM_CONTROLLER_PICO, true);
 		VR_SetPlatformFLag(VR_PLATFORM_EXTENSION_INSTANCE, true);
+		VR_SetPlatformFLag(VR_PLATFORM_EXTENSION_PASSTHROUGH, true);
 		VR_SetConfigFloat(VR_CONFIG_VIEWPORT_SUPERSAMPLING, 1.0f);
 	} else {
 		VR_SetPlatformFLag(VR_PLATFORM_CONTROLLER_QUEST, true);
