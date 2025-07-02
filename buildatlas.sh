53ac90e8249bfa663859bb5dd34af53e1aded3ee
Nab <22328803+NABN00B@users.noreply.github.com>
2025-07-02 20:26:22 +0000

    Update buildatlas shell scripts

--- buildatlas.sh
+++ buildatlas.sh
@@ -1,9 +1,1 @@
-# Note that we do not copy the big font atlas to Android assets. No longer needed!
-
-./ext/native/tools/build/atlastool ui_atlasscript.txt ui 8888 && cp ui_atlas.zim ui_atlas.meta assets && rm ui_atlas.cpp ui_atlas.h
-#./ext/native/tools/build/atlastool font_atlasscript.txt font 8888 && cp font_atlas.zim font_atlas.meta assets && rm font_atlas.cpp font_atlas.h
-#./ext/native/tools/build/atlastool asciifont_atlasscript.txt asciifont 8888 && cp asciifont_atlas.zim asciifont_atlas.meta assets && rm asciifont_atlas.cpp asciifont_atlas.h
-
-rm ui_atlas.zim ui_atlas.meta
-#rm font_atlas.zim font_atlas.meta
-#rm asciifont_atlas.zim asciifont_atlas.meta
+./ext/native/tools/build/atlastool ui_atlasscript.txt ui 8888 && mv ui_atlas.zim ui_atlas.meta assets && rm ui_atlas.cpp ui_atlas.h
