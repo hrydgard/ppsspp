b5a4f81c596d5aa41184aeb83ba0081fce31c95b
Henrik Rydgård <hrydgard@gmail.com>
2025-09-12 12:16:42 -0600

    Minor cleanup of fontatlas scripts (they still need more cleanup)

--- buildatlas.sh
+++ buildatlas.sh
@@ -1,1 +1,4 @@
-./ext/native/tools/build/atlastool ui_atlasscript.txt ui 8888 && mv ui_atlas.zim ui_atlas.meta assets && rm ui_atlas.cpp ui_atlas.h
+TOOL=./ext/native/tools/build/atlastool
+# TOOL=Windows/ARM64/Debug/AtlasTool.exe
+
+$TOOL ui_atlasscript.txt ui 8888 && mv ui_atlas.zim ui_atlas.meta assets && rm ui_atlas.cpp ui_atlas.h
