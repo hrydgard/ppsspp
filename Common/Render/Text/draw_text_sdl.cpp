bc17b7c822d674a5f6f0e3ce254e587188c97933
Henrik Rydgård <hrydgard@gmail.com>
2025-11-23 11:42:43 +0100

    iOS buildfix, SDL buildfix

--- Common/Render/Text/draw_text_sdl.cpp
+++ Common/Render/Text/draw_text_sdl.cpp
@@ -261,7 +261,7 @@ void TextDrawerSDL::SetOrCreateFont(const FontStyle &style) {
 		int ptSize = static_cast<int>(style.sizePts / dpiScale_ * 1.25f);
 		TTF_Font *font = TTF_OpenFont(useFont.c_str(), ptSize);
 		if (!font) {
-			bool exists = File::Exists(useFont);
+			bool exists = File::Exists(Path(useFont));
 			ERROR_LOG(Log::G3D, "Failed to load font file %s: exists=%d", useFont.c_str(), (int)exists);
 		}
 		// Still, even if it failed and font is nullptr, mark it in the map to avoid retrying.
