5aa266934b5f99dfe47dda2229d1998fd37e82f3
Katharine Chui <katharine.chui@gmail.com>
2026-01-19 20:49:57 +0100

    import aemu_postoffice as a submodule instead

--- ext/aemu_postoffice/client/delay_impl_linux.c
+++
@@ -1,9 +0,0 @@
-#include <time.h>
-
-void delay(int ms){
-	const struct timespec duration = {
-		.tv_sec = 0,
-		.tv_nsec = ms * 1000000,
-	};
-	nanosleep(&duration, NULL);
-}
--- ext/aemu_postoffice/client/delay_impl_windows.c
+++
@@ -1,5 +0,0 @@
-#include <windows.h>
-
-void delay(int ms){
-	Sleep(ms);
-}
