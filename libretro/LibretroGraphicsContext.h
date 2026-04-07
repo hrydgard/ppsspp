72bd0fa4d3d58c411186ab221bab911d1497089e
celerizer <celerizer@gmail.com>
2026-04-07 11:05:50 -0500

    add null-checks on software render path

--- libretro/LibretroGraphicsContext.h
+++ libretro/LibretroGraphicsContext.h
@@ -76,9 +76,11 @@ public:
 		GPUDebugBuffer buf;
 		u16 w = NATIVEWIDTH;
 		u16 h = NATIVEHEIGHT;
-		gpuDebug->GetOutputFramebuffer(buf);
-		const std::vector<u32> pixels = TranslateDebugBufferToCompare(&buf, w, h);
-		memcpy(soft_bmp, pixels.data(), SOFT_BMP_SIZE);
+		if (gpuDebug) {
+			gpuDebug->GetOutputFramebuffer(buf);
+			const std::vector<u32> pixels = TranslateDebugBufferToCompare(&buf, w, h);
+			memcpy(soft_bmp, pixels.data(), SOFT_BMP_SIZE);
+		}
 		u32 offset = g_Config.bDisplayCropTo16x9 ? w << 1 : 0;
 		h -= g_Config.bDisplayCropTo16x9 ? 2 : 0;
 		video_cb(soft_bmp + offset, w, h, w << 2);
