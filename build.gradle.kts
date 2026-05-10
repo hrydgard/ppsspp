51a420be4b569e9a6d90dd14b07d2e4d6153e54e
Henrik Rydgård <hrydgard@gmail.com>
2026-05-10 22:25:30 +0200

    Android Studio: Bump the plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-	id("com.android.application") version "9.2.0" apply false
+	id("com.android.application") version "9.2.1" apply false
 	id("com.google.protobuf") version "0.10.0" apply false
 }
