82e6f563d7a7f399ed70e26d154b236943222a71
Nemoumbra <garbuzov.nickolay@gmail.com>
2026-05-14 01:59:25 +0300

    Remove the outdated comment

--- Common/SysError.h
+++ Common/SysError.h
@@ -5,6 +5,5 @@
 // Generic function to get last error message.
 // Call directly after the command or use the error num.
 // This function might change the error code.
-// Defined in Misc.cpp.
 std::string GetLastErrorMsg();
 std::string GetStringErrorMsg(int errCode);
