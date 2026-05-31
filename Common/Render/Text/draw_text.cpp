d6060d4b3bda1aaec5af6960d15f5199d49faa2a
Kevin Reinholz <kreinholz@gmail.com>
2026-05-31 13:27:45 -0700

    Build fixes for clang on FreeBSD

--- Common/Render/Text/draw_text.cpp
+++ Common/Render/Text/draw_text.cpp
@@ -1,6 +1,6 @@
 #include "ppsspp_config.h"
 
-#if defined(__linux__) && !defined(__ANDROID__)
+#if defined(__linux__) || defined(__FreeBSD__) && !defined(__ANDROID__)
 # include <dlfcn.h>
 #elif defined(_WIN32)
 # include <windows.h>
@@ -29,13 +29,15 @@ static bool IsRenderDocLoaded() {
 	return false;
 #elif defined(_WIN32)
 	return (GetModuleHandleA("renderdoc.dll") != nullptr);
-#elif defined(__linux__)
+#elif defined(__linux__) || defined(__FreeBSD__)
 	// Returns a pointer if loaded, otherwise null
 	void* handle = dlopen("librenderdoc.so", RTLD_NOW | RTLD_NOLOAD);
 	if (handle) {
 		dlclose(handle);
 		return true;
 	}
+	return false;
+#else
 	return false;
 #endif
 }
