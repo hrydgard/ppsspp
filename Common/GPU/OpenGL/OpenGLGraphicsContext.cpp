f8ccc78e3122b06179ae3a1b35a95c9d2f671209
Henrik Rydgård <hrydgard@gmail.com>
2026-07-29 14:40:01 +0200

    OpenGL on Android: Don't try to detect version too early

--- Common/GPU/OpenGL/OpenGLGraphicsContext.cpp
+++ Common/GPU/OpenGL/OpenGLGraphicsContext.cpp
@@ -8,16 +8,19 @@
 
 OpenGLGraphicsContext::OpenGLGraphicsContext() {
 	SetGPUBackend(GPUBackend::OPENGL);
-	CheckGLExtensions();
+	// NOTE: On some Android devices, OpenGL is not yet available here so calling CheckGLExtension here
+	// doesn't work.
 	// OpenGL handles rotated rendering in the driver.
 	g_display.rotation = DisplayRotation::ROTATE_0;
 	g_display.rot_matrix.setIdentity();
 }
 
 bool OpenGLGraphicsContext::InitSurface(WindowSystem winsys, void *data1, void *data2, std::string *errorMessage) {
+	CheckGLExtensions();
 	INFO_LOG(Log::G3D, "OpenGLGraphicsContext::InitSurface");
 	draw_ = Draw::T3DCreateGLContext(false);  // Can't fail
 	renderManager_ = (GLRenderManager *)draw_->GetNativeObject(Draw::NativeObject::RENDER_MANAGER);
+	// TODO: In VR mode, we don't want a lot of inflight frames.
 	renderManager_->SetInflightFrames(g_Config.iInflightFrames);
 	const bool result = draw_->CreatePresets();
 	_dbg_assert_(result);
