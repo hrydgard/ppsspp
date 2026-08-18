cd5cf87120f172d762f37a1fbf7258305836eade
Henrik Rydgård <hrydgard@gmail.com>
2026-08-18 14:31:13 +0200

    Fix performance bug in IRJit when using rewind states

--- Core/MIPS/IR/IRFrontend.cpp
+++ Core/MIPS/IR/IRFrontend.cpp
@@ -51,7 +51,9 @@ void IRFrontend::DoState(PointerWrap &p) {
 	Do(p, js.startDefaultPrefix);
 	if (s >= 2) {
 		Do(p, js.hasSetRounding);
-		js.lastSetRounding = 0;
+		if (p.mode == p.MODE_READ) {
+			js.lastSetRounding = 0;
+		}
 	} else {
 		js.hasSetRounding = 1;
 	}
